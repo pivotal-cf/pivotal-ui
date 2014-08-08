cd87f6eca3ab6d075ee31c2b71f7573ee015ab42
Cameron Cundiff <ccundiff@pivotallabs.com>
2014-08-08 09:30:57 -0400

    Deploy via https.
    Signed-off-by: Nicole Sullivan <nsullivan@pivotallabs.com>

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ grunt
 wget 'https://cli.run.pivotal.io/stable?release=linux64-binary&source=github' -O cf-cli.tgz
 tar -xvf ./cf-cli.tgz
 chmod +x ./cf
-./cf login -u $PWS_USERNAME -p $PWS_PASSWORD -a http://api.run.pivotal.io
+./cf login -u $PWS_USERNAME -p $PWS_PASSWORD -a https://api.run.pivotal.io
 ./cf target -s pivotal-ui
 ./cf push -f manifest.yml
