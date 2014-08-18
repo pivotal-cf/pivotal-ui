1fd87ef1b8b888731ad393e1bc2797d8459d4e12
Nicole Sullivan <nsullivan@pivotallabs.com>
2014-08-18 15:43:00 -0400

    change path to cf
    Signed-off-by: Cameron Cundiff <ccundiff@pivotallabs.com>

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ grunt
 wget 'https://cli.run.pivotal.io/stable?release=linux64-binary&source=github' -O cf-cli.tgz
 tar -xvf ./cf-cli.tgz
 chmod +x ./cf
-cf login -u $PWS_USERNAME -p $PWS_PASSWORD -a https://api.run.pivotal.io -o pivotal -s pivotal-ui
-cf push -f manifest.yml
+./cf login -u $PWS_USERNAME -p $PWS_PASSWORD -a https://api.run.pivotal.io -o pivotal -s pivotal-ui
+./cf push -f manifest.yml
