3061cc7d424e78968083ab93c33d4db80952e2d3
Nicole Sullivan <nsullivan@pivotallabs.com>
2014-09-23 10:17:44 -0400

    Remove concat configuration and task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,5 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    concat: {
-      options: {
-        separator: ';'
-      },
-      production_js: {
-        src: [
-          'src/modernizr/modernizr.min.js',
-          'src/jquery/jquery.js',
-          'src/bootstrap/javascript/tooltip.js',
-          'src/bootstrap/javascript/popover.js',
-          'src/bootstrap/javascript/alert.js',
-          'src/bootstrap/javascript/affix.js',
-          'src/bootstrap/javascript/button.js',
-          'src/bootstrap/javascript/carousel.js',
-          'src/bootstrap/javascript/collapse.js',
-          'src/bootstrap/javascript/dropdown.js',
-          'src/bootstrap/javascript/modal.js',
-          'src/bootstrap/javascript/scrollspy.js',
-          'src/bootstrap/javascript/tab.js',
-          'src/bootstrap/javascript/transition.js',
-          'src/syntax-highlighting/prism.js',
-          'src/pivotal-ui/javascripts/*.js'
-        ],
-        dest: 'dist/pivotal-ui/pivotal-ui.js'
-      }
-    },
     compass: {
       dist: {
         options: {
@@ -104,7 +78,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-hologram');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
