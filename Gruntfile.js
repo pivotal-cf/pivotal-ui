4788ea3af4aabcd7081c331ff61896703a52e7a0
Cameron Cundiff <pair+ccundiff@pivotallabs.com>
2014-09-26 14:37:51 -0400

    Add grunt-semantic-release for versioning and release.
    [#79540384]

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,95 +1,11 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    compass: {
-      dist: {
-        options: {
-          config: 'config/compass.rb',
-          specify: ['src/pivotal-ui/pivotal-ui.scss', 'src/style_guide/style_guide.css.scss']
-        }
-      }
-    },
-    hologram: {
-      generate: {
-        options: {
-          config: 'hologram_config.yml'
-        }
-      }
-    },
-    watch: {
-      sass: {
-        files: ['src/**/*.scss', 'src/**/*.js', 'doc_assets/**/*', 'src/style_guide/*.html'],
-        tasks: ['default']
-      },
-      livereload: {
-        options: { livereload: true },
-        files: ['dist/**/*']
-      }
-    },
-    clean: ["build", "dist"],
-    browserify: {
-      dist: {
-        src: [
-          'src/pivotal-ui/javascripts/index.js'
-        ],
-        dest: 'dist/pivotal-ui/pivotal-ui.js'
-      }
-    },
-    copy: {
-      main: {
-        files: [
-          {
-            expand: true, src: ['src/style_guide/*.js', 'src/style_guide/github.css'],
-            dest: 'dist/style_guide', filter: 'isFile', flatten: true
-          },
-          {
-            expand: true, src: ['src/style_guide/*.html'],
-            dest: 'dist/', filter: 'isFile', flatten: true
-          },
-          {
-            expand: true, src: ['src/syntax-highlighting/*'],
-            dest: 'dist/syntax-highlighting/', filter: 'isFile', flatten: true
-          },
-          {
-            expand: true, src: ['src/nginx.conf','src/Staticfile'],
-            dest: 'dist/', filter: 'isFile', flatten: true
-          },
-          {
-            expand: true, src: ['font-awesome/**/*', 'source-sans-pro/**/*', 'images/**/*'], cwd: 'src/',
-            dest: 'dist/', filter: 'isFile', flatten: false
-          }
-        ]
-      },
-      to_pws: {
-        files: [
-          {
-            expand: true,
-            cwd: 'dist/',
-            src: ['**', '!nginx.conf', '!variables.css.scss', '!Staticfile'],
-            dest: '../pws-marketing-2/vendor',
-            filter: 'isFile',
-            flatten: false
-          },
-          {
-            expand: true,
-            cwd: 'src/pivotal-ui/components/',
-            src: ['variables.css.scss'],
-            dest: '../pws-marketing-2/_sass/',
-            filter: 'isFile',
-            rename: function(dest, src) {
-              return dest + '_pui_variables.scss';            }
-          }
-        ]
+    release: {
+      bump: {
+        commitMessage: 'Released v%VERSION%'
       }
     }
   });
 
-
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-hologram');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
-  grunt.registerTask('default', ['browserify:dist','compass','copy:main','hologram', 'copy:to_pws']);
+  grunt.loadNpmTasks('grunt-semantic-release');
 };
