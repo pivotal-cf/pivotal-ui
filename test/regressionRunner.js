5e6acc65f6b9e5a1d5a83f04693e6b3af138ca92
Geoff Pleiss <gpleiss@pivotal.io>
2014-09-26 11:14:30 -0400

    add tests for large and small gravatar components
    Signed-off-by: Nicole Sullivan <nsullivan@pivotal.io>

--- test/regressionRunner.js
+++ test/regressionRunner.js
@@ -2,5 +2,7 @@
 window.onload = function() {
   csscritic.addReporter(csscritic.BasicHTMLReporter());
   csscritic.add('components/alerts/index.html');
+  csscritic.add('components/small_gravatar/index.html');
+  csscritic.add('components/large_gravatar/index.html');
   csscritic.execute();
 };
