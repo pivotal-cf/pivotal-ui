cb746848c69727385d631e767402b067022745cd
Jenn Dodd <jdodd@pivotal.io>
2014-10-09 14:05:58 -0400

    Refactor scriptlets and cleanup styleguide.js
    [#78510872]
    
    Signed-off-by: Geoff Pleiss <gpleiss@pivotal.io>

--- src/style_guide/style_guide.js
+++ src/style_guide/style_guide.js
@@ -1,6 +1,3 @@
-// stylesheet swappin'
-
-
 // jquery plugin from: http://css-tricks.com/snippets/jquery/get-query-params-object/
 jQuery.extend({
   getQueryParameters : function(str) {
@@ -8,44 +5,41 @@ jQuery.extend({
   }
 });
 
-$(document).ready(
-  function(){
-    $('input.alternate-css').change(function() {
-      if($(this).get(0).checked) {
-        $("link[href*='prism-okaida.css']").attr('disabled', 'disabled');
-        $("link[href*='prism-default.css']").removeAttr('disabled');
-      } else {
-        $("link[href*='prism-okaida.css']").removeAttr('disabled');
-        $("link[href*='prism-default.css']").attr('disabled', 'disabled');
-      }
-    });
-  }
-);
-
-$(document).ready(
-  function() {
-    var headerHeight = 50;
-    $('a.styleguide-subsection').on('click', function(e){
-      e.preventDefault();
-      var target = $(this).attr('href');
-      var desiredScrollPosition = $(target).offset().top - headerHeight;
-      $('body, html').animate({
-        scrollTop: desiredScrollPosition
-      }, 500, function () {
-        $(target).focus();
-        window.location.hash = target;
-      });
-      return false;
-    });
-  }
-);
-
-$(document).ready (
-  function() {
-    if ($.getQueryParameters().target) {
-      var newScrollTop = $('#' + $.getQueryParameters().target).offset().top - 50;
-      $(window).scrollTop(newScrollTop);
+/* Stylesheet swappin */
+$(document).ready(function(){
+  $('input.alternate-css').change(function() {
+    if($(this).get(0).checked) {
+      $("link[href*='prism-okaida.css']").attr('disabled', 'disabled');
+      $("link[href*='prism-default.css']").removeAttr('disabled');
+    } else {
+      $("link[href*='prism-okaida.css']").removeAttr('disabled');
+      $("link[href*='prism-default.css']").attr('disabled', 'disabled');
     }
+  });
+});
+
+/* Adjust scrollTop when navigating to a subsection on the current page */
+$(document).ready(function() {
+  var headerHeight = 50;
+  $('a.styleguide-subsection').on('click', function(e){
+    e.preventDefault();
+    var target = $(this).attr('href');
+    var desiredScrollPosition = $(target).offset().top - headerHeight;
+    $('body, html').animate({
+      scrollTop: desiredScrollPosition
+    }, 500, function () {
+      $(target).focus();
+      window.location.hash = target;
+    });
+    return false;
+  });
+});
+
+/* Adjust scrollTop when navigating to a subsection on a different page */
+$(document).ready (function() {
+  if ($.getQueryParameters().target) {
+    var newScrollTop = $('#' + $.getQueryParameters().target).offset().top - 50;
+    $(window).scrollTop(newScrollTop);
   }
-);
+});
 
