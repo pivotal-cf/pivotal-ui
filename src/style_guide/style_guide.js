bf97096745fce5fbe2fc83b20e27a99940bc23b4
Bebe Peng <bpeng@pivotal.io>
2014-10-15 12:01:44 -0400

    fix(styleguide): Scroll to correct position from top level nav
    [#80392096]
    
    Signed-off-by: Nicole Sullivan <nsullivan@pivotal.io>

--- src/style_guide/style_guide.js
+++ src/style_guide/style_guide.js
@@ -1,10 +1,3 @@
-// jquery plugin from: http://css-tricks.com/snippets/jquery/get-query-params-object/
-jQuery.extend({
-  getQueryParameters : function(str) {
-    return (str || document.location.search).replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0];
-  }
-});
-
 /* Stylesheet swappin */
 $(document).ready(function(){
   $('input.alternate-css').change(function() {
@@ -19,15 +12,12 @@ $(document).ready(function(){
 });
 
 /* Adjust scrollTop when navigating to a subsection on the current page */
-$(document).ready(function() {
-  var headerHeight = 50;
-  $('a.styleguide-subsection').on('click', function(e) {
-    var queryString = e.target.href.slice(e.target.href.indexOf('?'));
-    window.history.pushState('', '', queryString);
-    e.preventDefault();
 
-    var $target = $('#' + $.getQueryParameters(queryString).target);
-    var desiredScrollPosition = $target.offset().top - headerHeight;
+$(document).ready(function() {
+  $('a.styleguide-subsection').on('click', function(e) {
+    var $target = $(e.target.hash);
+    var desiredScrollPosition = $target.offset().top;
+    window.history.pushState('', '', e.target.hash);
     $('body, html').animate({
       scrollTop: desiredScrollPosition
     }, 500, function () {
@@ -36,13 +26,3 @@ $(document).ready(function() {
     return false;
   });
 });
-
-/* Adjust scrollTop when navigating to a subsection on a different page */
-$(document).ready (function() {
-  if ($.getQueryParameters().target) {
-    $target = $('#' + $.getQueryParameters().target);
-    var newScrollTop = $target.offset().top - 50;
-    $(window).scrollTop(newScrollTop);
-  }
-});
-
