266f1b7931530671328cfd1ced9a9024f3b8dd21
Geoff Pleiss <gpleiss@pivotal.io>
2014-10-16 14:59:16 -0400

    chore(back-to-top): lint back-to-top component js

--- src/pivotal-ui/javascripts/back-to-top.js
+++ src/pivotal-ui/javascripts/back-to-top.js
@@ -1,6 +1,7 @@
+'use strict';
+var $ = require('jquery');
+
 $(document).ready(function(){
-	
-	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 400) {
 			$('.back-to-top').fadeIn();
@@ -8,8 +9,7 @@ $(document).ready(function(){
 			$('.back-to-top').fadeOut();
 		}
 	});
-	
-	//Click event to scroll to top
+
 	$('.back-to-top').click(function(){
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
