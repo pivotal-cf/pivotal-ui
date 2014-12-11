64ccd18e14403b3a3526fa4ef9f1afe0af3e2177
Nicole Sullivan <nsullivan@pivotal.io>
2014-12-11 11:30:40 -0500

    feat(react): Introduce React typography components
    * Add Heading generator and three type systems which
    wrap it (Default, Alternate, Marketing)
    * Add JSX support in the style guide
    * Point hologram to a fork for jsx support
    
    [Finishes #84033714]
    
    Signed-off-by: Paul Meskers <pmeskers@pivotal.io>

--- src/pivotal-ui/javascripts/pivotal-ui-react.js
+++ src/pivotal-ui/javascripts/pivotal-ui-react.js
@@ -8,3 +8,14 @@ require('./back-to-top')();
 
 global.React = require('react');
 global.TableSortable = global.React.createFactory(require('./table-sortable.jsx'));
+
+global.Heading = require('./typography.jsx').Heading;
+
+global.AlternateH1 = require('./typography.jsx').AlternateH1;
+global.AlternateH2 = require('./typography.jsx').AlternateH2;
+global.AlternateH3 = require('./typography.jsx').AlternateH3;
+
+global.DefaultH1 = require('./typography.jsx').DefaultH1;
+global.DefaultH2 = require('./typography.jsx').DefaultH2;
+global.DefaultH3 = require('./typography.jsx').DefaultH3;
+
---
+++ test/spec/javascripts/typography_spec.js
@@ -0,0 +1,196 @@
+'use strict';
+
+var $ = require('jquery');
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+
+var Heading = React.createFactory(require('../../../src/pivotal-ui/javascripts/typography.jsx').Heading);
+
+describe('Heading', function() {
+  beforeEach(function() {
+    this.node = $('<div id="container"></div>').appendTo('body').get(0);
+  });
+
+  afterEach(function() {
+    React.unmountComponentAtNode(this.node);
+  });
+
+  describe("when the Heading has no properties passed in", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "foo",
+        children: "Heading text here"
+      }),
+      this.node
+      );
+    });
+
+    it("creates a p tag", function() {
+      expect($('#container p')).toContainText("Heading text here");
+    });
+
+    it("sets no additional classes", function() {
+      expect($('#container p').prop('className')).toEqual("");
+    });
+  });
+
+  describe("when the Heading has a specified h1 - h6 element", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "h1",
+        children: "Heading text here"
+      }),
+      this.node
+      );
+    });
+
+    it("creates an element of the specified heading", function() {
+      expect($('#container h1')).toContainText("Heading text here");
+    });
+  });
+
+  describe("when the Heading has a className provided", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "h1",
+        allcaps: true,
+        className: "myClass",
+        children: "Heading text here"
+      }),
+      this.node
+      );
+    });
+
+    it("creates an element with the className AND default classes", function() {
+      expect($('#container h1')).toHaveClass('myClass');
+      expect($('#container h1')).toHaveClass('em-alt');
+    });
+  });
+
+  describe("when the Heading has valid size provided", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "h1",
+        size: 'small'
+      }),
+      this.node
+      );
+    });
+
+    it("creates an element with the size class set", function() {
+      expect($('#container h1')).toHaveClass('small');
+    });
+  });
+
+  describe("when the Heading has invalid size provided", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "h1",
+        size: "fop"
+      }),
+      this.node
+      );
+    });
+
+    it("creates an element with the size class set", function() {
+      expect($('#container h1')).not.toHaveClass('fop');
+    });
+  });
+
+
+  describe("when the Heading has allcaps set", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "h2",
+        allcaps: true,
+        children: "Heading text here"
+      }),
+      this.node
+      );
+    });
+
+    it("creates an element with the em-alt class", function() {
+      expect($('#container h2')).toHaveClass('em-alt');
+    });
+  });
+
+  describe("when the Heading has a valid bold set", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "h2",
+        bold: "high",
+        children: "Heading text here"
+      }),
+      this.node
+      );
+    });
+
+    it("creates an element with the em-{bold} class", function() {
+      expect($('#container h2')).toHaveClass('em-high');
+    });
+  });
+
+  describe("when the Heading has an invalid bold set", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "h2",
+        bold: "superbold",
+        children: "Heading text here"
+      }),
+      this.node
+      );
+    });
+
+    it("creates an element without the em-{bold} class", function() {
+      expect($('#container h2')).not.toHaveClass('em-superbold');
+    });
+  });
+
+  describe("when the Heading has color set", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "h2",
+        color: "purple",
+        children: "Heading text here"
+      }),
+      this.node
+      );
+    });
+
+    it("creates an element with the color class", function() {
+      expect($('#container h2')).toHaveClass('purple');
+    });
+  });
+
+  describe("when Heading has many properties set", function() {
+    beforeEach(function() {
+      React.render(
+        Heading({
+        element: "h2",
+        size: "h4",
+        color: "purple",
+        bold: "max",
+        allcaps: true,
+        children: "Heading text here"
+      }),
+      this.node
+      );
+    });
+
+    it("creates an element all the necessary classes", function() {
+      expect($('#container h2')).toHaveClass('purple');
+      expect($('#container h2')).toHaveClass('em-alt');
+      expect($('#container h2')).toHaveClass('em-max');
+      expect($('#container h2')).toHaveClass('h4');
+    });
+  });
+});
