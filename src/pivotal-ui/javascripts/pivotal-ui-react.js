3103c38335b5693902f6d734fb157e30d4c0add2
Paul Meskers <pair+pmeskers@pivotal.io>
2014-12-17 11:04:30 -0500

    feat(react-search): Add a stateless SearchInput component.
    [#84034250]

--- src/pivotal-ui/javascripts/pivotal-ui-react.js
+++ src/pivotal-ui/javascripts/pivotal-ui-react.js
@@ -31,6 +31,8 @@ global.DangerButton = require('./buttons.jsx').DangerButton;
 global.HighlightButton = require('./buttons.jsx').HighlightButton;
 global.HighlightAltButton = require('./buttons.jsx').HighlightAltButton;
 
+global.SearchInput = require('./inputs.jsx').SearchInput;
+
 global.Divider = require('./dividers.jsx').Divider;
 global.DividerInverse = require('./dividers.jsx').DividerInverse;
 
