9ad17e6530f87153972a2154d52a44fa95a6fbad
Paul Meskers <pmeskers@pivotal.io>
2015-01-07 10:19:37 -0500

    feat(react-tabs): Refactor Tabs component
    * Replace SimpleAltTabs with Tabs
    
    [#85440704]
    
    Signed-off-by: Nicole Sullivan <nsullivan@pivotal.io>

--- src/pivotal-ui/javascripts/tabs.jsx
+++ src/pivotal-ui/javascripts/tabs.jsx
@@ -3,50 +3,71 @@
 var React = require('react');
 var _ = require('lodash');
 
-var SimpleAltTabs = React.createClass({
+var TabMenu = React.createClass({
   render: function () {
-    var activeIndex = _.findIndex(this.props.children, function (tab) {
-      return tab.props.active;
-    });
-
-    if (activeIndex === -1) {
-      activeIndex = 0;
-    }
-
-    var tabs = _.map(this.props.children, function(tab, index) {
-      var className = index === activeIndex ? 'active' : '';
-      var tabId = '#tab-' + index;
-
+    var tabs = _.map(this.props.tabs, function(tab, index) {
+      var className = (index === this.props.activeIndex) ? "active" : "";
+      var key = "tab-control-" + index;
       return (
-        <li key={'tab-head-' + index} className={className}>
-          <a data-toggle="tab" href={tabId}>{tab.props.heading}</a>
+        <li key={key} className={className}>
+          <a onClick={this.props.onTabClick} data-index={index}>{tab}</a>
         </li>
       );
-    });
-
-    var contents = _.map(this.props.children, function(tab, index) {
-      var classes = ['tab-pane', 'fade'];
-      var tabId = 'tab-' + index;
-      if (index === activeIndex) {
-        classes.push('in');
-        classes.push('active');
-      }
-
-      return (
-        <div key={'tab-content-' + index} id={tabId} className={classes.join(" ")}>
-          {tab.props.children}
-        </div>
-      );
-    });
+    }, this);
 
     return (
-      <div className="tab-simple-alt">
-        <ul className="nav nav-tabs">
-          {tabs}
-        </ul>
-        <div className="tab-content">
-          {contents}
+      <ul className="nav nav-tabs">
+        {tabs}
+      </ul>
+    );
+  }
+});
+
+var TabContents = React.createClass({
+  render: function () {
+    var panels = _.map(this.props.children, function(panel, index) {
+      var className = "tab-pane fade";
+      className += (this.props.activeIndex === index) ? " active in" : "";
+
+      var key = "tab-panel-" + index;
+
+      return (
+        <div key={key} className={className}>
+          {panel.props.children}
         </div>
+      );
+    }, this);
+
+    return (
+      <div className="tab-content">
+        {panels}
+      </div>
+    );
+  }
+});
+
+var Tabs = React.createClass({
+  getInitialState: function () {
+    return {
+      activeIndex: 0
+    };
+  },
+  componentWillMount: function () {
+    if (this.props.activeTab) {
+      this.setState({ activeIndex: this.props.activeTab });
+    }
+  },
+  updateTabs: function (event) {
+    var tabIndex = parseInt(event.target.getAttribute('data-index'));
+    this.setState({ activeIndex: tabIndex });
+  },
+  render: function () {
+    var tabLinks = _.pluck(_.pluck(this.props.children, 'props'), 'heading');
+
+    return (
+      <div className="tab-simple">
+        <TabMenu tabs={tabLinks} activeIndex={this.state.activeIndex} onTabClick={this.updateTabs}/>
+        <TabContents activeIndex={this.state.activeIndex}>{this.props.children}</TabContents>
       </div>
     );
   }
@@ -54,11 +75,15 @@ var SimpleAltTabs = React.createClass({
 
 var Tab = React.createClass({
   render: function () {
-    return "";
+    return (
+      <div>
+        {this.props.children}
+      </div>
+      );
   }
 });
 
 module.exports = {
-  SimpleAltTabs: SimpleAltTabs,
+  Tabs: Tabs,
   Tab: Tab
 };
--- test/spec/javascripts/tabs_spec.js
+++ test/spec/javascripts/tabs_spec.js
@@ -4,10 +4,10 @@ var $ = require('jquery');
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 
-var SimpleAltTabs = require('../../../src/pivotal-ui/javascripts/tabs.jsx').SimpleAltTabs;
+var Tabs = require('../../../src/pivotal-ui/javascripts/tabs.jsx').Tabs;
 var Tab = require('../../../src/pivotal-ui/javascripts/tabs.jsx').Tab;
 
-describe('SimpleAltTabs', function() {
+describe("Tabs", function() {
   beforeEach(function() {
     this.node = $('<div id="container"></div>').appendTo('body').get(0);
   });
@@ -17,42 +17,65 @@ describe('SimpleAltTabs', function() {
     document.body.removeChild(this.node);
   });
 
-  describe("when the active flag is not set", function() {
+  describe("when the active tab is not set", function() {
     beforeEach(function() {
       React.render(
-        <SimpleAltTabs>
+        <Tabs>
           <Tab heading="My first tab" >Content for first tab</Tab>
           <Tab heading="My second tab" >Content for second tab</Tab>
-        </SimpleAltTabs>,
+        </Tabs>,
         this.node
       );
     });
 
-    it("renders the correct tabs and content", function() {
-      expect($('#container .tab-simple-alt ul.nav.nav-tabs li.active a')).toHaveText("My first tab");
-      expect($('#container .tab-simple-alt ul.nav.nav-tabs li:not(.active) a')).toHaveText("My second tab");
+    it("renders the tabs and content with first tab active", function() {
+      expect($('#container .tab-simple ul.nav.nav-tabs li.active a')).toHaveText("My first tab");
+      expect($('#container .tab-simple ul.nav.nav-tabs li:not(.active) a')).toHaveText("My second tab");
 
-      expect($('#container li.active a').attr('href')).toEqual('#' + $('#container .tab-pane.active').attr("id"));
-
-      expect($('#container .tab-simple-alt .tab-content .tab-pane.in.active')).toHaveText("Content for first tab");
-      expect($('#container .tab-simple-alt .tab-content .tab-pane:not(.in.active)')).toHaveText("Content for second tab");
+      expect($('#container .tab-simple .tab-content .tab-pane.in.active')).toHaveText("Content for first tab");
+      expect($('#container .tab-simple .tab-content .tab-pane:not(.in.active)')).toHaveText("Content for second tab");
     });
   });
 
-  describe("when the active flag is set on a tab", function() {
+  describe("when the active tab is set", function() {
     beforeEach(function() {
       React.render(
-        <SimpleAltTabs>
+        <Tabs activeTab={1}>
           <Tab heading="My first tab" >Content for first tab</Tab>
-          <Tab heading="My second tab" active="true">Content for second tab</Tab>
-        </SimpleAltTabs>,
+          <Tab heading="My second tab" >Content for second tab</Tab>
+        </Tabs>,
         this.node
       );
     });
 
-    it("sets the specified tab as active", function() {
-      expect($('#container .tab-simple-alt ul.nav.nav-tabs li.active a')).toHaveText("My second tab");
-      expect($('#container .tab-simple-alt .tab-content .tab-pane.in.active')).toHaveText("Content for second tab");
+    it("renders the tabs and content with specified tab active", function() {
+      expect($('#container .tab-simple ul.nav.nav-tabs li.active a')).toHaveText("My second tab");
+      expect($('#container .tab-simple ul.nav.nav-tabs li:not(.active) a')).toHaveText("My first tab");
+
+      expect($('#container .tab-simple .tab-content .tab-pane.in.active')).toHaveText("Content for second tab");
+      expect($('#container .tab-simple .tab-content .tab-pane:not(.in.active)')).toHaveText("Content for first tab");
+    });
+  });
+
+  describe("clicking a non-active tab", function() {
+    beforeEach(function() {
+      React.render(
+        <Tabs>
+          <Tab heading="My first tab" >Content for first tab</Tab>
+          <Tab heading="My second tab" >Content for second tab</Tab>
+        </Tabs>,
+        this.node
+      );
+
+      TestUtils.Simulate.click($('li:not(.active) a').get(0));
+    });
+
+    it("updates the active tab and content", function() {
+      expect($('#container .tab-simple ul.nav.nav-tabs li.active a')).toHaveText("My second tab");
+      expect($('#container .tab-simple ul.nav.nav-tabs li:not(.active) a')).toHaveText("My first tab");
+
+      expect($('#container .tab-simple .tab-content .tab-pane.in.active')).toHaveText("Content for second tab");
+      expect($('#container .tab-simple .tab-content .tab-pane:not(.in.active)')).toHaveText("Content for first tab");
     });
   });
 });
