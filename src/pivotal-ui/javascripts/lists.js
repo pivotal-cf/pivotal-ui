ad5312a0cd56ec96547aea7c7478f1b8f1ee47ad
Paul Meskers <pmeskers@pivotal.io>
2015-01-29 09:33:02 -0500

    feat(react-lists): add list spacing
    [Finishes #86747994]
    
    Signed-off-by: Bebe Peng <bpeng@pivotal.io>

--- src/pivotal-ui/javascripts/lists.js
+++ src/pivotal-ui/javascripts/lists.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var _ = require('lodash');
 var React = require('react/addons');
+var ListMixin = require('./mixins/list-mixin');
 var setClass = React.addons.classSet;
 
 var ListItem = React.createClass({
+  mixins: [ListMixin],
+
   render: function() {
     return (
       <li {...this.props}>{this.props.children}</li>
@@ -13,65 +15,104 @@ var ListItem = React.createClass({
 });
 
 var UnorderedList = React.createClass({
+  mixins: [ListMixin],
+
   render: function() {
+    var { spacing, children } = this.props;
     var classes = setClass({
       'list-unordered': !this.props.unstyled && !this.props.checked,
       'list-unstyled': this.props.unstyled,
       'list-checked': this.props.checked
     });
+    if (spacing) { classes += ' lv' + spacing; }
+
     return (
-      <ul className={classes}>{this.props.children}</ul>
+      <ul className={classes}>{children}</ul>
     );
   }
 });
 
 var OrderedList = React.createClass({
+  mixins: [ListMixin],
+
   render: function() {
+    var { spacing, children } = this.props;
     var classes = setClass({
       'list-unstyled': this.props.unstyled
     });
+    if (spacing) { classes += ' lv' + spacing; }
+
     return (
-      <ol className={classes}>{this.props.children}</ol>
+      <ol className={classes}>{children}</ol>
     );
   }
 });
 
 var InlineList = React.createClass({
+  mixins: [ListMixin],
+
   render: function() {
+    var { spacing, children } = this.props;
     var classes = setClass({
       'list-inline': true,
       'list-inline-divider': this.props.divider,
     });
+    if (spacing) { classes += ' lh' + spacing; }
+
     return (
-      <ul className={classes}>{this.props.children}</ul>
+      <ul className={classes}>{children}</ul>
     );
   }
 });
 
 var GroupList = React.createClass({
+  mixins: [ListMixin],
+
   renderChildren: function() {
     return React.Children.map(this.props.children, c => React.addons.cloneWithProps(c, {className: 'list-group-item'}));
   },
 
   render: function() {
+    var { spacing } = this.props;
+    var classes = setClass({
+      'list-group': true,
+    });
+    if (spacing) { classes += ' lv' + spacing; }
+
     return (
-      <ul className='list-group'>{this.renderChildren()}</ul>
+      <ul className={classes}>{this.renderChildren()}</ul>
     );
   }
 });
 
 var StepList = React.createClass({
+  mixins: [ListMixin],
+
   render: function() {
+    var { spacing, children } = this.props;
+    var classes = setClass({
+      'list-steps': true,
+    });
+    if (spacing) { classes += ' lh' + spacing; }
+
     return (
-      <ol className='list-steps'>{this.props.children}</ol>
+      <ol className={classes}>{children}</ol>
     );
   }
 });
 
 var BreadcrumbList = React.createClass({
+  mixins: [ListMixin],
+
   render: function() {
+    var { spacing, children } = this.props;
+    var classes = setClass({
+      'list-breadcrumb': true
+    });
+    if (spacing) { classes += ' lh' + spacing; }
+
     return (
-      <ul className='list-breadcrumb'>{this.props.children}</ul>
+      <ul className={classes}>{children}</ul>
     );
   }
 });
