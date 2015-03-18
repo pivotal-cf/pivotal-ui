221562806d4c197b12fd0c6d4a6fc606f8898b7e
Nicole Sullivan <nsullivan@pivotal.io>
2015-03-18 15:43:59 -0700

    feat(group-list-inverse): a group list for dark backgrounds [#88958434]
    - also propogates classnames for all lists
    
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

--- src/pivotal-ui/javascripts/lists.js
+++ src/pivotal-ui/javascripts/lists.js
@@ -2,7 +2,7 @@
 
 var React = require('react/addons');
 var ListMixin = require('./mixins/list-mixin');
-var setClass = React.addons.classSet;
+var classnames = require('classnames');
 
 var ListItem = React.createClass({
   mixins: [ListMixin],
@@ -17,14 +17,15 @@ var ListItem = React.createClass({
 var UnorderedList = React.createClass({
   mixins: [ListMixin],
 
+  listAlignment: 'vertical',
+
   render: function() {
-    var { spacing, children } = this.props;
-    var classes = setClass({
+    var { children, className } = this.props;
+    var classes = classnames({
       'list-unordered': !this.props.unstyled && !this.props.checked,
       'list-unstyled': this.props.unstyled,
       'list-checked': this.props.checked
-    });
-    if (spacing) { classes += ' lv' + spacing; }
+    }, this.listSpacing(), className);
 
     return (
       <ul className={classes}>{children}</ul>
@@ -35,12 +36,13 @@ var UnorderedList = React.createClass({
 var OrderedList = React.createClass({
   mixins: [ListMixin],
 
+  listAlignment: 'vertical',
+
   render: function() {
-    var { spacing, children } = this.props;
-    var classes = setClass({
+    var { children, className } = this.props;
+    var classes = classnames({
       'list-unstyled': this.props.unstyled
-    });
-    if (spacing) { classes += ' lv' + spacing; }
+    }, this.listSpacing(), className);
 
     return (
       <ol className={classes}>{children}</ol>
@@ -51,17 +53,14 @@ var OrderedList = React.createClass({
 var InlineList = React.createClass({
   mixins: [ListMixin],
 
-  render: function() {
-    var { spacing, children } = this.props;
-    var classes = setClass({
-      'list-inline': true,
-      'list-inline-divider': this.props.divider,
-    });
-    if (spacing) { classes += ' lh' + spacing; }
+  listAlignment: 'horizontal',
 
-    if (this.props.className) {
-      classes += ' ' + this.props.className;
-    }
+  render: function() {
+    var { children, className } = this.props;
+    var classes = classnames({
+      'list-inline': true,
+      'list-inline-divider': this.props.divider
+    }, this.listSpacing(), className);
 
     return (
       <ul className={classes}>{children}</ul>
@@ -72,16 +71,38 @@ var InlineList = React.createClass({
 var GroupList = React.createClass({
   mixins: [ListMixin],
 
+  listAlignment: 'vertical',
+
   renderChildren: function() {
     return React.Children.map(this.props.children, c => React.addons.cloneWithProps(c, {className: 'list-group-item'}));
   },
 
   render: function() {
-    var { spacing } = this.props;
-    var classes = setClass({
-      'list-group': true,
-    });
-    if (spacing) { classes += ' lv' + spacing; }
+    var {className} = this.props;
+    var classes = classnames({
+      'list-group': true
+    }, this.listSpacing(), className);
+
+    return (
+      <ul className={classes}>{this.renderChildren()}</ul>
+    );
+  }
+});
+
+var GroupListInverse = React.createClass({
+  mixins: [ListMixin],
+
+  renderChildren: function() {
+    return React.Children.map(this.props.children, c => React.addons.cloneWithProps(c, {className: 'list-group-item'}));
+  },
+
+  listAlignment: 'vertical',
+
+  render: function() {
+    var {className} = this.props;
+    var classes = classnames({
+      'list-group-inverse': true
+    }, this.listSpacing(), className);
 
     return (
       <ul className={classes}>{this.renderChildren()}</ul>
@@ -92,12 +113,13 @@ var GroupList = React.createClass({
 var StepList = React.createClass({
   mixins: [ListMixin],
 
+  listAlignment: 'horizontal',
+
   render: function() {
-    var { spacing, children } = this.props;
-    var classes = setClass({
-      'list-steps': true,
-    });
-    if (spacing) { classes += ' lh' + spacing; }
+    var { children, className } = this.props;
+    var classes = classnames({
+      'list-steps': true
+    }, this.listSpacing(), className);
 
     return (
       <ol className={classes}>{children}</ol>
@@ -108,12 +130,13 @@ var StepList = React.createClass({
 var BreadcrumbList = React.createClass({
   mixins: [ListMixin],
 
+  listAlignment: 'horizontal',
+
   render: function() {
-    var { spacing, children } = this.props;
-    var classes = setClass({
+    var { children, className } = this.props;
+    var classes = classnames({
       'list-breadcrumb': true
-    });
-    if (spacing) { classes += ' lh' + spacing; }
+    }, this.listSpacing(), className);
 
     return (
       <ul className={classes}>{children}</ul>
@@ -127,6 +150,7 @@ module.exports = {
   OrderedList,
   InlineList,
   GroupList,
+  GroupListInverse,
   StepList,
   BreadcrumbList
 };
--- src/pivotal-ui/javascripts/mixins/list-mixin.js
+++ src/pivotal-ui/javascripts/mixins/list-mixin.js
@@ -5,6 +5,11 @@ var React = require('react');
 var ListMixin = {
   propTypes: {
     spacing: React.PropTypes.oneOf(['n', 's', 'm', 'l', 'xl'])
+  },
+
+  listSpacing: function() {
+    var {spacing} = this.props;
+    return spacing && `l${this.listAlignment === 'vertical' ? 'v' : 'h'}${spacing}`;
   }
 };
 
