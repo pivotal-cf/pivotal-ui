3c7a952e9c70c08bcdf37d85f04d8003e70321cd
Anand Gaitonde, Mike Kenyon and Paul Meskers <pair+agaitonde+mkenyon+pmeskers@pivotallabs.com>
2015-04-09 14:28:10 -0400

    feat(react-expander): Add Expander components to Pivotal UI.
    * This is a result of Pivotal Network story
    https://www.pivotaltracker.com/story/show/91556390

---
+++ src/pivotal-ui/javascripts/expander.jsx
@@ -0,0 +1,76 @@
+'use strict';
+
+var React = require('react');
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+var ExpanderContent = React.createClass({
+  propTypes: {
+    expanded: React.PropTypes.bool
+  },
+
+  getInitialState: function() {
+    return {
+      expanded: this.props.expanded
+    };
+  },
+
+  toggle: function() {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  },
+
+  render: function() {
+    var content;
+
+    if (this.state.expanded) {
+      content = (
+        <div key='expandedContent'>
+          {this.props.children}
+        </div>
+      );
+    }
+
+    return (
+      <ReactCSSTransitionGroup transitionName='expander'>
+        {content}
+      </ReactCSSTransitionGroup>
+    );
+  }
+});
+
+var ExpanderTrigger = React.createClass({
+  getInitialState: function () {
+    return { };
+  },
+
+  setTarget: function(target) {
+    this.setState({
+      target: target
+    });
+  },
+
+  render: function() {
+    var self = this;
+
+    function toggleExpander() {
+      if (self.state.target) {
+        self.state.target.toggle();
+      } else {
+        console.warn("No ExpanderContent provided to ExpanderTrigger.");
+      }
+    }
+
+    return (
+      <div onClick={toggleExpander}>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+
+module.exports = {
+  ExpanderContent: ExpanderContent,
+  ExpanderTrigger: ExpanderTrigger
+};
