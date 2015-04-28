5274508a0db1d332f79f2d53d8dff72f290de763
Charles Hansen <chansen@pivotal.io>
2015-04-28 09:23:18 -0700

    chore(packaging): Use npm packages for all react components
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

--- tasks/test.js
+++ tasks/test.js
@@ -4,39 +4,19 @@ var connect = require('gulp-connect');
 var errorHandler = require('./errorHandler.js');
 var fs = require('fs');
 var gulp = require('gulp');
-var karma = require('karma').server;
 var open = require('gulp-open');
 
 gulp.task('ci', [
   'lint',
   'assets',
-  '_rspec',
-  'karma'
+  '_rspec'
 ]);
 
 gulp.task('test', [
   'lint',
   '_cssCritic',
-  '_rspec',
-  'karma'
+  '_rspec'
 ]);
-
-gulp.task('karma', function (done) {
-  karma.start({
-    configFile: __dirname + '/../karma.config.js',
-    browsers: ['PhantomJS'],
-    singleRun: true
-  }, done);
-});
-
-gulp.task('karma-debug', function (done) {
-  karma.start({
-    configFile: __dirname + '/../karma.config.js',
-    browsers: ['Chrome'],
-    singleRun: false,
-    autoWatch: true
-  }, done);
-});
 
 gulp.task('_createTestFileList', ['assets'], function(done) {
   fs.readdir('./test/css/components/', function(err, files) {
