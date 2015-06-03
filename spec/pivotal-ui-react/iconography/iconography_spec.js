5e14a2344ed339f672560a8866b91cd6a25383b2
Geoff Pleiss and Matt Royal <gpleiss@pivotal.io>
2015-06-03 16:46:17 -0700

    feat(react-iconography): add size options to iconography
    [Finishes #96041146]

--- spec/pivotal-ui-react/iconography/iconography_spec.js
+++ spec/pivotal-ui-react/iconography/iconography_spec.js
@@ -1,16 +1,42 @@
 require('../spec_helper');
 
-describe('iconography', function() {
-  beforeEach(function() {
-    var Icon = require('../../../src/pivotal-ui-react/iconography/iconography').Icon;
-    React.render(<Icon name="plus"/>, root);
-  });
+var Icon = require('../../../src/pivotal-ui-react/iconography/iconography').Icon;
+var {toBeValid} = require('../support/matchers');
 
+describe('iconography', function() {
   afterEach(function() {
     React.unmountComponentAtNode(root);
   });
 
   it('works', function() {
+    React.render(<Icon name='plus'/>, root);
     expect('.fa.fa-plus').toExist();
+  });
+
+  describe('when a size is given', function() {
+    beforeEach(function() {
+      jasmine.addMatchers({toBeValid});
+    });
+
+    it('does not fail prop-validation', function() {
+      for (var size of ['title', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'sm', 'xs', 'lg', '2x', '3x', '4x', '5x']) {
+        expect(<Icon name='plus' size={size}/>, root).toBeValid();
+      }
+    });
+
+    it('adds the size class to the icon', function() {
+      for (var size of ['title', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'sm', 'xs', 'lg', '2x', '3x', '4x', '5x']) {
+        React.render(<Icon name='plus' size={size}/>, root);
+        expect('.fa.fa-plus').toHaveClass(`fa-${size}`);
+        React.unmountComponentAtNode(root);
+      }
+    });
+
+    describe('when a className and a size are given', function() {
+      it('adds the size class to the icon and includes the given className also', function() {
+        React.render(<Icon name='plus' className='a-class-name' size='h6'/>, root);
+        expect('.fa.fa-plus.a-class-name.fa-h6').toExist();
+      });
+    });
   });
 });
--- src/pivotal-ui-react/iconography/iconography.js
+++ src/pivotal-ui-react/iconography/iconography.js
@@ -1,15 +1,54 @@
+const React = require('react');
+const {PropTypes} = React;
+const ReactFaIcon = require('react-fa/dist/Icon');
+const objectAssign = require('object-assign');
+
+class Icon extends React.Component {
+  render() {
+    const {size, className, ...props} = this.props;
+
+    return (<ReactFaIcon className={`${className} fa-${size}`.trim()} {...props} />);
+  }
+}
+
+function satisfiesOneOf(...propTypes) {
+  return function(props, propName, componentName) {
+    var error;
+    var errorMessages = [];
+    for (var propType of propTypes) {
+      error = propType(props, propName, componentName);
+      if (!error) {
+        return null;
+      } else {
+        errorMessages.push(error.message);
+      }
+    }
+
+    return new Error(`Failed to satisfy any of the possible requirements:\n  ${errorMessages.join('\n  ')}`);
+  };
+}
+
+Icon.propTypes = objectAssign({}, ReactFaIcon.propTypes, {
+  size: satisfiesOneOf(
+    ReactFaIcon.propTypes.size,
+    PropTypes.oneOf([ 'title', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'sm', 'xs' ])
+  )
+});
+
 module.exports = {
   /**
    * @component Icon
    * @description Inserts a Font Awesome icon
    *
    * @property name {String} The name of the icon (without the fa- prefix)
+   * @property size {String} Can either be a PUI typography size (title, h1,
+   * h2, h3, h4, h5, h6, sm, xs) or a FA size (lg, 2x, 3x, 4x, 5x).
    *
    * @example ```js
    * var Icon = require('pui-react-iconography').Icon;
    * var MyComponent = React.createClass({
    *   render() {
-   *     return <Icon name="plus"/>;
+   *     return <Icon name="plus" size="h5"/>;
    *   }
    * });
    * ```
@@ -17,5 +56,5 @@ module.exports = {
    * @see [Pivotal UI React](http://styleguide.pivotal.io/react_beta.html#icon_react)
    * @see [Pivotal UI CSS](http://styleguide.pivotal.io/elements.html#iconography)
    */
-  Icon: require('react-fa/dist/Icon')
+  Icon: Icon
 };
