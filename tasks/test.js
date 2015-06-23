42cfa08d67ae8d771f517ce0a8c0cf104a2109b4
Geoff Pleiss and Matt Royal <mroyal@pivotal.io>
2015-06-23 16:51:56 -0700

    chore(ci): consolidate gulp test tasks

--- tasks/test.js
+++ tasks/test.js
@@ -2,9 +2,31 @@ import {exec} from 'child_process';
 import gulp from 'gulp';
 import runSequence from 'run-sequence';
 import loadPlugins from 'gulp-load-plugins';
+import webpack from 'webpack-stream';
 const plugins = loadPlugins();
 
-gulp.task('ci', callback => runSequence('jasmine-task-helpers', 'set-ci-port', 'rspec', 'lint', 'jasmine-ci', callback));
+gulp.task('ci', callback => runSequence(
+  'lint',
+  'jasmine-task-helpers',
+  'set-ci-port',
+  'rspec',
+  'jasmine-react-ci',
+  callback
+));
+
+gulp.task('lint', function() {
+  return gulp.src(['tasks/**/*.js', 'src/pivotal-ui-react/**/*.js', 'spec/pivotal-ui-react/**/*.js', 'spec/task-helpers/**/*.js'])
+    .pipe(plugins.plumber())
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format('stylish'))
+    .pipe(plugins.eslint.failOnError());
+});
+
+gulp.task('jasmine-task-helpers', function() {
+  return gulp.src(['spec/task-helpers/**/*-spec.js'])
+    .pipe(plugins.plumber())
+    .pipe(plugins.jasmine({includeStackTrace: true}));
+});
 
 gulp.task('set-ci-port', () => process.env.STYLEGUIDE_PORT = 9001);
 
@@ -19,8 +41,21 @@ gulp.task('rspec', ['monolith-serve'], function(done) {
   });
 });
 
-gulp.task('jasmine-task-helpers', function() {
-  return gulp.src(['spec/task-helpers/**/*-spec.js'])
+function reactTestAssets(options = {}) {
+  return gulp.src('spec/pivotal-ui-react/**/*_spec.js')
     .pipe(plugins.plumber())
-    .pipe(plugins.jasmine({includeStackTrace: true}));
+    .pipe(webpack(Object.assign(require('../config/webpack/test'), options)));
+}
+
+gulp.task('jasmine-react-ci', function() {
+  return reactTestAssets({watch: false})
+    .pipe(plugins.jasmineBrowser.specRunner({console: true}))
+    .pipe(plugins.jasmineBrowser.phantomjs());
+});
+
+gulp.task('jasmine-react', function() {
+  var plugin = new (require('gulp-jasmine-browser/webpack/jasmine-plugin'))();
+  return reactTestAssets({watch: true, plugins: [plugin]})
+    .pipe(plugins.jasmineBrowser.specRunner())
+    .pipe(plugins.jasmineBrowser.server({whenReady: plugin.whenReady}));
 });
