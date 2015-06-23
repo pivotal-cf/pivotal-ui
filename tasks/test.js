ae558dd5c647ede22213233d58bfcaaf82ec8136
Geoff Pleiss and Matt Royal <gpleiss@pivotal.io>
2015-06-23 16:32:30 -0700

    chore(ci): fix rspec test

--- tasks/test.js
+++ tasks/test.js
@@ -1,3 +1,4 @@
+import {exec} from 'child_process';
 import gulp from 'gulp';
 import runSequence from 'run-sequence';
 import loadPlugins from 'gulp-load-plugins';
@@ -8,10 +9,10 @@ gulp.task('ci', callback => runSequence('jasmine-task-helpers', 'set-ci-port', '
 gulp.task('set-ci-port', () => process.env.STYLEGUIDE_PORT = 9001);
 
 gulp.task('rspec', ['monolith-serve'], function(done) {
-  exec('rspec spec/features', function(exitStatus) {
-    if (exitStatus !== 0) {
+  exec('rspec spec/features', function(error) {
+    if (error) {
       console.error('Exiting: Specs Failed');
-      process.exit(exitStatus);
+      process.exit(error.code);
     }
     plugins.connect.serverClose();
     done();
