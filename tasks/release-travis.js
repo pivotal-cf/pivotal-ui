11761bccd453517f8705817b7cb70ac7d10cce9e
Geoff Pleiss and Matt Royal <gpleiss@pivotal.io>
2015-06-26 16:14:26 -0700

    chore(release): Update the release description when cutting a new release
    [#96495576]

--- tasks/release-travis.js
+++ tasks/release-travis.js
@@ -1,6 +1,7 @@
 import {map} from 'event-stream';
 import gulp from 'gulp';
 import {addReleaseNotesToLatest} from './helpers/github-service';
+import {log} from 'gulp-util';
 
 const plugins = require('gulp-load-plugins')();
 
@@ -12,9 +13,14 @@ gulp.task('release-zip', () => {
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('release-add-release-notes', () =>
-  gulp.src('LATEST_CHANGES.md')
+gulp.task('release-add-release-notes', () => {
+  if (!process.env.TRAVIS_TAG) {
+    log('Skipping - because we did not cut a new release');
+    return;
+  }
+  return gulp.src('LATEST_CHANGES.md')
     .pipe(map(async (latestChangesFile, callback) => {
+      log('Updating release notes...');
       try {
         await addReleaseNotesToLatest(latestChangesFile.contents.toString());
         callback();
@@ -23,5 +29,5 @@ gulp.task('release-add-release-notes', () =>
         console.error(error);
         callback(error);
       }
-    }))
-);
+    }));
+});
