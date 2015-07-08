ff8035e4394bc19ddc91819c4c709387961aeaeb
Geoff Pleiss, Kenny Wang and Matt Royal <mroyal@pivotal.io>
2015-07-08 14:17:11 -0700

    chore(feature-tests): run feature tests against dev server if test server isn't up

--- spec/features/spec_helper.rb
+++ spec/features/spec_helper.rb
@@ -1,11 +1,28 @@
 require 'rspec'
 require 'capybara/rspec'
 require 'selenium-webdriver'
+require 'net/http'
+require 'uri'
 require 'pry'
+
+TEST_STYLEGUIDE_HOST = 'http://localhost:9001'
+DEV_STYLEGUIDE_HOST = 'http://localhost:8000'
+
+def test_styleguide_running?
+  begin
+    uri = URI(TEST_STYLEGUIDE_HOST)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    true
+  rescue
+    false
+  end
+end
 
 Dir[File.dirname(__FILE__) + "/support/*.rb"].each { |file| require file }
 
-Capybara.app_host = 'http://localhost:9001'
+Capybara.app_host = test_styleguide_running? ? TEST_STYLEGUIDE_HOST : DEV_STYLEGUIDE_HOST
 Capybara.run_server = false
 Capybara.javascript_driver = :selenium
 
