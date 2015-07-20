64c5330ee3bcf132f09da3011ddb013b8df19d6a
Caroline Taymor and Matt Royal <mroyal@pivotal.io>
2015-07-20 15:02:08 -0700

    feat(tabs): applying className, id, and style to outer tab div
    [Finishes #98913444]

--- spec/pivotal-ui-react/tab/tab_spec.js
+++ spec/pivotal-ui-react/tab/tab_spec.js
@@ -1,4 +1,6 @@
 require('../spec_helper');
+import {itPropagatesAttributes} from '../support/shared_examples';
+
 describe('Tabs', function() {
   var Tab;
   beforeEach(function() {
@@ -10,8 +12,12 @@ describe('Tabs', function() {
       var SimpleTabs = require('../../../src/pivotal-ui-react/tabs/tabs').SimpleTabs;
       React.render(
         (
-          <SimpleTabs defaultActiveKey={1}>
-            <Tab eventKey={1} tab="Tab1">Content1</Tab>
+          <SimpleTabs
+            className="testSimpleTabsClass"
+            id="testId"
+            style={{opacity: '0.5'}}
+            defaultActiveKey={1}>
+            <Tab eventKey={1} className="tabPaneClass" tab="Tab1">Content1</Tab>
             <Tab eventKey={2} tab="Tab2"/>
           </SimpleTabs>
         ),
@@ -27,6 +33,18 @@ describe('Tabs', function() {
       expect('.tab-simple nav ul.nav.nav-tabs li.active').toContainText('Tab1');
       expect('.tab-simple .tab-content .tab-pane.fade.active.in').toContainText('Content1');
     });
+
+    itPropagatesAttributes('#root .tab-simple', {
+      className: 'testSimpleTabsClass',
+      id: 'testId',
+      style: {opacity: '0.5'}
+    });
+
+    describe('Tab', function() {
+      it('passes className to the tab-pane', function() {
+        expect('#root .tab-pane:nth(0)').toHaveClass('tabPaneClass');
+      });
+    });
   });
 
   describe('SimpleAltTabs', function() {
@@ -34,8 +52,12 @@ describe('Tabs', function() {
       var SimpleAltTabs = require('../../../src/pivotal-ui-react/tabs/tabs').SimpleAltTabs;
 
       React.render((
-          <SimpleAltTabs defaultActiveKey={1}>,
-            <Tab eventKey={1} tab="Tab1">Content1</Tab>
+          <SimpleAltTabs
+            className="testSimpleAltTabsClass"
+            id="testId"
+            style={{opacity: '0.5'}}
+            defaultActiveKey={1}>,
+            <Tab eventKey={1} className="tabPaneClass" tab="Tab1">Content1</Tab>
             <Tab eventKey={2} tab="Tab2"/>
           </SimpleAltTabs>
         ),
@@ -50,6 +72,18 @@ describe('Tabs', function() {
     it('creates tabs in a simpletab container', function() {
       expect('.tab-simple-alt nav ul.nav.nav-tabs li.active').toContainText('Tab1');
       expect('.tab-simple-alt .tab-content .tab-pane.fade.active.in').toContainText('Content1');
+    });
+
+    itPropagatesAttributes('#root .tab-simple-alt', {
+      className: 'testSimpleAltTabsClass',
+      id: 'testId',
+      style: {opacity: '0.5'}
+    });
+
+    describe('Tab', function() {
+      it('passes className to the tab-pane', function() {
+        expect('#root .tab-pane:nth(0)').toHaveClass('tabPaneClass');
+      });
     });
   });
 });
