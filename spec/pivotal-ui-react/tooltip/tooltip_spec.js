ae06ad125d0bfa8433843c54092b57e0c5408e43
Caroline Taymor and Geoff Pleiss <ctaymor@pivotal.io>
2015-07-21 10:07:15 -0700

    feat(tooltip): Adds className, id, and style passthrough
    - Upgrade to latest version of React Bootstrap
    
    [Finishes #98913250]

--- spec/pivotal-ui-react/tooltip/tooltip_spec.js
+++ spec/pivotal-ui-react/tooltip/tooltip_spec.js
@@ -1,9 +1,18 @@
 require('../spec_helper');
+import {itPropagatesAttributes} from '../support/shared_examples';
 
 describe('tooltip', function() {
+  var props = {
+    className: 'test-class',
+    id: 'test-id',
+    style: {
+      opacity: '0.5'
+    }
+  };
+
   beforeEach(function() {
     var Tooltip = require('../../../src/pivotal-ui-react/tooltip/tooltip').Tooltip;
-    React.render((<Tooltip><div className="tooltip-text">Hello World</div></Tooltip>), root);
+    React.render((<Tooltip {...props}><div className="tooltip-text">Hello World</div></Tooltip>), root);
   });
 
   afterEach(function() {
@@ -13,4 +22,6 @@ describe('tooltip', function() {
   it('works', function() {
     expect('.tooltip-text').toExist();
   });
+
+  itPropagatesAttributes('#root #test-id', props);
 });
