c140f97eeee0089c436211f5dc3168d3f03a4d32
Caroline Taymor <ctaymor@pivotal.io>
2015-08-07 17:45:52 -0700

    feat(tabs): Makes tabs responsive
    [Finishes #99800334]
    
    Signed-off-by: Caroline Taymor <ctaymor@pivotal.io>
    Signed-off-by: Geoff Pleiss <gpleiss@pivotal.io>

--- src/pivotal-ui-react/tabs/tabs.js
+++ src/pivotal-ui-react/tabs/tabs.js
@@ -1,11 +1,73 @@
-var React = require('react');
-var TabbedArea = require('react-bootstrap').TabbedArea;
+import classnames from 'classnames';
+import React from 'react';
+import TabbedArea from 'react-bootstrap/lib/TabbedArea';
+import Tab from 'react-bootstrap/lib/TabPane';
+import Accordion from 'react-bootstrap/lib/Accordion';
+import Panel from 'react-bootstrap/lib/Panel';
+
+const BaseTabs = React.createClass({
+  propTypes: {
+    defaultActiveKey: React.PropTypes.any,
+    tabType: React.PropTypes.oneOf(['tab-simple', 'tab-simple-alt']),
+    responsiveBreakpoint: React.PropTypes.oneOf(['xs', 'sm', 'md', 'lg']),
+    largeScreenClassName: React.PropTypes.string,
+    smallScreenClassName: React.PropTypes.string,
+    onSelect: React.PropTypes.function
+  },
+
+  getDefaultProps() {
+    return {
+      responsiveBreakpoint: 'xs'
+    };
+  },
+
+  getInitialState() {
+    return {activeKey: this.props.defaultActiveKey};
+  },
+
+  setActiveKey(key) {
+    this.setState({activeKey: key});
+  },
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.defaultActiveKey !== this.props.defaultActiveKey) {
+      this.setActiveKey(nextProps.defaultActiveKey);
+    }
+  },
+
+  handleSelect(key) {
+    if (!this.props.onSelect) {
+      this.setActiveKey(key);
+    } else {
+      this.props.onSelect(key);
+    }
+  },
+
+  render() {
+    const {defaultActiveKey, children, responsiveBreakpoint, tabType, largeScreenClassName, smallScreenClassName, onSelect, ...props} = this.props;
+    const largeScreenClasses = classnames([`hidden-${responsiveBreakpoint}`, tabType, largeScreenClassName]);
+    const smallScreenClasses = classnames([`visible-${responsiveBreakpoint}-block`, `${tabType}-small-screen`, smallScreenClassName]);
+    return (
+      <div {...props}>
+        <div className={largeScreenClasses}>
+          <TabbedArea activeKey={this.state.activeKey} onSelect={this.handleSelect}>
+            {children}
+          </TabbedArea>
+        </div>
+        <Accordion className={smallScreenClasses} activeKey={this.state.activeKey} onSelect={this.handleSelect}>
+          {React.Children.map(children, (child) => {
+            const {tab, ...childProps} = child.props;
+            return <Panel header={tab} {...childProps}/>;
+          })}
+        </Accordion>
+      </div>
+    );
+  }
+});
 
 /**
  * @component SimpleTabs
  * @description Tab-based content toggling
- *
- * @property defaultActiveKey {Number} Indicates which `<Tab>` to show by default
  *
  * @example ```js
  * var SimpleTabs = require('pui-react-tabs').SimpleTabs;
@@ -23,20 +85,37 @@ var TabbedArea = require('react-bootstrap').TabbedArea;
  * });
  * ```
  *
+ * @property responsiveBreakpoint {one of: `"xs"`, `"sm"`, `"md"`, `lg`} The
+ * size at which the small-screen tabs (accordion-style) should switch to
+ * large-screen tabs (folder-style)
+ *
+ * @property defaultActiveKey {should equal one of your tab's event keys} The
+ * tab which will start out open
+ *
+ * @property smallScreenClassName {css class} Will be applied to small screen
+ * tabs only
+ *
+ * @property largeScreenClassName {css class} Will be applied to large screen
+ * tabs only
+ *
+ * @property onSelect {function} Will override default behavior when clicking
+ * on a tab. If you want to retain the default behavior as well as add new
+ * functionality, change default active key in the function you provide
+ *
  * @see [Pivotal UI React](http://styleguide.pivotal.io/react.html#tabs_react)
  * @see [Pivotal UI CSS](http://styleguide.pivotal.io/objects.html#tab)
  */
-var SimpleTabs = React.createClass({
+const SimpleTabs = React.createClass({
   render() {
-    return <div className="tab-simple"><TabbedArea {...this.props}/></div>;
+    return (
+      <BaseTabs {...this.props} tabType="tab-simple"/>
+    );
   }
 });
 
 /**
  * @component SimpleAltTabs
  * @description `<SimpleTabs>` with different styling
- *
- * @property defaultActiveKey {Number} Indicates which tab to show by default
  *
  * @example ```js
  * var SimpleAltTabs = require('pui-react-tabs').SimpleAltTabs;
@@ -54,12 +133,31 @@ var SimpleTabs = React.createClass({
  * });
  * ```
  *
+ * @property responsiveBreakpoint {one of: `"xs"`, `"sm"`, `"md"`, `lg`} The
+ * size at which the small-screen tabs (accordion-style) should switch to
+ * large-screen tabs (folder-style)
+ *
+ * @property defaultActiveKey {should equal one of your tab's event keys} The
+ * tab which will start out open
+ *
+ * @property smallScreenClassName {css class} Will be applied to small screen
+ * tabs only
+ *
+ * @property largeScreenClassName {css class} Will be applied to large screen
+ * tabs only
+ *
+ * @property onSelect {function} Will override default behavior when clicking
+ * on a tab. If you want to retain the default behavior as well as add new
+ * functionality, change default active key in the function you provide
+ *
  * @see [Pivotal UI React](http://styleguide.pivotal.io/react.html#tabs_react)
  * @see [Pivotal UI CSS](http://styleguide.pivotal.io/objects.html#tab)
  */
 var SimpleAltTabs = React.createClass({
   render() {
-    return <div className="tab-simple-alt"><TabbedArea {...this.props}/></div>;
+    return (
+      <BaseTabs {...this.props} tabType="tab-simple-alt"/>
+    );
   }
 });
 
@@ -72,7 +170,6 @@ var SimpleAltTabs = React.createClass({
  * @see [Pivotal UI React](http://styleguide.pivotal.io/react.html#tabs_react)
  * @see [Pivotal UI CSS](http://styleguide.pivotal.io/objects.html#tab)
  */
-var Tab = require('react-bootstrap').TabPane;
 
 module.exports = {
   SimpleTabs,
