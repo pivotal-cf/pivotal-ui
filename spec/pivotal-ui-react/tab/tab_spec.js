721cdbf5efe02c9acb775923ca064e4bee4a27bc
Caroline Taymor <ctaymor@pivotal.io>
2015-08-07 17:45:52 -0700

    feat(tabs): Makes tabs responsive
    [Finishes #99800334]
    
    Signed-off-by: Caroline Taymor <ctaymor@pivotal.io>
    Signed-off-by: Geoff Pleiss <gpleiss@pivotal.io>

--- spec/pivotal-ui-react/tab/tab_spec.js
+++ spec/pivotal-ui-react/tab/tab_spec.js
@@ -1,148 +1,160 @@
 require('../spec_helper');
+import {SimpleAltTabs, SimpleTabs, Tab} from '../../../src/pivotal-ui-react/tabs/tabs';
+import EventEmitter from 'node-event-emitter';
+import {itPropagatesAttributes} from '../support/shared_examples';
+
 describe('Tabs', function() {
-  var Tab;
-  beforeEach(function() {
-    Tab = require('../../../src/pivotal-ui-react/tabs/tabs').Tab;
+  afterEach(function() {
+    React.unmountComponentAtNode(root);
   });
 
-  describe('SimpleTabs', function() {
-    let SimpleTabs = require('../../../src/pivotal-ui-react/tabs/tabs').SimpleTabs;
+  function itBehavesLikeTabs(Component, tabType) {
+    let emitter;
 
-    beforeEach(function() {
-      React.render(
-        (
-          <SimpleTabs defaultActiveKey={1}>
+    describe('props', function() {
+      let onSelectSpy;
+
+      beforeEach(function() {
+        onSelectSpy = jasmine.createSpy('onSelectSpy');
+
+        React.render(
+          <Component defaultActiveKey={2} className="test-class" id="test-id" style={{opacity: 0.5}} responsiveBreakpoint="md" smallScreenClassName="small-class" largeScreenClassName="large-class" onSelect={onSelectSpy}>
             <Tab eventKey={1} title="Tab1">Content1</Tab>
-            <Tab eventKey={2} title="Tab2"/>
-          </SimpleTabs>
-        ),
-        root
-      );
-    });
-
-    afterEach(function() {
-      React.unmountComponentAtNode(root);
-    });
-
-    it('creates tabs in a simpletab container', function() {
-      expect('.tab-simple nav ul.nav.nav-tabs li.active').toContainText('Tab1');
-      expect('.tab-simple .tab-content .tab-pane.fade.active.in').toContainText('Content1');
-    });
-
-    describe('when no ID is given to the Tabs component', function() {
-      beforeEach(function() {
-        React.render(
-          (
-            <SimpleTabs defaultActiveKey={1}>
-              <Tab eventKey={1} title="Tab1">Content1</Tab>
-              <Tab eventKey={2} title="Tab2"/>
-            </SimpleTabs>
-          ),
-          root
-        );
+            <Tab eventKey={2} title="Tab2">Content2</Tab>
+          </Component>,
+          root);
       });
 
-      it('sets up the correct aria-controls relationship', function() {
-        let pane1 = $(root).find('.tab-simple .tab-pane:first');
-        expect(pane1.length).toEqual(1);
-        expect(pane1.attr('id')).toBeTruthy();
-        expect('.tab-simple nav ul.nav.nav-tabs li:first a').toHaveAttr('aria-controls', pane1.attr('id'));
+      afterEach(function() {
+        React.unmountComponentAtNode(root);
+      });
+
+      itPropagatesAttributes('#root > div', {className: 'test-class', id: 'test-id', style: {opacity: '0.5'}});
+
+      it('sets the responsive breakpoint', function() {
+        expect(`.${tabType}`).toHaveClass('hidden-md');
+        expect('.panel-group').toHaveClass('visible-md-block');
+      });
+
+      it('passes screen-size specific classes', function() {
+        expect(`.${tabType}`).toHaveClass('large-class');
+        expect('.panel-group').toHaveClass('small-class');
+      });
+
+      it('uses the supplied onSelect method when clicking on large-screen tabs', function() {
+        $(`.${tabType} li a:eq(0)`).simulate('click');
+        expect(onSelectSpy).toHaveBeenCalled();
+      });
+
+      it('uses the supplied onSelect method when clicking on small-screen tabs', function() {
+        $(`.panel-group .panel-title a:eq(0)`).simulate('click');
+        expect(onSelectSpy).toHaveBeenCalled();
+      });
+
+      describe('when an ID is given to the Tabs component', function() {
+        it('sets up the correct aria-controls relationship', function() {
+          let pane1 = $(root).find(`.${tabType} .tab-pane:first`);
+          expect(pane1.length).toEqual(1);
+          expect(pane1.attr('id')).toBeTruthy();
+          expect(`.${tabType} nav ul.nav.nav-tabs li:first a`).toHaveAttr('aria-controls', pane1.attr('id'));
+        });
+      });
+
+      describe('default behavior', function() {
+        beforeEach(function() {
+          emitter = new EventEmitter();
+
+          const TestComponent = React.createClass({
+            getInitialState() {
+              return {defaultActiveKey: 2};
+            },
+
+            componentDidMount() {
+              emitter.on('changeActiveKey', (key) => this.setState({defaultActiveKey: key}));
+            },
+
+            render() {
+              return (
+                <Component defaultActiveKey={this.state.defaultActiveKey}>
+                  <Tab eventKey={1} title="Tab1">Content1</Tab>
+                  <Tab eventKey={2} title="Tab2">Content2</Tab>
+                </Component>
+              );
+            }
+          });
+
+          React.render(<TestComponent />, root);
+        });
+
+        it('creates tabs in the correct container', function() {
+          expect(`.${tabType} nav ul.nav.nav-tabs li.active`).toContainText('Tab2');
+          expect(`.${tabType} .tab-content .tab-pane.fade.active.in`).toContainText('Content2');
+        });
+
+        describe('for screens greater than the responsiveBreakpoint', function() {
+          it('displays tabs in a simple tab container', function() {
+            expect(`.hidden-xs.${tabType} nav li.active`).toContainText('Tab2');
+            expect(`.hidden-xs.${tabType} .tab-content`).toContainText('Content2');
+          });
+        });
+
+        describe('for screens smaller than the responsiveBreakpoint', function() {
+          it('renders an accordion', function() {
+            expect('.visible-xs-block.panel-group').toExist();
+          });
+
+          it('renders headers for each tab', function() {
+            expect('.visible-xs-block.panel-group .panel-title:eq(0)').toContainText('Tab1');
+            expect('.visible-xs-block.panel-group .panel-title:eq(1)').toContainText('Tab2');
+            expect('.visible-xs-block.panel-group .panel-title a:eq(1)').toHaveAttr('aria-expanded', 'true');
+          });
+
+          it('renders content for each tab', function() {
+            expect('.visible-xs-block.panel-group .panel-collapse:eq(0)').toContainText('Content1');
+            expect('.visible-xs-block.panel-group .panel-collapse:eq(1)').toContainText('Content2');
+            expect('.visible-xs-block.panel-group .panel-collapse:eq(1)').toHaveClass('in');
+          });
+        });
+
+        describe('when switching tabs', function() {
+          it('switches tabs in both small-screen and large-screen tabs', function() {
+            $('.hidden-xs li:eq(0) a').simulate('click');
+            expect('.hidden-xs li.active').toContainText('Tab1');
+            expect('.visible-xs-block .panel-title a[aria-expanded=true]').toContainText('Tab1');
+            $('.visible-xs-block .panel-title:eq(1) a').simulate('click');
+            expect('.hidden-xs li.active').toContainText('Tab2');
+            expect('.visible-xs-block .panel-title a[aria-expanded=true]').toContainText('Tab2');
+          });
+        });
+
+        describe('changing the defaultActiveKey props', function() {
+          beforeEach(function() {
+            emitter.emit('changeActiveKey', 1);
+          });
+
+          it('updates the current open tab', function() {
+            expect('.hidden-xs li.active').toContainText('Tab1');
+            expect('.visible-xs-block .panel-title a[aria-expanded=true]').toContainText('Tab1');
+          });
+        });
+
+        describe('when an id is not given to the Tabs component', function() {
+          it('sets up the correct aria-controls relationship', function() {
+            let pane1 = $(root).find(`.${tabType} .tab-pane:first`);
+            expect(pane1.length).toEqual(1);
+            expect(pane1.attr('id')).toBeTruthy();
+            expect(`.${tabType} nav ul.nav.nav-tabs li:first a`).toHaveAttr('aria-controls', pane1.attr('id'));
+          });
+        });
       });
     });
+  }
 
-    describe('when an ID is given to the Tabs component', function() {
-      beforeEach(function() {
-        React.render(
-          (
-            <SimpleTabs defaultActiveKey={1} id="tabs-id" >
-              <Tab eventKey={1} title="Tab1">Content1</Tab>
-              <Tab eventKey={2} title="Tab2"/>
-            </SimpleTabs>
-          ),
-          root
-        );
-      });
-
-      it('uses the given id for the tabs element in the DOM', function() {
-        expect('.tab-simple > #tabs-id').toExist();
-      });
-
-      it('sets up the correct aria-controls relationship', function() {
-        let pane1 = $(root).find('.tab-simple .tab-pane:first');
-        expect(pane1.length).toEqual(1);
-        expect(pane1.attr('id')).toBeTruthy();
-        expect('.tab-simple nav ul.nav.nav-tabs li:first a').toHaveAttr('aria-controls', pane1.attr('id'));
-      });
-    });
+  describe('SimpleTabs', function() {
+    itBehavesLikeTabs(SimpleTabs, 'tab-simple');
   });
 
   describe('SimpleAltTabs', function() {
-    let SimpleAltTabs = require('../../../src/pivotal-ui-react/tabs/tabs').SimpleAltTabs;
-
-    beforeEach(function() {
-      React.render((
-          <SimpleAltTabs defaultActiveKey={1}>,
-            <Tab eventKey={1} title="Tab1">Content1</Tab>
-            <Tab eventKey={2} title="Tab2"/>
-          </SimpleAltTabs>
-        ),
-        root
-      );
-    });
-
-    afterEach(function() {
-      React.unmountComponentAtNode(root);
-    });
-
-    it('creates tabs in a simpletab container', function() {
-      expect('.tab-simple-alt nav ul.nav.nav-tabs li.active').toContainText('Tab1');
-      expect('.tab-simple-alt .tab-content .tab-pane.fade.active.in').toContainText('Content1');
-    });
-
-    describe('when no ID is given to the Tabs component', function() {
-      beforeEach(function() {
-        React.render(
-          (
-            <SimpleAltTabs defaultActiveKey={1}>
-              <Tab eventKey={1} title="Tab1">Content1</Tab>
-              <Tab eventKey={2} title="Tab2"/>
-            </SimpleAltTabs>
-          ),
-          root
-        );
-      });
-
-      it('sets up the correct aria-controls relationship', function() {
-        let pane1 = $(root).find('.tab-simple-alt .tab-pane:first');
-        expect(pane1.length).toEqual(1);
-        expect(pane1.attr('id')).toBeTruthy();
-        expect('.tab-simple-alt nav ul.nav.nav-tabs li:first a').toHaveAttr('aria-controls', pane1.attr('id'));
-      });
-    });
-
-    describe('when an ID is given to the Tabs component', function() {
-      beforeEach(function() {
-        React.render(
-          (
-            <SimpleAltTabs defaultActiveKey={1} id="tabs-id" >
-              <Tab eventKey={1} title="Tab1">Content1</Tab>
-              <Tab eventKey={2} title="Tab2"/>
-            </SimpleAltTabs>
-          ),
-          root
-        );
-      });
-
-      it('uses the given id for the tabs element in the DOM', function() {
-        expect('.tab-simple-alt > #tabs-id').toExist();
-      });
-
-      it('sets up the correct aria-controls relationship', function() {
-        let pane1 = $(root).find('.tab-simple-alt .tab-pane:first');
-        expect(pane1.length).toEqual(1);
-        expect(pane1.attr('id')).toBeTruthy();
-        expect('.tab-simple-alt nav ul.nav.nav-tabs li:first a').toHaveAttr('aria-controls', pane1.attr('id'));
-      });
-    });
+    itBehavesLikeTabs(SimpleAltTabs, 'tab-simple-alt');
   });
 });
--- src/pivotal-ui-react/tabs/tabs.js
+++ src/pivotal-ui-react/tabs/tabs.js
@@ -1,14 +1,77 @@
 import React from 'react';
 import Tabs from 'react-bootstrap/lib/Tabs';
 import BsTab from 'react-bootstrap/lib/Tab';
+import Accordion from 'react-bootstrap/lib/Accordion';
+import Panel from 'react-bootstrap/lib/Panel';
 import uniqueid from 'lodash.uniqueid';
+import classnames from 'classnames';
+
+
+const BaseTabs = React.createClass({
+  propTypes: {
+    defaultActiveKey: React.PropTypes.any,
+    tabType: React.PropTypes.oneOf(['tab-simple', 'tab-simple-alt']),
+    responsiveBreakpoint: React.PropTypes.oneOf(['xs', 'sm', 'md', 'lg']),
+    largeScreenClassName: React.PropTypes.string,
+    smallScreenClassName: React.PropTypes.string,
+    onSelect: React.PropTypes.func
+  },
+
+  getDefaultProps() {
+    return {
+      responsiveBreakpoint: 'xs'
+    };
+  },
+
+  getInitialState() {
+    return {activeKey: this.props.defaultActiveKey};
+  },
+
+  setActiveKey(key) {
+    this.setState({activeKey: key});
+  },
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.defaultActiveKey !== this.props.defaultActiveKey) {
+      this.setActiveKey(nextProps.defaultActiveKey);
+    }
+  },
+
+  handleSelect(key) {
+    if (!this.props.onSelect) {
+      this.setActiveKey(key);
+    } else {
+      this.props.onSelect(key);
+    }
+  },
+
+  render() {
+    const {defaultActiveKey, children, responsiveBreakpoint, tabType, largeScreenClassName, smallScreenClassName, onSelect, ...props} = this.props;
+    const largeScreenClasses = classnames([`hidden-${responsiveBreakpoint}`, tabType, largeScreenClassName]);
+    const smallScreenClasses = classnames([`visible-${responsiveBreakpoint}-block`, `${tabType}-small-screen`, smallScreenClassName]);
+    return (
+      <div {...props}>
+        <div className={largeScreenClasses}>
+          <Tabs id={uniqueid('pui-react-tabs-')} activeKey={this.state.activeKey} onSelect={this.handleSelect}>
+            {children}
+          </Tabs>
+        </div>
+        <Accordion className={smallScreenClasses} activeKey={this.state.activeKey} onSelect={this.handleSelect}>
+          {React.Children.map(children, (child) => {
+            const {title, ...childProps} = child.props;
+            return <Panel header={title} {...childProps}/>;
+          })}
+        </Accordion>
+      </div>
+    );
+  }
+});
 
 /**
  * @component SimpleTabs
  * @description Tab-based content toggling
  *
- * @property defaultActiveKey {Number} Indicates which `<Tab>` to show by default
- *
+
  * @example ```js
  * var SimpleTabs = require('pui-react-tabs').SimpleTabs;
  * var Tab = require('pui-react-tabs').Tab;
@@ -25,20 +88,37 @@ import uniqueid from 'lodash.uniqueid';
  * });
  * ```
  *
+ * @property responsiveBreakpoint {one of: `"xs"`, `"sm"`, `"md"`, `lg`} The
+ * size at which the small-screen tabs (accordion-style) should switch to
+ * large-screen tabs (folder-style)
+ *
+ * @property defaultActiveKey {should equal one of your tab's event keys} The
+ * tab which will start out open
+ *
+ * @property smallScreenClassName {css class} Will be applied to small screen
+ * tabs only
+ *
+ * @property largeScreenClassName {css class} Will be applied to large screen
+ * tabs only
+ *
+ * @property onSelect {function} Will override default behavior when clicking
+ * on a tab. If you want to retain the default behavior as well as add new
+ * functionality, change default active key in the function you provide
+ *
  * @see [Pivotal UI React](http://styleguide.pivotal.io/react.html#tabs_react)
  * @see [Pivotal UI CSS](http://styleguide.pivotal.io/objects.html#tab)
  */
-let SimpleTabs = React.createClass({
+const SimpleTabs = React.createClass({
   render() {
-    return <div className="tab-simple"><Tabs id={uniqueid('pui-react-tabs-')} {...this.props}/></div>;
+    return (
+      <BaseTabs {...this.props} tabType="tab-simple"/>
+    );
   }
 });
 
 /**
  * @component SimpleAltTabs
  * @description `<SimpleTabs>` with different styling
- *
- * @property defaultActiveKey {Number} Indicates which tab to show by default
  *
  * @example ```js
  * var SimpleAltTabs = require('pui-react-tabs').SimpleAltTabs;
@@ -56,12 +136,31 @@ let SimpleTabs = React.createClass({
  * });
  * ```
  *
+ * @property responsiveBreakpoint {one of: `"xs"`, `"sm"`, `"md"`, `lg`} The
+ * size at which the small-screen tabs (accordion-style) should switch to
+ * large-screen tabs (folder-style)
+ *
+ * @property defaultActiveKey {should equal one of your tab's event keys} The
+ * tab which will start out open
+ *
+ * @property smallScreenClassName {css class} Will be applied to small screen
+ * tabs only
+ *
+ * @property largeScreenClassName {css class} Will be applied to large screen
+ * tabs only
+ *
+ * @property onSelect {function} Will override default behavior when clicking
+ * on a tab. If you want to retain the default behavior as well as add new
+ * functionality, change default active key in the function you provide
+ *
  * @see [Pivotal UI React](http://styleguide.pivotal.io/react.html#tabs_react)
  * @see [Pivotal UI CSS](http://styleguide.pivotal.io/objects.html#tab)
  */
-let SimpleAltTabs = React.createClass({
+const SimpleAltTabs = React.createClass({
   render() {
-    return <div className="tab-simple-alt"><Tabs id={uniqueid('pui-react-tabs-')} {...this.props}/></div>;
+    return (
+      <BaseTabs {...this.props} tabType="tab-simple-alt"/>
+    );
   }
 });
 
