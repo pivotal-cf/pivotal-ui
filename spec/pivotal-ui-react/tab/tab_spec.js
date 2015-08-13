75fd100b7c87f97158cf55f0923ce2a90ce4075e
Matt Royal <mroyal@pivotal.io>
2015-08-13 14:32:10 -0700

    feat(tabs): Set correct aria-controls value for Tab nav items
    Signed-off-by: Dominick Reinhold <dreinhold@pivotal.io>

--- spec/pivotal-ui-react/tab/tab_spec.js
+++ spec/pivotal-ui-react/tab/tab_spec.js
@@ -6,8 +6,9 @@ describe('Tabs', function() {
   });
 
   describe('SimpleTabs', function() {
+    let SimpleTabs = require('../../../src/pivotal-ui-react/tabs/tabs').SimpleTabs;
+
     beforeEach(function() {
-      var SimpleTabs = require('../../../src/pivotal-ui-react/tabs/tabs').SimpleTabs;
       React.render(
         (
           <SimpleTabs defaultActiveKey={1}>
@@ -27,12 +28,58 @@ describe('Tabs', function() {
       expect('.tab-simple nav ul.nav.nav-tabs li.active').toContainText('Tab1');
       expect('.tab-simple .tab-content .tab-pane.fade.active.in').toContainText('Content1');
     });
+
+    describe('when no ID is given to the Tabs component', function() {
+      beforeEach(function() {
+        React.render(
+          (
+            <SimpleTabs defaultActiveKey={1}>
+              <Tab eventKey={1} title="Tab1">Content1</Tab>
+              <Tab eventKey={2} title="Tab2"/>
+            </SimpleTabs>
+          ),
+          root
+        );
+      });
+
+      it('sets up the correct aria-controls relationship', function() {
+        let pane1 = $(root).find('.tab-simple .tab-pane:first');
+        expect(pane1.length).toEqual(1);
+        expect(pane1.attr('id')).toBeTruthy();
+        expect('.tab-simple nav ul.nav.nav-tabs li:first a').toHaveAttr('aria-controls', pane1.attr('id'));
+      });
+    });
+
+    describe('when an ID is given to the Tabs component', function() {
+      beforeEach(function() {
+        React.render(
+          (
+            <SimpleTabs defaultActiveKey={1} id="tabs-id" >
+              <Tab eventKey={1} title="Tab1">Content1</Tab>
+              <Tab eventKey={2} title="Tab2"/>
+            </SimpleTabs>
+          ),
+          root
+        );
+      });
+
+      it('uses the given id for the tabs element in the DOM', function() {
+        expect('.tab-simple > #tabs-id').toExist();
+      });
+
+      it('sets up the correct aria-controls relationship', function() {
+        let pane1 = $(root).find('.tab-simple .tab-pane:first');
+        expect(pane1.length).toEqual(1);
+        expect(pane1.attr('id')).toBeTruthy();
+        expect('.tab-simple nav ul.nav.nav-tabs li:first a').toHaveAttr('aria-controls', pane1.attr('id'));
+      });
+    });
   });
 
   describe('SimpleAltTabs', function() {
-    beforeEach(function() {
-      var SimpleAltTabs = require('../../../src/pivotal-ui-react/tabs/tabs').SimpleAltTabs;
+    let SimpleAltTabs = require('../../../src/pivotal-ui-react/tabs/tabs').SimpleAltTabs;
 
+    beforeEach(function() {
       React.render((
           <SimpleAltTabs defaultActiveKey={1}>,
             <Tab eventKey={1} title="Tab1">Content1</Tab>
@@ -50,6 +97,52 @@ describe('Tabs', function() {
     it('creates tabs in a simpletab container', function() {
       expect('.tab-simple-alt nav ul.nav.nav-tabs li.active').toContainText('Tab1');
       expect('.tab-simple-alt .tab-content .tab-pane.fade.active.in').toContainText('Content1');
+    });
+
+    describe('when no ID is given to the Tabs component', function() {
+      beforeEach(function() {
+        React.render(
+          (
+            <SimpleAltTabs defaultActiveKey={1}>
+              <Tab eventKey={1} title="Tab1">Content1</Tab>
+              <Tab eventKey={2} title="Tab2"/>
+            </SimpleAltTabs>
+          ),
+          root
+        );
+      });
+
+      it('sets up the correct aria-controls relationship', function() {
+        let pane1 = $(root).find('.tab-simple-alt .tab-pane:first');
+        expect(pane1.length).toEqual(1);
+        expect(pane1.attr('id')).toBeTruthy();
+        expect('.tab-simple-alt nav ul.nav.nav-tabs li:first a').toHaveAttr('aria-controls', pane1.attr('id'));
+      });
+    });
+
+    describe('when an ID is given to the Tabs component', function() {
+      beforeEach(function() {
+        React.render(
+          (
+            <SimpleAltTabs defaultActiveKey={1} id="tabs-id" >
+              <Tab eventKey={1} title="Tab1">Content1</Tab>
+              <Tab eventKey={2} title="Tab2"/>
+            </SimpleAltTabs>
+          ),
+          root
+        );
+      });
+
+      it('uses the given id for the tabs element in the DOM', function() {
+        expect('.tab-simple-alt > #tabs-id').toExist();
+      });
+
+      it('sets up the correct aria-controls relationship', function() {
+        let pane1 = $(root).find('.tab-simple-alt .tab-pane:first');
+        expect(pane1.length).toEqual(1);
+        expect(pane1.attr('id')).toBeTruthy();
+        expect('.tab-simple-alt nav ul.nav.nav-tabs li:first a').toHaveAttr('aria-controls', pane1.attr('id'));
+      });
     });
   });
 });
