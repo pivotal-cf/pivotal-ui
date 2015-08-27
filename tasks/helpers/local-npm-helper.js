71c74947996c504343c02c878cbb550d3b0fa813
August Toman-Yih <atomanyih@pivotal.io>
2015-08-27 11:49:46 -0700

    chore(acceptance): Makes sinopia setup more reproduceable
    [#101438070]
    
    Signed-off-by: Dominick Reinhold <dreinhold@pivotal.io>

--- tasks/helpers/local-npm-helper.js
+++ tasks/helpers/local-npm-helper.js
@@ -1,14 +1,18 @@
-import {exec} from 'child_process';
 import promisify from 'es6-promisify';
 import {log} from 'gulp-util';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
+import rimraf from 'rimraf';
 
-const SINOPIA_STORAGE_LOCATION = '~/.local/share/sinopia/storage';
 const SINOPIA_SERVER_URL = 'http://localhost:4873/';
 
 module.exports = {
   registryUrl: SINOPIA_SERVER_URL,
   async clean() {
-    log('Cleaning Sinopia storage at', SINOPIA_STORAGE_LOCATION);
-    await promisify(exec)(`rm -rf ${SINOPIA_STORAGE_LOCATION}`);
+    const config = yaml.safeLoad(fs.readFileSync('./.sinopia/config.yaml', 'utf8'));
+    const storageLocation = path.join('.','.sinopia', config.storage);
+    log('Cleaning Sinopia storage at', storageLocation);
+    await promisify(rimraf)(storageLocation);
   }
 };
