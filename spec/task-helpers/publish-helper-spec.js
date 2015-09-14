c6d341e3ff5e97828910e48aaeab782cfc97eb53
Claire Thompson <cthompson@pivotal.io>
2015-09-14 16:18:56 -0700

    chore(test): backfill tests for infoForUpdatedPackages
    Signed-off-by: August Toman-Yih <atomanyih@pivotal.io>

--- spec/task-helpers/publish-helper-spec.js
+++ spec/task-helpers/publish-helper-spec.js
@@ -1,5 +1,7 @@
 import mockPromises from 'mock-promises';
+import {readArray, writeArray, map} from 'event-stream';
 const proxyquire = require('proxyquire').noPreserveCache();
+
 global.Promise = require('bluebird');
 
 describe('filterPackages', () => {
@@ -59,5 +61,50 @@ describe('filterPackages', () => {
     mockPromises.tick(1);
 
     expect(packageInfo).toEqual(undefined);
+  });
+});
+
+describe('infoForUpdatedPackages', () => {
+  let infoForUpdatedPackages;
+
+  beforeEach(() => {
+    infoForUpdatedPackages = proxyquire('../../tasks/helpers/publish-helper', {
+      'child_process': {
+        exec: (command, callback) => {
+          callback(null, '1.0.0');
+        }
+      },
+      'gulp-util': {
+        log: () => {}
+      }
+    }).infoForUpdatedPackages;
+  });
+
+  it('returns packages that have newer versions', (done) => {
+    const file1 = {
+      contents: {
+        toString() {
+          return '{"name": "hamburger", "version": "0.0.1"}';
+        }
+      },
+      path: 'i/like/hamburger'
+    };
+
+    const file2 = {
+      contents: {
+        toString() {
+          return '{"name": "hotdog", "version": "1.0.1"}';
+        }
+      },
+      path: 'i/love/hotdog'
+    };
+
+    readArray([file1, file2])
+      .pipe(infoForUpdatedPackages())
+      .pipe(map((data, callback) => {
+        expect(data.length).toEqual(1);
+        expect(data[0].name).toEqual('hotdog');
+        done();
+      }));
   });
 });
