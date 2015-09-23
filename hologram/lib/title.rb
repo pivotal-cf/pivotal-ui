4cac33797c8d6c3a71b47bddae7b97fb4d7ca6f3
August Toman-Yih <atomanyih@pivotal.io>
2015-09-23 12:04:13 -0700

    feat(styleguide): generate pretty titles for new styleguide organization
    [Finishes #103959032]

---
+++ hologram/lib/title.rb
@@ -0,0 +1,41 @@
+require_relative('./navigation')
+
+class Title
+  def self.from_category(hologram_title)
+    nav = NavPath.from_category(hologram_title)
+
+    if nav
+      new(
+        language: nav.language,
+        component: nav.component,
+      )
+    else
+      "Pivotal UI: #{hologram_title}"
+    end
+  end
+
+  def initialize(language:, component:)
+    @language = language
+    @component = component
+  end
+
+  def to_s
+    "Pivotal UI #{pretty_language}: #{pretty_component}"
+  end
+
+  private
+
+  def pretty_language
+    if language == 'css'
+      language.upcase
+    elsif language =='react'
+      language.capitalize
+    end
+  end
+
+  def pretty_component
+    component.split('-').map(&:capitalize).join(' ')
+  end
+
+  attr_reader :language, :component
+end
---
+++ spec/hologram/lib/title_spec.rb
@@ -0,0 +1,24 @@
+require_relative('../spec_helper')
+require_relative('../../../hologram/lib/title')
+
+describe Title do
+  it 'returns a beautified version of the portion of the category string' do
+    title = Title.from_category('react_astronomy_spacebubble')
+    expect(title.to_s).to eq('Pivotal UI React: Spacebubble')
+  end
+
+  it 'upcases css to CSS' do
+    title = Title.from_category('css_canoe_baseball')
+    expect(title.to_s).to eq('Pivotal UI CSS: Baseball')
+  end
+
+  it 'subs spaces for dashes and capitalizes every word in component name' do
+    title = Title.from_category('react_sandbag_existential-angst')
+    expect(title.to_s).to eq('Pivotal UI React: Existential Angst')
+  end
+
+  it 'uses the original title when it does not match the nav pattern' do
+    title = Title.from_category('I am special love me')
+    expect(title.to_s).to eq('Pivotal UI: I am special love me')
+  end
+end
