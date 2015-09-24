55ed9ff36aba96f510eb4be974118ead36de34f3
Caroline Taymor <ctaymor@pivotal.io>
2015-09-24 15:24:18 -0700

    chore(tile-layout): add javadocs
    Signed-off-by: August Toman-Yih <atomanyih@pivotal.io>

--- src/pivotal-ui-react/tile-layout/tile-layout.js
+++ src/pivotal-ui-react/tile-layout/tile-layout.js
@@ -2,6 +2,37 @@ const React = require('react');
 const classnames = require('classnames');
 import {mergeProps} from 'pui-react-helpers';
 
+/**
+ * @component TileLayout
+ * @description A responsive left to right wrapping layout with a set number of columns
+ * where items will scale to fit their container
+ *
+ * @property columns {Number|Object} Number of columns or mapping from screen size to number of columns
+ * @property noGutter {Boolean} Removes the gutter
+ *
+ * @example ```js
+ * var TileLayout = require('pui-react-tile-layout').TileLayout;
+ * var MyComponent = React.createClass({
+ *   render() {
+ *     return (
+ *       <TileLayout columns={3}>
+ *         <TileLayout.Item>
+ *           An Item
+ *         </TileLayout.Item>
+ *         <TileLayout.Item>
+ *           Another Item
+ *         </TileLayout.Item>
+ *         <TileLayout.Item>
+ *           A third item
+ *         </TileLayout.Item>
+ *       </TileLayout>
+ *     );
+ *   }
+ * });
+ * ```
+ *
+ * @see [Pivotal UI React](http://styleguide.pivotal.io/react_all_tile-layout.html)
+ */
 const TileLayout = React.createClass({
   propTypes: {
     columns: React.PropTypes.oneOfType([
@@ -43,6 +74,34 @@ const TileLayout = React.createClass({
   }
 });
 
+/**
+ * @component TileLayout.Item
+ * @description Wrapper element for items in a tile layout
+ *
+ * @example ```js
+ * var TileLayout = require('pui-react-tile-layout').TileLayout;
+ *
+ * var MyComponent = React.createClass({
+ *   render() {
+ *     return (
+ *       <TileLayout columns={3}>
+ *         <TileLayout.Item>
+ *           An Item
+ *         </TileLayout.Item>
+ *         <TileLayout.Item>
+ *           Another Item
+ *         </TileLayout.Item>
+ *         <TileLayout.Item>
+ *           A third item
+ *         </TileLayout.Item>
+ *       </TileLayout>
+ *     );
+ *   }
+ * });
+ * ```
+ *
+ * @see [Pivotal UI React](http://styleguide.pivotal.io/react_all_tile-layout.html)
+ */
 TileLayout.Item = React.createClass({
   render() {
     return (
