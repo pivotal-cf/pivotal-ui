07b1f8bb41c14f4dea3b50d4e69b643ee9267832
August Toman-Yih <atomanyih@pivotal.io>
2015-10-01 17:22:09 -0700

    chore(styleguide): only include the full pivotal-ui-react on react pages
    somehow it was interfering with google maps
    
    [fixes #104657126]

--- src/styleguide/styleguide-react.js
+++ src/styleguide/styleguide-react.js
@@ -1,5 +1,7 @@
 
 $(document).ready(() => {
+  const React = global.React || require('react');
+
   const StyleguideNav = require('./styleguide-nav');
   const getComponentPath = require('./get-component-path');
 
