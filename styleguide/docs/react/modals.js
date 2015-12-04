cd1a38001ea707cf5465c6ab1fa2a87a942b336e
August Toman-Yih <atomanyih@pivotal.io>
2015-12-04 10:39:23 -0800

    chore(modal): cast out stateful modal from docs
    [#105703024]
    
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- styleguide/docs/react/modals.js
+++ styleguide/docs/react/modals.js
@@ -18,95 +18,30 @@ For the example, you also need to install [Buttons](#button_react) and require `
 Require the subcomponent:
 
 ```
-var Modal = require('pui-react-modals').Modal;
+var BaseModal = require('pui-react-modals').BaseModal;
 var ModalBody = require('pui-react-modals').ModalBody;
 var ModalFooter = require('pui-react-modals').ModalFooter;
 ```
 
 We provide 3 components that can be used to assemble modals:
 
-* `Modal`
+* `BaseModal`
 * `ModalBody`
 * `ModalFooter`
 
-<div class="alert alert-info mbxl">
-  <h5 class="em-high mtn">
-    Opening and closing the modal with callbacks
-  </h5>
-  <p>
-    Modals will be closed by default. To open the modal, add a <code>ref</code>
-    property to the modal (i.e. <code>ref='myModal'</code>). Trigger
-    <code>this.refs.myModal.open();</code> to open the modal, and
-    <code>this.refs.myModal.close();</code> to close the modal.
-  </p>
-</div>
-
-There are 4 ways to close the modal
-
-* Clicking the "x" button
-* Clicking on the modal backdrop
-* Clicking the esc key
-* Doing any action that triggers `this.refs.myModal.close`.
-
-```jsx_example
-
-var MyModal = React.createClass({
-  _openModal: function(){
-    this.refs.modal.open();
-  },
-
-  _closeModal: function() {
-    this.refs.modal.close();
-  },
-
-  render: function() {
-    return (
-      <div>
-        <DefaultButton id='openButton' onClick={this._openModal}>
-          Open Modal
-        </DefaultButton>
-        <Modal title='What a Header!' ref='modal' className='optional-custom-class'>
-          <ModalBody>Text in a body</ModalBody>
-          <ModalFooter>
-            <DefaultButton id='closeButton' onClick={this._closeModal}>
-              Close
-            </DefaultButton>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
-});
-
-```
-
-```react_example_table
-<MyModal/>
-```
-*/
-
-/*doc
- ---
- title: Stateless Modal
- name: modal_react_stateless
- parent: modal_react
- ---
-
- You can use the stateless `BaseModal`. It takes the same properties as modal, plus two extra:
-
+Relevant Properties:
 
  `show`: a boolean; whether the modal should be opened or closed
 
+ `onHide`: a function; called when the modal is signaled to close in one of these ways:
 
- `onHide`: a function; called when the modal receives an event that would normally cause it to close
-
-
-
- Here's the same example as above, but implemented with `BaseModal`
+ * Clicking the "x" button
+ * Clicking on the modal backdrop
+ * Clicking the esc key
 
  ```jsx_example
 
- var MyOtherModal = React.createClass({
+ var MyModal = React.createClass({
    getInitialState: function() {
      return {
        modalOpen: false
@@ -123,7 +58,7 @@ var MyModal = React.createClass({
    render: function() {
      return (
        <div>
-         <DefaultButton id='openStatelessModal' onClick={this._openModal}>
+         <DefaultButton onClick={this._openModal}>
            Open Stateless Modal
          </DefaultButton>
          <BaseModal title='What a Header!'
@@ -132,7 +67,7 @@ var MyModal = React.createClass({
                     onHide={this._closeModal}>
            <ModalBody>Text in a body</ModalBody>
            <ModalFooter>
-             <DefaultButton id='closeStatelessModal' onClick={this._closeModal}>
+             <DefaultButton onClick={this._closeModal}>
                Close
              </DefaultButton>
            </ModalFooter>
@@ -145,6 +80,6 @@ var MyModal = React.createClass({
  ```
 
  ```react_example_table
- <MyOtherModal/>
+ <MyModal/>
  ```
- */
+*/
