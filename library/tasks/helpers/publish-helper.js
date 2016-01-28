2d6dfe2192923c6bc34c64d494dc5f818de44c82
Adam Berkovec <aberkovec@pivotal.io>
2016-01-28 15:37:33 -0800

    chore(tasks): Update author list for publish-helper
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- library/tasks/helpers/publish-helper.js
+++ library/tasks/helpers/publish-helper.js
@@ -69,7 +69,7 @@ export function publishPackages(registry) {
         await npmPublish([packageInfo.dir]);
 
         if (!registry) { //sinopia doesn't seem to support maintainers
-          const owners = ['charleshansen', 'atomanyih', 'kennyw1019', 'd-reinhold'];
+          const owners = ['charleshansen', 'atomanyih', 'kennyw1019', 'd-reinhold', 'aberkovec'];
           for (const owner of owners) {
             await npmOwner(['add', owner, packageInfo.name]);
           }
