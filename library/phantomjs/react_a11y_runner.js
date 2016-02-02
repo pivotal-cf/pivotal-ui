36570da5e13f2ae738706bc7c77dcd3aad9b7213
Charles Hansen <chansen@pivotal.io>
2016-02-02 10:22:30 -0800

    chore(phantom): Switch from phantomjs to phantomjs-prebuilt, remove uses of phantomjs-polyfill

--- library/phantomjs/react_a11y_runner.js
+++ library/phantomjs/react_a11y_runner.js
@@ -1,7 +1,6 @@
 var system = require('system');
 var webPage = require('webpage');
 var args = system.args;
-var BIND_POLYFILL_PATH = 'node_modules/phantomjs-polyfill/bind-polyfill.js';
 
 var page = webPage.create();
 var messages = [];
@@ -11,11 +10,6 @@ page.onConsoleMessage = function(message) {
 page.onCallback = function() {
   system.stdout.write(messages.join('\n'));
   phantom.exit(0);
-};
-
-// Need to polyfill bind...
-page.onInitialized = function () {
-  page.injectJs(BIND_POLYFILL_PATH);
 };
 
 var port = args[1];
