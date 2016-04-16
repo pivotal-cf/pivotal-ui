fd76223fa1a59c948015a566e0115c7e4901d765
Ryan Dy <rdy@pivotal.io>
2016-04-15 20:12:10 -0700

    feature(mounted): add mounted mixin that determines when a component is mounted

---
+++ library/spec/pivotal-ui-react/mixins/mounted_mixin_spec.js
@@ -0,0 +1,24 @@
+require('../spec_helper');
+
+describe('MountedMixin', () => {
+  let Component, subject;
+  beforeEach(() => {
+    const mixin = require('pui-react-mixins');
+    const Mounted = require('../../../src/pivotal-ui-react/mixins/mixins/mounted_mixin');
+    class Klass extends mixin(React.Component).with(Mounted) {
+      render() { return null; }
+    }
+    Component = Klass;
+    subject = ReactDOM.render(<Component/>, root);
+  });
+
+  describe('#mounted', () => {
+    it('returns true when the component is mounted', () => {
+      expect(subject.mounted()).toBe(true);
+      ReactDOM.unmountComponentAtNode(root);
+      expect(subject.mounted()).toBe(false);
+      subject = ReactDOM.render(<Component/>, root);
+      expect(subject.mounted()).toBe(true);
+    });
+  });
+});
