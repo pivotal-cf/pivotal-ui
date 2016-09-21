50149a56d4452f27f3a286278a0ba114810c69da
Charles Hansen <chansen@pivotal.io>
2016-09-21 15:51:16 -0700

    chore(cleanup): Delete unused javascripts folder in css components
    Signed-off-by: Weyman Fung <wfung@pivotal.io>

--- library/src/pivotal-ui/javascripts/pivotal-ui.js
+++
@@ -1,8 +0,0 @@
-global.$ = global.jQuery = require('jquery');
-global._ = require('lodash');
-require('bootstrap');
-
-require('pui-prismjs');
-
-require('./scale')();
-require('pui-react-back-to-top/jquery-plugin');
