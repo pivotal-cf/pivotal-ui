7053f73ce3267795d258fa2fb3191a1dd0e412f5
Charles Hansen <chansen@pivotal.io>
2016-09-27 08:19:10 -0700

    chore(spec): uncomment accessibility matchers

--- library/spec/pivotal-ui-react/accessibility-developer-tools-matcher.js
+++ library/spec/pivotal-ui-react/accessibility-developer-tools-matcher.js
@@ -12,24 +12,24 @@ module.exports = function toPassADT() {
   return {
     compare(node) {
       let result = {};
-      //
-      // let config = new axs.AuditConfiguration();
-      // config.showUnsupportedRulesWarning = false;
-      // config.scope = node;
-      //
-      // const adtResults = axs.Audit.run(config)
-      //   .filter((adtResult) => adtResult.result === 'FAIL');
-      //
-      // result.pass = adtResults.length === 0;
-      //
-      // if (result.pass) {
-      //   result.message = 'Expected ADT to fail';
-      // } else {
-      //   const failures = adtResults
-      //     .map((result) => failureMessageForAdtResult(result))
-      //     .join('\n  ');
-      //   result.message = `Expected ADT to pass but got errors:\n  ${failures}`;
-      // }
+
+      let config = new axs.AuditConfiguration();
+      config.showUnsupportedRulesWarning = false;
+      config.scope = node;
+
+      const adtResults = axs.Audit.run(config)
+        .filter((adtResult) => adtResult.result === 'FAIL');
+
+      result.pass = adtResults.length === 0;
+
+      if (result.pass) {
+        result.message = 'Expected ADT to fail';
+      } else {
+        const failures = adtResults
+          .map((result) => failureMessageForAdtResult(result))
+          .join('\n  ');
+        result.message = `Expected ADT to pass but got errors:\n  ${failures}`;
+      }
 
       return result;
     }
