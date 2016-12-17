1903e6693264befbbcace992f9820c6134df7bce
Jean de Klerk <jadekler@gmail.com>
2016-12-16 17:08:57 -0800

    feat(Iconography): Add support for spinner, spinner-md, and spinner-sm

--- styleguide/docs/react/iconography.js
+++ styleguide/docs/react/iconography.js
@@ -38,6 +38,12 @@ Icons by default will be sized based on the local font size. You can override th
 <Icon src="add" style={{width: 100, height: 100}}/>
 ```
 
+Note that for the spinner icons (`spinner`, `spinner-md`, `spinner-sm`) we recommend:
+
+- Spinner used for the 80px+ range. Loading a page front and center, nothing else displayed
+- Spinner-md used for the 80px-25px range. Loading a panel or larger component
+- Spinner-sm used for the 24px-10px range. Loading inside a button or form element
+
  Property           |  Type         | Description
  -------------      | --------------| --------------------------------------------------------------------------
  `src`       | String  | Name of the svg to load
