74cf394a422d5ec4c2bf52f24d91d618b9c9f901
Jean de Klerk <jadekler@gmail.com>
2017-01-19 14:29:54 -0700

    Deploy workflow for routing

--- styleguide_new/src/app.jsx
+++ styleguide_new/src/app.jsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom'
 import '../stylesheets/app.scss'
 import SideBar from './sidebar'
 import Content from './content'
+import {Router, Route, browserHistory} from 'react-router'
 
-ReactDOM.render(<div id="app">
-  <SideBar/>
-  <Content/>
-</div>, document.getElementById('root'))
+class App extends React.Component{
+  render() {
+    return <div id="app">
+      <SideBar/>
+      <Content/>
+    </div>
+  }
+}
+
+class Foo extends React.Component{
+  render() {
+    return <div>foo</div>
+  }
+}
+
+class Bar extends React.Component{
+  render() {
+    return <div>bar</div>
+  }
+}
+ReactDOM.render(<Router history={browserHistory}>
+  <Route path="/" component={App}>
+    <Route path="foo" component={Foo}/>
+    <Route path="bar" component={Bar}/>
+  </Route>
+</Router>, document.getElementById('root'))
--- styleguide_new/src/content.js
+++ styleguide_new/src/content.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default () => <div className="content">
+export default () => <div id="content" className="content">
   Hello World!
 </div>
