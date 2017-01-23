4d4fe620ea392924a465e4545bc9655f2dd017c4
Jean de Klerk <jadekler@gmail.com>
2017-01-23 13:57:03 -0700

    Single page app navigation

--- styleguide_new/src/App.jsx
+++ styleguide_new/src/App.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import '../stylesheets/app.scss'
+
 import SideBar from './components/Sidebar'
 import Content from './components/Content'
+import ButtonStuff from '../docs/Buttons.md'
+import RibbonStuff from '../docs/Ribbons.md'
+import PanelStuff from '../docs/Panels.md'
 
-ReactDOM.render(<div id="app">
-  <SideBar/>
-  <Content/>
-</div>, document.getElementById('root'))
+const content = {
+  '/buttons': ButtonStuff,
+  '/ribbons': RibbonStuff,
+  '/panels': PanelStuff,
+}
+
+const homepage = <div className="content">
+  Make a selection on the left
+</div>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {content: false}
+  }
+
+  updateContent(event) {
+    event.preventDefault()
+    window.history.pushState({}, '', event.target.href)
+    const path = window.location.pathname
+
+    if(path === "/" || !content[path]) {
+      this.setState({content: false})
+    }
+
+    this.setState({content: content[path]})
+  }
+
+  render() {
+    return <div id="app">
+      <SideBar clickHandler={this.updateContent.bind(this)}/>
+      {this.state.content ? <Content content={this.state.content}/> : homepage}
+    </div>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- styleguide_new/src/components/Sidebar.js
+++ styleguide_new/src/components/Sidebar.js
@@ -1,59 +1,67 @@
 import React from 'react'
 
 const styleItems = [
-  {text: "Icons", link: "icons"},
-  {text: "Colors", link: "colors"},
-  {text: "Grid", link: "grid"},
+  {text: 'Icons', link: 'icons'},
+  {text: 'Colors', link: 'colors'},
+  {text: 'Grid', link: 'grid'},
 ]
 
 const componentItems = [
-  {text: "Alerts", link: "alerts"},
-  {text: "Alignment", link: "alignment"},
-  {text: "Autocomplete", link: "autocomplete"},
-  {text: "Buttons", link: "buttons"},
-  {text: "Collapse", link: "collapse"},
-  {text: "Copy To Clipboard", link: "copy_to_clipboard"},
-  {text: "Dividers", link: "dividers"},
-  {text: "Dropdowns", link: "dropdowns"},
-  {text: "Ellipsis", link: "ellipsis"},
-  {text: "Expander", link: "expander"},
-  {text: "Forms", link: "forms"},
-  {text: "Grids", link: "grids"},
-  {text: "Hoverable", link: "hoverable"},
-  {text: "Images", link: "images"},
-  {text: "Labels", link: "labels"},
-  {text: "Lists", link: "lists"},
-  {text: "Media", link: "media"},
-  {text: "Modals", link: "modals"},
-  {text: "Notifications", link: "notifications"},
-  {text: "Pagination", link: "pagination"},
-  {text: "Tooltips", link: "tooltips"},
-  {text: "Panels", link: "panels"},
-  {text: "Panes", link: "panes"},
-  {text: "Portals", link: "portals"},
-  {text: "Responsive Utilities", link: "responsive_utilities"},
-  {text: "Ribbons", link: "ribbons"},
-  {text: "Select", link: "select"},
-  {text: "Svg", link: "svg"},
-  {text: "Tables", link: "tables"},
-  {text: "Tabs", link: "tabs"},
-  {text: "Tile Layout", link: "tile_layout"},
+  {text: 'Alerts', link: 'alerts'},
+  {text: 'Alignment', link: 'alignment'},
+  {text: 'Autocomplete', link: 'autocomplete'},
+  {text: 'Buttons', link: 'buttons'},
+  {text: 'Collapse', link: 'collapse'},
+  {text: 'Copy To Clipboard', link: 'copy_to_clipboard'},
+  {text: 'Dividers', link: 'dividers'},
+  {text: 'Dropdowns', link: 'dropdowns'},
+  {text: 'Ellipsis', link: 'ellipsis'},
+  {text: 'Expander', link: 'expander'},
+  {text: 'Forms', link: 'forms'},
+  {text: 'Grids', link: 'grids'},
+  {text: 'Hoverable', link: 'hoverable'},
+  {text: 'Images', link: 'images'},
+  {text: 'Labels', link: 'labels'},
+  {text: 'Lists', link: 'lists'},
+  {text: 'Media', link: 'media'},
+  {text: 'Modals', link: 'modals'},
+  {text: 'Notifications', link: 'notifications'},
+  {text: 'Pagination', link: 'pagination'},
+  {text: 'Tooltips', link: 'tooltips'},
+  {text: 'Panels', link: 'panels'},
+  {text: 'Panes', link: 'panes'},
+  {text: 'Portals', link: 'portals'},
+  {text: 'Responsive Utilities', link: 'responsive_utilities'},
+  {text: 'Ribbons', link: 'ribbons'},
+  {text: 'Select', link: 'select'},
+  {text: 'Svg', link: 'svg'},
+  {text: 'Tables', link: 'tables'},
+  {text: 'Tabs', link: 'tabs'},
+  {text: 'Tile Layout', link: 'tile_layout'},
 ]
 
-export default () => {
-  const styles = styleItems
-    .map((style, i) => <a key={i} href={style.link} className="sidebar--item">{style.text}</a>)
+export default class Sidebar extends React.PureComponent {
+  render() {
+    const styles = styleItems
+      .map((style, i) => <a key={i}
+                            onClick={this.props.clickHandler}
+                            href={style.link}
+                            className="sidebar--item">{style.text}</a>)
 
-  const components = componentItems
-    .map((component, i) => <a key={i} href={component.link} className="sidebar--item">{component.text}</a>)
+    const components = componentItems
+      .map((component, i) => <a key={i}
+                                onClick={this.props.clickHandler}
+                                href={component.link}
+                                className="sidebar--item">{component.text}</a>)
 
-  return <div className="sidebar">
-    <input className="sidebar--search" placeholder="search by component name"/>
-    <div className="sidebar--content">
-      <h2>Styles</h2>
-      {styles}
-      <h2>Components</h2>
-      {components}
+    return <div className="sidebar">
+      <input className="sidebar--search" placeholder="search by component name"/>
+      <div className="sidebar--content">
+        <h2>Styles</h2>
+        {styles}
+        <h2>Components</h2>
+        {components}
+      </div>
     </div>
-  </div>
+  }
 }
