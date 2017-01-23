c19757d3e25dfe9d1df0a780bfff64e35ebb366b
Jean de Klerk <jadekler@gmail.com>
2017-01-23 13:57:03 -0700

    Single page app navigation

--- styleguide_new/src/components/Content.js
+++ styleguide_new/src/components/Content.js
@@ -1,26 +1,10 @@
 import React from 'react'
 import MarkdownViewer from './code_area/MarkdownViewer'
 
-import ButtonStuff from '../../docs/Buttons.md'
-import RibbonStuff from '../../docs/Ribbons.md'
-import PanelStuff from '../../docs/Panels.md'
-
-const content = {
-  "/buttons": ButtonStuff,
-  "/ribbons": RibbonStuff,
-  "/panels": PanelStuff,
-}
-
-const homepage = <div className="content">
-  Make a selection on the left
-</div>
-
-export default () => {
-  if(window.location.pathname === "/" || !content[window.location.pathname]) {
-    return homepage
+export default class Content extends React.PureComponent {
+  render() {
+    return <div className="content">
+      <MarkdownViewer html={this.props.content}/>
+    </div>
   }
-
-  return <div className="content">
-    <MarkdownViewer html={content[window.location.pathname]}/>
-  </div>
 }
--- styleguide_new/src/components/code_area/MarkdownViewer.js
+++ styleguide_new/src/components/code_area/MarkdownViewer.js
@@ -4,16 +4,20 @@ import CodeArea from './CodeArea'
 import {AllHtmlEntities} from 'html-entities'
 
 export default class MarkdownViewer extends React.Component {
-  static renderEditableArea(codeElement) {
-    const code = AllHtmlEntities.decode(codeElement.innerHTML)
-    ReactDOM.render(<CodeArea code={code}/>, codeElement)
-  }
-
   static renderEditableAreas(element) {
-    for(let i = 0; element.getElementsByTagName("code").length > i; i++) {
+    if (!element) {
+      return
+    }
+
+    for(let i = 0; element.getElementsByTagName('code').length > i; i++) {
       const codeElement = element.getElementsByTagName('code')[i]
       MarkdownViewer.renderEditableArea(codeElement)
     }
+  }
+
+  static renderEditableArea(codeElement) {
+    const code = AllHtmlEntities.decode(codeElement.innerHTML)
+    ReactDOM.render(<CodeArea code={code}/>, codeElement)
   }
 
   render() {
