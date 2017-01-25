37d44bce9ea66e3f365b3793cc378c74bcdfca2b
Jean de Klerk <jadekler@gmail.com>
2017-01-25 12:01:11 -0700

    chore(code areas): add ability to toggle editor and preview in code areas
    [#137317799]
    
    Signed-off-by: Tom Chen <tochen@pivotal.io>

--- styleguide_new/src/components/code_area/HtmlCodeArea.js
+++ styleguide_new/src/components/code_area/HtmlCodeArea.js
@@ -1,31 +1,47 @@
 import React from 'react'
 import {AllHtmlEntities} from 'html-entities'
 import AceEditor from 'react-ace'
+import {Icon} from 'pui-react-iconography'
+import {InlineList, ListItem} from 'pui-react-lists'
 
 import 'brace/mode/html'
 
 export default class HtmlCodeArea extends React.PureComponent {
   constructor(props) {
     super(props)
-    this.state = {code: props.code}
+    this.state = {
+      code: props.code,
+      showEditor: false
+    }
   }
 
   changeHandler(value) {
     this.setState({code: AllHtmlEntities.decode(value)})
   }
 
+  toggleEditor() {
+    this.setState({showEditor: this.state.showEditor ? false : true})
+  }
+
   render() {
     const {code} = this.state
 
     return <div className="code-editor">
-      <AceEditor className="code-editor--edit"
-                 width="100%"
-                 height="200px"
-                 mode="html"
-                 theme="crimson_editor"
-                 value={code}
-                 onChange={this.changeHandler.bind(this)}
-                 editorProps={{$blockScrolling: Infinity}}/>
+      <InlineList className="code-editor--toolbar">
+        <ListItem className="code-editor--toolbar--item" onClick={this.toggleEditor.bind(this)}>
+          <Icon className={this.state.showEditor ? "code-editor--toolbar--toggle-open" : ""}
+                src="check"/>
+          <div className="code-editor--toolbar--label">HTML</div>
+        </ListItem>
+      </InlineList>
+      {this.state.showEditor && <AceEditor className="code-editor--edit"
+                                           width="100%"
+                                           height="200px"
+                                           mode="html"
+                                           theme="crimson_editor"
+                                           value={code}
+                                           onChange={this.changeHandler.bind(this)}
+                                           editorProps={{$blockScrolling: Infinity}}/>}
       <div className="code-editor--live-preview" dangerouslySetInnerHTML={{__html: code}}></div>
     </div>
   }
--- styleguide_new/src/components/code_area/JsCodeArea.js
+++ styleguide_new/src/components/code_area/JsCodeArea.js
@@ -4,6 +4,8 @@ import * as Babel from 'babel-standalone'
 import {AllHtmlEntities} from 'html-entities'
 import AceEditor from 'react-ace'
 import pretty from 'pretty'
+import {Icon} from 'pui-react-iconography'
+import {InlineList, ListItem} from 'pui-react-lists'
 
 import 'brace/mode/jsx'
 import 'brace/mode/html'
@@ -16,7 +18,9 @@ export default class JsCodeArea extends React.PureComponent {
     super(props)
     this.state = {
       code: props.code,
-      codePreviewHtml: defaultLoadingMessage
+      codePreviewHtml: defaultLoadingMessage,
+      showReact: false,
+      showHTMLPreview: false
     }
   }
 
@@ -28,11 +32,20 @@ export default class JsCodeArea extends React.PureComponent {
     // <label for="radio81"> and <!-- react-text: 784 -->. If codePreviewHtml triggered
     // a redraw, the counters in the rendered jsx would increase, which would cause the
     // codePreviewHtml to be different, causing a re-render, etc, infinite loop
-    return this.state.code != nextState.code || firstCodepreviewLoad
+    return this.state.code != nextState.code || firstCodepreviewLoad ||
+              this.state.showReact != nextState.showReact || this.state.showHTMLPreview != nextState.showHTMLPreview
   }
 
   changeHandler(value) {
     this.setState({code: AllHtmlEntities.decode(value)})
+  }
+
+  toggleEditor() {
+    this.setState({showReact: this.state.showReact ? false : true})
+  }
+
+  toggleHTMLPreview() {
+    this.setState({showHTMLPreview: this.state.showHTMLPreview ? false : true})
   }
 
   static getRenderedReact(code) {
@@ -55,15 +68,27 @@ export default class JsCodeArea extends React.PureComponent {
     }
 
     return <div className="code-editor">
-      <AceEditor className="code-editor--edit"
+      <InlineList className="code-editor--toolbar">
+        <ListItem className="code-editor--toolbar--item" onClick={this.toggleEditor.bind(this)}>
+          <Icon className={this.state.showReact ? "code-editor--toolbar--toggle-open" : ""}
+                src="check"/>
+          <div className="code-editor--toolbar--label">React</div>
+        </ListItem>
+        <ListItem className="code-editor--toolbar--item" onClick={this.toggleHTMLPreview.bind(this)}>
+          <Icon className={this.state.showHTMLPreview ? "code-editor--toolbar--toggle-open" : ""}
+                src="check"/>
+          <div className="code-editor--toolbar--label">HTML</div>
+        </ListItem>
+      </InlineList>
+      {this.state.showReact && <AceEditor className="code-editor--edit"
                  width="100%"
                  height="200px"
                  mode="jsx"
                  theme="crimson_editor"
                  value={code}
                  onChange={this.changeHandler.bind(this)}
-                 editorProps={{$blockScrolling: Infinity}}/>
-      <AceEditor className="code-editor--html-preview"
+                 editorProps={{$blockScrolling: Infinity}}/>}
+      {this.state.showHTMLPreview && <AceEditor className="code-editor--html-preview"
                  width="100%"
                  height="200px"
                  mode="html"
@@ -71,7 +96,7 @@ export default class JsCodeArea extends React.PureComponent {
                  theme="crimson_editor"
                  wrap={true}
                  value={JsCodeArea.getRenderedReact(transpiledCode)}
-                 editorProps={{$blockScrolling: Infinity}}/>
+                 editorProps={{$blockScrolling: Infinity}}/>}
       <div className="code-editor--live-preview">
         {eval(transpiledCode)}
       </div>
