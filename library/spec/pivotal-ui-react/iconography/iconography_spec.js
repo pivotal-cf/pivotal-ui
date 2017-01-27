7af40de5f25de3afd689dc4f45ef1dd8e0eb6b75
Jean de Klerk <jadekler@gmail.com>
2017-01-27 11:26:17 -0700

    Remove unnecessary test

--- library/spec/pivotal-ui-react/iconography/iconography_spec.js
+++ library/spec/pivotal-ui-react/iconography/iconography_spec.js
@@ -11,14 +11,7 @@ describe('iconography', function() {
     ReactDOM.render(<Icon src='add'/>, root);
     expect('.svgicon svg').toExist();
   });
-
-  describe('when a style is given', function() {
-    it('adds the style to the svg', function() {
-      ReactDOM.render(<Icon src='add' style={{height: 100}}/>, root);
-      expect('.svgicon svg').toHaveAttr('style', /height: 100px;/);
-    });
-  });
-
+  
   it('propagates className and id to the span', () => {
     ReactDOM.render(<Icon src='add' className="foo" id="bar"/>, root);
     expect('.svgicon').toHaveClass('foo');
