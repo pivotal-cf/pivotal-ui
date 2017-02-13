6e6f3b350154664a171db30f54f9d8db851e672f
Jean de Klerk <jadekler@gmail.com>
2017-02-13 10:10:08 -0700

    setting path correctly for gemini in pipeline
    Signed-off-by: Tom Chen <tochen@pivotal.io>

--- styleguide_new/bin/gemini-in-docker.sh
+++ styleguide_new/bin/gemini-in-docker.sh
@@ -8,18 +8,4 @@ set -ex
 service dbus restart
 xvfb-run chromedriver --port=4444 --url-base=wd/hub &
 
-cd $1
-
-pushd library
-    rm -rf node_modules
-    npm i
-    gulp build
-popd
-
-pushd styleguide_new
-    rm -rf node_modules
-    npm i
-    npm run watch &
-    sleep 30 # wait for compilation to happen
-    ./node_modules/.bin/gemini test --reporter flat --reporter html
-popd
+../node_modules/.bin/gemini test --reporter flat --reporter html
