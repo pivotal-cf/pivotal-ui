318515c20bb06713040d138508d2273fd9af9a85
Jean de Klerk <jadekler@gmail.com>
2017-02-15 14:15:04 -0700

    Condense tooltip spec and switch to react test utils
    Signed-off-by: Tom Chen <chentom77@gmail.com>

--- library/spec/pivotal-ui-react/tooltip/tooltip_spec.js
+++ library/spec/pivotal-ui-react/tooltip/tooltip_spec.js
@@ -1,27 +1,10 @@
-require('../spec_helper');
-import {itPropagatesAttributes} from '../support/shared_examples';
+import '../spec_helper'
+import ReactTestUtils from 'react-addons-test-utils';
+import {Tooltip} from '../../../src/pivotal-ui-react/tooltip/tooltip'
 
-describe('tooltip', function() {
-  var props = {
-    className: 'test-class',
-    id: 'test-id',
-    style: {
-      opacity: '0.5'
-    }
-  };
-
-  beforeEach(function() {
-    var Tooltip = require('../../../src/pivotal-ui-react/tooltip/tooltip').Tooltip;
-    ReactDOM.render((<Tooltip {...props}><div className="tooltip-text">Hello World</div></Tooltip>), root);
-  });
-
-  afterEach(function() {
-    ReactDOM.unmountComponentAtNode(root);
-  });
-
-  it('works', function() {
-    expect('.tooltip-text').toExist();
-  });
-
-  itPropagatesAttributes('#root #test-id', props);
-});
+describe('tooltip', () => {
+  it('renders', () => {
+    const result = ReactTestUtils.renderIntoDocument(<Tooltip>Hello World</Tooltip>)
+    expect(ReactTestUtils.scryRenderedDOMComponentsWithClass(result, 'pui-tooltip').length).toEqual(1)
+  })
+})
