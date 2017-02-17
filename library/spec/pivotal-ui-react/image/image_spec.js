607bfab61e5d8428be512c04cf4ee7bbf4270ad6
Jean de Klerk <jadekler@gmail.com>
2017-02-17 16:39:05 -0700

    Input spec switch to react test utils
    Signed-off-by: Michael Byrd <mbyrd@pivotal.io>

--- library/spec/pivotal-ui-react/image/image_spec.js
+++ library/spec/pivotal-ui-react/image/image_spec.js
@@ -1,35 +1,30 @@
 import '../spec_helper';
 import ReactTestUtils from 'react-addons-test-utils'
 import {Image} from '../../../src/pivotal-ui-react/images/images'
-import {reactCompPropagatesAttrs} from '../support/shared_examples';
+import {reactCompPropagatesAttrs} from '../support/shared_examples'
 
 describe('Image', () => {
-  const renderComponent = props => ReactTestUtils.renderIntoDocument(<Image src="http://placehold.it/20x20" {...props} />)
+  const renderComponent = props => ReactTestUtils.renderIntoDocument(<Image
+    src="http://placehold.it/20x20" {...props} />)
 
   describe('when responsive', () => {
-    let result
-
-    beforeEach(() => {
-      result = renderComponent({responsive: true})
-    })
-
     it('adds the image-responsive class to the image', () => {
+      const result = renderComponent({responsive: true})
       const component = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'img')
       expect(component).not.toBeNull()
       expect(component.className).toContain('img-responsive')
     })
 
-    describe('when the href is set', function() {
-      it('wraps the image in an link', function() {
-        result = renderComponent({responsive: true, href: 'http://google.com'})
+    describe('when the href is set', () => {
+      it('wraps the image in an link', () => {
+        const result = renderComponent({responsive: true, href: 'http://google.com'})
         const component = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'a')
         expect(component).not.toBeNull()
       })
     })
 
-    reactCompPropagatesAttrs(renderComponent({className: 'foo', id: 'bar', style:{color: 'red'}}), 'foo', 'bar', {color: 'red'})
-
-    it('adds the gutter class to the row', function() {
+    it('adds the gutter class to the row', () => {
+      const result = renderComponent({responsive: true})
       const component = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'img')
       expect(component).not.toBeNull()
       expect(component.hasAttribute('src')).toBe(true)
@@ -37,16 +32,15 @@ describe('Image', () => {
     })
   })
 
-  describe('when responsive is set to be false', () => {
-    let result
-
-    beforeEach(() => {
-      result = renderComponent({responsive: false})
-    })
-
-    it('does not add the image-responsive class to the image', function() {
-      const component = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'img')
-      expect(component.className).not.toContain('img-responsive')
-    })
+  it('when responsive is set to be false does not add the image-responsive class to the image', () => {
+    const result = renderComponent({responsive: false})
+    const component = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'img')
+    expect(component.className).not.toContain('img-responsive')
   })
+
+  reactCompPropagatesAttrs(renderComponent({
+    className: 'foo',
+    id: 'bar',
+    style: {color: 'red'}
+  }), 'foo', 'bar', {color: 'red'})
 })
--- library/spec/pivotal-ui-react/inputs/inputs_spec.js
+++ library/spec/pivotal-ui-react/inputs/inputs_spec.js
@@ -1,118 +1,105 @@
-require('../spec_helper');
+import '../spec_helper'
+import {Input} from '../../../src/pivotal-ui-react/inputs/inputs'
+import ReactTestUtils from 'react-addons-test-utils'
 
-describe('Input', function() {
-  let changeSpy, Input, subject;
-  const id = 'firstNameInput';
-  const label = 'First Name';
-  beforeEach(function() {
-    Input = require('../../../src/pivotal-ui-react/inputs/inputs').Input;
-    changeSpy = jasmine.createSpy('change');
-    subject = ReactDOM.render(<Input success {...{className: 'input-class', label, id, onChange: changeSpy}}/>, root);
-  });
+describe('Input Component', () => {
+  const renderComponent = props => ReactTestUtils.renderIntoDocument(<Input {...props}/>)
 
-  it('renders an input with the label', function() {
-    expect('.control-label').toContainText('First Name');
-  });
+  it('renders', () => {
+    const result = renderComponent()
+    const component = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'input')
+    expect(component).not.toBeUndefined()
+  })
 
-  it('attaches the label to the input', function() {
-    expect('.control-label').toHaveAttr('for', id);
-    expect('.form-group input').toHaveAttr('id', id);
-  });
+  it('propagates id, classname, style', () => {
+    const result = renderComponent({id: 'some-id', className: 'some-class', style: {color: 'red'}})
 
-  it('passes properties to the input', function() {
-    $('.form-group input').simulate('change');
-    expect(changeSpy).toHaveBeenCalled();
-  });
+    const input = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'input')
+    expect(input.id).toEqual('some-id')
+    expect(input.style.color).toEqual('red')
+
+    const formGroup = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'form-group')
+    expect(formGroup.className).toContain('some-class')
+  })
 
   it('displays a checkmark when success prop is true', () => {
-    expect('.success').toExist();
-    expect('.form-group svg').toHaveClass('icon-check');
-  });
+    const result = renderComponent({success: true})
 
-  it('merges classnames', function() {
-    ReactDOM.render(<Input className="foo"/>, root);
-    expect('.form-group').toHaveClass('foo');
-  });
+    expect(ReactTestUtils.scryRenderedDOMComponentsWithClass(result, 'success').length).toEqual(1)
 
-  describe('when label is undefined', () => {
-    beforeEach(() => {
-      subject::setProps({label: undefined});
-    });
+    const svg = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'svg')
+    expect(svg.className.baseVal).toEqual('icon-check')
+  })
 
-    it('does not render label', () => {
-      expect('label').not.toExist();
-    });
-  });
+  it('renders search icons when search prop is true', () => {
+    const result = renderComponent({search: true})
 
-  describe('search', () => {
-    beforeEach(() => {
-      subject::setProps({success: false, search: true});
-    });
+    const formGroup = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'form-group')
+    expect(formGroup.className).toContain('form-group-left-icon')
 
-    it('renders a form group with the search classes', function() {
-      expect('.form-group').toHaveClass('form-group-left-icon');
-      expect('.form-group svg').toHaveClass('icon-search');
-    });
-  });
+    const svg = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'svg')
+    expect(svg.className.baseVal).toEqual('icon-search')
+  })
+
+  describe('label', () => {
+    it('when not given a label prop it wont render a label element', () => {
+      const result = renderComponent({label: null})
+      const labels = ReactTestUtils.scryRenderedDOMComponentsWithTag(result, 'label')
+      expect(labels.length).toEqual(0)
+    })
+
+    it('when given a label prop it will render a label element', () => {
+      const result = renderComponent({label: 'label text'})
+      const component = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'label')
+      expect(component).not.toBeUndefined()
+      expect(component.textContent).toEqual('label text')
+    })
+  })
 
   describe('leftIcon', () => {
-    beforeEach(() => {
-      subject::setProps({leftIcon: 'add', success: true, search: true});
-    });
+    it('renders a form group with icon', () => {
+      const result = renderComponent({leftIcon: 'add'})
 
-    it('renders a form group with custom icon', function() {
-      expect('.form-group').toHaveClass('form-group-left-icon');
-      expect('.form-group svg:eq(0)').toHaveClass('icon-add');
-    });
+      const formGroup = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'form-group')
+      expect(formGroup.className).toContain('form-group-left-icon')
+
+      const svgs = ReactTestUtils.scryRenderedDOMComponentsWithTag(result, 'svg')
+      expect(svgs[0].className.baseVal).toEqual('icon-add')
+    })
 
     it('overrides search option', () => {
-      expect('.form-group svg.icon-search').not.toExist();
-    });
+      const result = renderComponent({leftIcon: 'add', search: true})
+      const svgs = ReactTestUtils.scryRenderedDOMComponentsWithClass(result, 'icon-search')
+      expect(svgs.length).toEqual(0)
+    })
 
     it('can be used simultaneously with success', () => {
-      expect('.form-group svg:eq(1)').toHaveClass('icon-check');
-    });
+      const result = renderComponent({leftIcon: 'add', success: true})
+      const svgs = ReactTestUtils.scryRenderedDOMComponentsWithClass(result, 'icon-check')
+      expect(svgs.length).toEqual(1)
+    })
 
-    describe('when leftIcon is a node', () => {
-      beforeEach(() => {
-        subject::setProps({leftIcon: <div className="custom" />, success: true, search: true});
-      });
+    it('allows node icons', () => {
+      const result = renderComponent({leftIcon: <div className="my-custom-icon-node">Hello!</div>})
+      const innerNode = ReactTestUtils.scryRenderedDOMComponentsWithClass(result, 'my-custom-icon-node')
+      expect(innerNode.length).toEqual(1)
+    })
+  })
 
-      it('renders the node', () => {
-        expect('.form-group .custom').toExist();
-      });
+  describe('when a placeholder is provided', () => {
+    it('renders the input with a placeholder', () => {
+      const result = renderComponent({placeholder: 'Search here...'})
+      const input = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'input')
+      expect(input.placeholder).toEqual('Search here...')
 
-      it('overrides search option', () => {
-        expect('.form-group svg.icon-search').not.toExist();
-      });
+      expect(input.hasAttribute('aria-label')).toBeTruthy()
+      expect(input.getAttribute('aria-label')).toEqual('Search here...')
+    })
 
-      it('can be used simultaneously with success', () => {
-        expect('.form-group svg.icon-check').toExist();
-      });
-    });
-  });
-
-  describe('when a placeholder is provided', function() {
-    beforeEach(function() {
-      subject::setProps({placeholder: 'Search here...'});
-    });
-
-    it('renders the input with a placeholder', function() {
-      expect('.form-group input').toHaveAttr('placeholder', 'Search here...');
-    });
-
-    it('adds an aria label with the placeholder value', function() {
-      expect('.form-group input').toHaveAttr('aria-label', 'Search here...');
-    });
-
-    describe('when an aria-label is provided as well', function() {
-      beforeEach(function() {
-        subject::setProps({'aria-label': 'Search Box'});
-      });
-
-      it('uses the label as the aria-label instead of the placeholder', function() {
-        expect('.form-group input').toHaveAttr('aria-label', 'Search Box');
-      });
-    });
-  });
-});
+    it('uses the label as the aria-label instead of the placeholder when an aria-label is provided as well', () => {
+      const result = renderComponent({placeholder: 'Search here...', 'aria-label': 'Search Box'})
+      const input = ReactTestUtils.findRenderedDOMComponentWithTag(result, 'input')
+      expect(input.getAttribute('aria-label')).toEqual('Search Box')
+    })
+  })
+})
