e031f1b8f85fd6a063e3961e7ae59d92d3d73d08
Tom Chen <tochen@pivotal.io>
2017-03-03 08:25:23 -0700

    feat(dropdown) dropdown styleguide formatting
    [#140723763]
    
    Signed-off-by: Chris Wang <cwang@pivotal.io>

--- styleguide/docs/react/dropdowns.js
+++ styleguide/docs/react/dropdowns.js
@@ -42,99 +42,151 @@ Import the subcomponents:
 ```
 import {Dropdown, DropdownItem} from 'pui-react-dropdowns';
 ```
+*/
 
-## Basic Dropdown
+/*doc
+---
+title: Basic Dropdown
+name: 1_basic_dropdown_react
+parent: dropdown_react
+---
 
 This is the basic dropdown. It has an attached menu that extends from the bottom.
 
 ```react_example
-<Dropdown title='Basic DropDown'>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-</Dropdown>
+<div className="form-group form-inline">
+  <Dropdown title='Basic DropDown'>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+  </Dropdown>
+</div>
 ```
+*/
 
-## Float Dropdown Menu:
+/*doc
+---
+title: Float Dropdown Menu
+name: 2_float_dropdown_menu_react
+parent: dropdown_react
+---
 
 The float dropdown menu is spaced 2px below the toggle. It can also be pinned to the left or right for content that
 exceeds the parent dropdown width.
 
 ```react_example
-<div className="form-group" style={{display: 'flex'}}>
- <Dropdown title='Float DropDown Menu' menuCaret style={{marginRight: 32}}>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
- </Dropdown>
+<div>
+  <div className="form-group form-inline">
+    <Dropdown title='Float DropDown Menu' menuCaret>
+      <DropdownItem href="#">lorem ipsum</DropdownItem>
+      <DropdownItem href="#">lorem ipsum</DropdownItem>
+      <DropdownItem href="#">lorem ipsum</DropdownItem>
+    </Dropdown>
+  </div>
 
- <Dropdown title='Pinned Left' menuCaret menuAlign='left' style={{marginRight: 32}}>
- <DropdownItem href="#">Note that this option exceeds the width of the parent dropdown toggle</DropdownItem>
- <DropdownItem href="#">lorem ipsum</DropdownItem>
- <DropdownItem href="#">lorem ipsum</DropdownItem>
- </Dropdown>
+  <div className="form-group form-inline">
+   <Dropdown title='Pinned Left' menuCaret menuAlign='left'>
+     <DropdownItem href="#">Note that this option exceeds the width of the parent dropdown toggle</DropdownItem>
+     <DropdownItem href="#">lorem ipsum</DropdownItem>
+     <DropdownItem href="#">lorem ipsum</DropdownItem>
+   </Dropdown>
+  </div>
 
- <Dropdown title='Pinned Right' menuCaret menuAlign='right'>
- <DropdownItem href="#">lorem ipsum</DropdownItem>
- <DropdownItem href="#">lorem ipsum</DropdownItem>
- <DropdownItem href="#">lorem ipsum</DropdownItem>
- </Dropdown>
+  <div className="form-group form-inline">
+   <Dropdown title='Pinned Right' menuCaret menuAlign='right'>
+     <DropdownItem href="#">lorem ipsum</DropdownItem>
+     <DropdownItem href="#">lorem ipsum</DropdownItem>
+     <DropdownItem href="#">lorem ipsum</DropdownItem>
+   </Dropdown>
+  </div>
 </div>
 ```
+*/
 
-## Float Scroll Menu:
+/*doc
+---
+title: Float Scroll Menu
+name: 3_float_scroll_menu_react
+parent: dropdown_react
+---
 
 Lists that are indeterminately long can utilize the float scroll menu. The size is
 fixed so you'll have to customize the fixed height to achieve your desired results.
 
 ```react_example
-<Dropdown title='Float Scroll Menu' scroll menuCaret>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-</Dropdown>
+<div className="form-group form-inline">
+  <Dropdown title='Float Scroll Menu' scroll menuCaret>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+  </Dropdown>
+</div>
 ```
+*/
 
-## Split Dropdown:
+/*doc
+---
+title: Split Dropdown
+name: 4_split_dropdown_react
+parent: dropdown_react
+---
 
 Action with additional actions hidden in a dropdown. Only appropriate as a float menu.
 
 ```react_example
-<Dropdown title='Split Dropdown' split>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-</Dropdown>
+<div className="form-group form-inline">
+  <Dropdown title='Split Dropdown' split>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+  </Dropdown>
+</div>
 ```
+*/
 
-## Flat Button Dropdown:
+/*doc
+---
+title: Flat Button Dropdown
+name: 5_flat_button_dropdown_react
+parent: dropdown_react
+---
 
 Dropdown with the flat button styling.
 
 ```react_example
-<Dropdown title='Flat Button DropDown' flat>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-</Dropdown>
+<div className="form-group form-inline">
+  <Dropdown title='Flat Button DropDown' flat>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+  </Dropdown>
+</div>
 ```
+*/
 
-## Link Dropdown:
+/*doc
+---
+title: Link Dropdown
+name: 7_link_dropdown_react
+parent: dropdown_react
+---
 
 Dropdown with the link styling, retains dropdown padding.
 
 ```react_example
-<Dropdown title='Link DropDown' link>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-  <DropdownItem href="#">lorem ipsum</DropdownItem>
-</Dropdown>
+<div className="form-group form-inline">
+  <Dropdown title='Link DropDown' link>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+    <DropdownItem href="#">lorem ipsum</DropdownItem>
+  </Dropdown>
+</div>
 ```
 */
