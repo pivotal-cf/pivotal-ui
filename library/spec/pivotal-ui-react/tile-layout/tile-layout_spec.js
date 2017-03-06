50d9bacb1384e5a81b8a7a9db3e121889f2c8e8e
Chris Wang <cwang@pivotal.io>
2017-03-06 16:55:08 -0700

    chore(tileLayout) refactor tileLayout specs to use ReactTestUtils
    [#139475461]
    
    Signed-off-by: Chris Wang <cwang@pivotal.io>

--- library/spec/pivotal-ui-react/tile-layout/tile-layout_spec.js
+++ library/spec/pivotal-ui-react/tile-layout/tile-layout_spec.js
@@ -1,77 +1,91 @@
-require('../spec_helper');
-import {itPropagatesAttributes} from '../support/shared_examples';
+require('../spec_helper')
+import ReactTestUtils from 'react-addons-test-utils'
+import {TileLayout, TileLayoutItem} from '../../../src/pivotal-ui-react/tile-layout/tile-layout'
 
 describe('TileLayout', () => {
-  const {TileLayout, TileLayoutItem} = require('../../../src/pivotal-ui-react/tile-layout/tile-layout');
+  const renderComponent = (tileLayoutProps, tileItemProps) => ReactTestUtils.renderIntoDocument(
+    <TileLayout {...tileLayoutProps}>
+      <TileLayoutItem {...tileItemProps}>
+        Item 1
+      </TileLayoutItem>
+      <TileLayoutItem>
+        Item 2
+      </TileLayoutItem>
+    </TileLayout>
+  )
 
   it('creates tile-layout', () => {
-    ReactDOM.render(<TileLayout/>, root);
-    expect('#root .tile-layout').toExist();
-  });
-  it('creates tile-items', () => {
-    ReactDOM.render(
-      <TileLayout>
-        <TileLayoutItem>
-          Item 1
-        </TileLayoutItem>
-        <TileLayoutItem>
-          Item 2
-        </TileLayoutItem>
-      </TileLayout>,
-      root
-    );
+    const result = renderComponent()
+    expect(ReactTestUtils.findRenderedDOMComponentWithClass(result, 'tile-layout')).toBeTruthy()
+  })
 
-    expect('#root .tile-layout .tile-item:eq(0)')
-      .toHaveText('Item 1');
-    expect('#root .tile-layout .tile-item:eq(1)')
-      .toHaveText('Item 2');
-  });
+  it('creates tile-items', () => {
+    const result = renderComponent()
+    const tileLayout = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'tile-layout')
+    expect(tileLayout.childNodes.length).toEqual(2)
+
+    expect(tileLayout.childNodes[0].className).toEqual('tile-item')
+    expect(tileLayout.childNodes[0].textContent).toEqual('Item 1')
+    expect(tileLayout.childNodes[1].className).toEqual('tile-item')
+    expect(tileLayout.childNodes[1].textContent).toEqual('Item 2')
+  })
+
   describe('columns', () => {
     it('supports setting number of columns', () => {
-      ReactDOM.render(<TileLayout columns={4}/>, root);
+      const result = renderComponent({columns: 4})
 
-      expect('#root .tile-layout').toHaveClass('tile-layout-xs-4');
-    });
+      const tileLayout = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'tile-layout')
+      expect(tileLayout.classList).toContain('tile-layout-xs-4')
+    })
 
     it('supports supports setting number of columns for different screen sizes', () => {
-      ReactDOM.render(<TileLayout columns={{sm: 4, md: 3, lg: 2, xl: 1}}/>, root);
+      const result = renderComponent({columns: {sm: 4, md: 3, lg: 2, xl: 1}})
 
-      expect('#root .tile-layout').toHaveClass('tile-layout-sm-4');
-      expect('#root .tile-layout').toHaveClass('tile-layout-md-3');
-      expect('#root .tile-layout').toHaveClass('tile-layout-lg-2');
-      expect('#root .tile-layout').toHaveClass('tile-layout-xl-1');
-    });
-  });
+      const tileLayout = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'tile-layout')
+      expect(tileLayout.classList).toContain('tile-layout-sm-4')
+      expect(tileLayout.classList).toContain('tile-layout-md-3')
+      expect(tileLayout.classList).toContain('tile-layout-lg-2')
+      expect(tileLayout.classList).toContain('tile-layout-xl-1')
+    })
+  })
 
   describe('gutters', () => {
     it('supports no gutter', () => {
-      ReactDOM.render(<TileLayout noGutter/>, root);
+      const result = renderComponent({noGutter: true})
+      const tileLayout = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'tile-layout')
 
-      expect('#root .tile-layout').not.toHaveClass('tile-gutter');
-    });
+      expect(tileLayout.classList).not.toContain('tile-gutter')
+    })
 
-    it('supports gutter', () => {
-      ReactDOM.render(<TileLayout/>, root);
+    it('has a gutter by default', () => {
+      const result = renderComponent()
+      const tileLayout = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'tile-layout')
 
-      expect('#root .tile-layout').toHaveClass('tile-gutter');
-    });
-  });
+      expect(tileLayout.classList).toContain('tile-gutter')
+    })
+  })
 
   describe('attributes', () => {
-    beforeEach(() => {
-      ReactDOM.render(
-        <TileLayout className="outer-class" id="outer-id" style={{opacity: '0.5'}}>
-          <TileLayoutItem className="inner-class" id="inner-id" style={{opacity: '0'}}>
-            Item 1
-          </TileLayoutItem>
-        </TileLayout>,
-        root);
-    });
-    it("doesn't wipe out tile layout classes", () => {
-      expect('#root .tile-layout').toExist();
-      expect('#root .tile-layout > .tile-item').toExist();
-    });
-    itPropagatesAttributes('.tile-layout', {className: 'outer-class', id: 'outer-id', style: {opacity: '0.5'}});
-    itPropagatesAttributes('.tile-item', {className: 'inner-class', id: 'inner-id', style: {opacity: '0'}});
-  });
-});
+    it("respects the passed-in class, id and style attributes", () => {
+      const result = renderComponent({
+        className: 'outer-class',
+        id: 'outer-id',
+        style: {opacity: '0.5'}
+      }, {
+        className: 'inner-class',
+        id: 'inner-id',
+        style: {opacity: '0.75'}
+      })
+
+      const tileLayout = ReactTestUtils.findRenderedDOMComponentWithClass(result, 'tile-layout')
+      expect(tileLayout.classList).toContain('outer-class')
+      expect(tileLayout.id).toEqual('outer-id')
+      expect(tileLayout.style.opacity).toEqual('0.5')
+
+      const tileItem = tileLayout.childNodes[0]
+      expect(tileItem.classList).toContain('inner-class')
+      expect(tileItem.id).toEqual('inner-id')
+      expect(tileItem.style.opacity).toEqual('0.75')
+    })
+  })
+})
