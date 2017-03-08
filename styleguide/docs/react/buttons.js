e039688b8074991a21223a704fa17efd04432789
Tom Chen <tochen@pivotal.io>
2017-03-08 14:33:45 -0700

    feat(button) Button with spinner support and styleguide
    [#141255195]
    
    Signed-off-by: Chris Wang <cwang@pivotal.io>

--- styleguide/docs/react/buttons.js
+++ styleguide/docs/react/buttons.js
@@ -42,79 +42,83 @@ Buttons use the button tag by default. If you'd like a link rather than a button
   Link
 </DefaultButton>
 ```
+*/
 
-## Styles
+/*doc
+---
+title: Styling
+name: 01_button_styling_react
+parent: button_react
+---
 
-There are 3 main button color schemes: Default, Primary, Danger. There is also Brand, but this should only be used for marketing.
+Every button type has a default style, an alt style (with inverted colors and a transparent background) and a flat style (alt with transparent borders).
 
-```react_example_table
-<DefaultButton>
-  Default
-</DefaultButton>
+Button                                           | Button Alt                                               | Button Flat                                                | Description
+-------------------------------------------------|----------------------------------------------------------|------------------------------------------------------------|-----------
+<button class="btn btn-default"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Default</button> |<button class="btn btn-default-alt"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Default Alt</button>  | <button class="btn"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Default Flat</button>                  | This the default button style.
+<button class="btn btn-primary"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Primary</button> |<button class="btn btn-primary-alt"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Primary Alt</button>  | <button class="btn btn-primary-flat"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Primary Flat</button> | Use this button for primary actions.
+<button class="btn btn-danger"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Danger</button>   |<button class="btn btn-danger-alt"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Danger Alt</button>    | <button class="btn btn-danger-flat"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Danger Flat</button>   | This button is for delete actions, these actions should also have a confirmation dialog
+<button class="btn btn-brand"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Brand</button>     |<button class="btn btn-brand-alt"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Brand Alt</button>      | <button class="btn btn-brand-flat"><div class="icon icon-middle spinner"><svg class="icon-spinner-sm" width="100px" height="100px" viewBox="0 0 101 101" xmlns="http://www.w3.org/2000/svg"><circle class="ring" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle><circle class="path" fill="none" stroke-width="10%" stroke-linecap="butt" cx="50%" cy="50%" r="45%"></circle></svg></div>Brand Flat</button>     | This button is for marketing purposes only
 
-<PrimaryButton>
-  Primary
-</PrimaryButton>
-
-<DangerButton>
-  Danger
-</DangerButton>
-
-<BrandButton>
-  Brand
-</BrandButton>
-```
-
-For each color scheme there is the default style, an alt style (with inverted colors and a transparent background) and a flat style (alt with transparent borders).
-To use the alt style, set the `alt` prop, to use the flat style, use the `flat` prop
+The most common button examples are below:
 
 ```react_example_table
-<DefaultButton alt>
-  Alt
-</DefaultButton>
+<DefaultButton>Default</DefaultButton>
 
-<DefaultButton flat>
-  Flat
-</DefaultButton>
+<DefaultButton alt>Default Alt</DefaultButton>
 
-<PrimaryButton alt>
-  Primary Alt
-</PrimaryButton>
+<PrimaryButton>Primary</PrimaryButton>
+
+<PrimaryButton alt>Primary Alt</PrimaryButton>
+
+<PrimaryButton flat>Primary Flat</PrimaryButton>
+
+<DangerButton>Danger</DangerButton>
 ```
 
-## Sizing
+*/
 
+
+/*doc
+---
+title: Sizes
+name: 02_button_sizes_react
+parent: button_react
+---
 To make a button large, set the `large` property to true, to make it small, set `small` to true.
 
 ```react_example_table
-<PrimaryButton large>
+<DefaultButton large>
   Big Button
-</PrimaryButton>
+</DefaultButton>
 
-<PrimaryButton small>
+<DefaultButton>
+Default
+</DefaultButton>
+
+<DefaultButton small>
   Small Button
-</PrimaryButton>
+</DefaultButton>
 ```
 
-## Icons
+*/
 
-Buttons work well with icons!
+/*doc
+---
+title: Icons
+name: 03_button_icons_react
+parent: button_react
+---
+
+Buttons can contain icons:
 
 ```react_example_table
 <PrimaryButton>
-  <Icon src="add"/> Some button
-</PrimaryButton>
-
-<PrimaryButton large>
-  <Icon src="add"/> Some button
-</PrimaryButton>
-
-<PrimaryButton small>
-  <Icon src="add"/> Some button
+ <Icon src="add"/>Some button
 </PrimaryButton>
 
 <DefaultButton alt>
- <Icon src="spinner"/> Loading
+ <Icon src="spinner-sm"/>Loading
 </DefaultButton>
 ```
 */
