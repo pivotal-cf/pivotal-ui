11d76dbfa77259f37edf84c73c34834e17c5148a
Chris Wang <cwang@pivotal.io>
2017-03-10 16:07:56 -0700

    chore(back to top) update tests to use ReactTestUtils
    [#139475461]
    
    Signed-off-by: Chris Wang <cwang@pivotal.io>

--- library/spec/pivotal-ui-react/back-to-top/back-to-top_spec.js
+++ library/spec/pivotal-ui-react/back-to-top/back-to-top_spec.js
@@ -1,105 +1,100 @@
-require('../spec_helper');
-var ScrollTop = require('../../../src/pivotal-ui-react/back-to-top/scroll-top');
+require('../spec_helper')
+import {BackToTop} from 'pui-react-back-to-top'
+import ReactTestUtils from 'react-addons-test-utils'
+let ScrollTop = require('../../../src/pivotal-ui-react/back-to-top/scroll-top')
 
-describe('BackToTop', function() {
-  var BackToTop;
-  var originalGetScrollTop, originalSetScrollTop;
-  var scrollTop;
+describe('BackToTop', () => {
+  let originalGetScrollTop, originalSetScrollTop, scrollTop, subject
 
-  function triggerScroll() {
-    var event = document.createEvent('Event');
-    event.initEvent('scroll', false, false);
-    window.dispatchEvent(event);
+  const triggerScroll = () => {
+    const event = document.createEvent('Event')
+    event.initEvent('scroll', false, false)
+    window.dispatchEvent(event)
   }
 
-  beforeEach(function() {
-    scrollTop = 0;
-    originalGetScrollTop = ScrollTop.getScrollTop;
-    originalSetScrollTop = ScrollTop.setScrollTop;
-    ScrollTop.getScrollTop = () => scrollTop || 0;
-    ScrollTop.setScrollTop = (value) => scrollTop = value;
-  });
+  beforeEach(done => {
+    scrollTop = 0
+    originalGetScrollTop = ScrollTop.getScrollTop
+    originalSetScrollTop = ScrollTop.setScrollTop
+    ScrollTop.getScrollTop = () => scrollTop || 0
+    ScrollTop.setScrollTop = value => scrollTop = value
 
-  afterEach(function() {
-    ScrollTop.getScrollTop = originalGetScrollTop;
-    ScrollTop.setScrollTop = originalSetScrollTop;
-  });
+    subject = renderComponent({className: 'foo', id: 'bar', style: {fontSize: '200px'}})
 
-  beforeEach(function(done) {
-    BackToTop = require('../../../src/pivotal-ui-react/back-to-top/back-to-top').BackToTop;
-    ReactDOM.render(<BackToTop className="foo" id="bar" style={{fontSize: '200px'}}/>, root);
+    jasmine.clock().uninstall()
+    setTimeout(() => {
+      jasmine.clock().install()
+      ScrollTop.setScrollTop(500)
+      triggerScroll()
+      done()
+    }, 0)
+  })
 
-    jasmine.clock().uninstall();
-    setTimeout(function() {
-      jasmine.clock().install();
-      ScrollTop.setScrollTop(500);
-      triggerScroll();
-      done();
-    }, 0);
-  });
+  afterEach(() => {
+    ScrollTop.getScrollTop = originalGetScrollTop
+    ScrollTop.setScrollTop = originalSetScrollTop
+  })
 
-  afterEach(function() {
-    ReactDOM.unmountComponentAtNode(root);
-  });
+  const renderComponent = props => ReactTestUtils.renderIntoDocument(<BackToTop {...props}/>)
 
-  it('passes down the className, id, and style properties', () => {
-    expect('.back-to-top').toHaveClass('foo');
-    expect('.back-to-top').toHaveProp('id', 'bar');
-    expect('.back-to-top').toHaveCss({'font-size': '200px'});
-  });
+  it('renders a back to top link that is visible', () => {
+    const backToTop = ReactTestUtils.findRenderedDOMComponentWithClass(subject, 'back-to-top')
+    expect(backToTop.tagName).toEqual('A')
+    expect(backToTop.href).toContain('#top')
+  })
 
-  it('renders a back to top link that is visible', function() {
-    expect('.back-to-top').toExist();
-  });
+  it('passes through className, id, and style', () => {
+    const backToTop = ReactTestUtils.findRenderedDOMComponentWithClass(subject, 'back-to-top')
+    expect(backToTop).toHaveClass('foo')
+    expect(backToTop.id).toEqual('bar')
+    expect(backToTop).toHaveCss({'font-size': '200px'})
+  })
 
-  it('renders a arrow upward icon', () => {
-    expect('svg.icon-arrow_upward').toExist();
-  });
+  it('renders an arrow upward icon', () => {
+    const arrow = ReactTestUtils.findRenderedDOMComponentWithClass(subject, 'icon-arrow_upward')
+    expect(arrow.tagName).toEqual('svg')
+  })
 
-  it('fades in the button', function() {
-    expect('.back-to-top').toHaveCss({opacity: '0'});
-    MockNow.tick(BackToTop.FADE_DURATION / 2);
-    MockRaf.next();
-    expect('.back-to-top').toHaveCss({opacity: '0.5'});
-    MockNow.tick(BackToTop.FADE_DURATION / 2);
-    MockRaf.next();
-    expect('.back-to-top').toHaveCss({opacity: '1'});
-  });
+  it('fades in the button when scroll is > 400, and fades out when scroll is < 400', () => {
+    const backToTop = ReactTestUtils.findRenderedDOMComponentWithClass(subject, 'back-to-top')
 
-  describe('when the scroll top is less than 400', function() {
-    beforeEach(function() {
-      MockNow.tick(BackToTop.FADE_DURATION);
-      MockRaf.next();
-      expect('.back-to-top').toHaveCss({opacity: '1'});
+    expect(backToTop).toHaveCss({opacity: '0'})
+    MockNow.tick(BackToTop.FADE_DURATION / 2)
+    MockRaf.next()
+    expect(backToTop).toHaveCss({opacity: '0.5'})
+    MockNow.tick(BackToTop.FADE_DURATION / 2)
+    MockRaf.next()
+    expect(backToTop).toHaveCss({opacity: '1'})
 
-      ScrollTop.setScrollTop(0);
-      triggerScroll();
-    });
+    ScrollTop.setScrollTop(300)
+    triggerScroll()
 
-    it('fades out the button', function() {
-      expect('.back-to-top').toHaveCss({opacity: '1'});
-      MockNow.tick(BackToTop.FADE_DURATION / 2);
-      MockRaf.next();
-      expect('.back-to-top').toHaveCss({opacity: '0.5'});
-      MockNow.tick(BackToTop.FADE_DURATION / 2);
-      MockRaf.next();
-      expect('.back-to-top').toHaveCss({opacity: '0'});
-    });
-  });
+    expect(backToTop).toHaveCss({opacity: '1'})
+    MockNow.tick(BackToTop.FADE_DURATION / 2)
+    MockRaf.next()
+    expect(backToTop).toHaveCss({opacity: '0.5'})
+    MockNow.tick(BackToTop.FADE_DURATION / 2)
+    MockRaf.next()
+    expect(backToTop).toHaveCss({opacity: '0'})
+  })
 
-  describe('when the back to top link is clicked', function() {
-    beforeEach(function() {
-      $('.back-to-top').simulate('click');
-    });
+  describe('when the back to top link is clicked', () => {
+    beforeEach(() => {
+      MockNow.tick(BackToTop.FADE_DURATION)
+      MockRaf.next()
 
-    it('animates the body scroll to the top', function() {
-      expect(ScrollTop.getScrollTop()).toEqual(500);
-      MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-      MockRaf.next();
-      expect(ScrollTop.getScrollTop()).toEqual(250);
-      MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-      MockRaf.next();
-      expect(ScrollTop.getScrollTop()).toEqual(0);
-    });
-  });
-});
+      const backToTop = ReactTestUtils.findRenderedDOMComponentWithClass(subject, 'back-to-top')
+      ReactTestUtils.Simulate.click(backToTop)
+    })
+
+    it('animates the body scroll to the top', () => {
+      expect(ScrollTop.getScrollTop()).toEqual(500)
+      MockNow.tick(BackToTop.SCROLL_DURATION / 2)
+      MockRaf.next()
+      expect(ScrollTop.getScrollTop()).toEqual(250)
+      MockNow.tick(BackToTop.SCROLL_DURATION / 2)
+      MockRaf.next()
+      expect(ScrollTop.getScrollTop()).toEqual(0)
+    })
+  })
+})
