bdc08e13a27f62b13776336ac749770b6a28fb39
Chris Wang <cwang@pivotal.io>
2017-03-21 15:51:34 -0600

    chore(modal) update modal_spec to use ReactTestUtils where possible
    [#139475461]

--- library/spec/pivotal-ui-react/modals/modals_spec.js
+++ library/spec/pivotal-ui-react/modals/modals_spec.js
@@ -1,190 +1,247 @@
 import '../spec_helper'
-import {Modal} from 'pui-react-modals'
+import {findByClass, findAllByClass, clickOn} from '../spec_helper'
+import {Modal, BaseModal} from 'pui-react-modals'
+import ReactTestUtils from 'react-addons-test-utils'
+
+let result
 
 describe('Modals', () => {
-  let modal
+  const renderComponent = props => ReactTestUtils.renderIntoDocument(<Modal {...props}>Hi</Modal>)
 
   beforeEach(() => {
-    modal = ReactDOM.render(
-      <Modal id="yolo" animation={false}> Hi </Modal>,
-      root
-    )
-  })
-
-  afterEach(() => {
-    ReactDOM.unmountComponentAtNode(root)
+    result = renderComponent({className: 'myModal', animation: false})
   })
 
   it('is closed by default', () => {
-    expect('#yolo').not.toExist()
+    expect(findAllByClass(result, 'myModal')).toHaveLength(0)
   })
 
   describe('#open', () => {
     it('opens the modal', () => {
-      modal.open()
+      result.open()
 
-      expect('#yolo').toExist()
+      expect(findByClass(result, 'myModal')).toBeDefined()
     })
   })
+
   describe('#close', () => {
     beforeEach(() => {
-      modal.open()
+      result.open()
     })
 
     it('closes the modal', () => {
-      modal.close()
+      result.close()
 
-      expect('#yolo').not.toExist()
+      expect(findAllByClass(result, 'myModal')).toHaveLength(0)
+    })
+  })
+
+  describe('animations', () => {
+    describe('with animations true', () => {
+      const delay = BaseModal.ANIMATION_TIME
+      beforeEach(() => {
+        result = renderComponent({animation: true})
+        result.open()
+      })
+
+      it('animates the modal in (with easeOutQuad)', () => {
+        const modalDialog = findByClass(result, 'modal-dialog')
+        const modalBackdrop = findByClass(result, 'modal-backdrop')
+
+        expect(modalDialog).toHaveCss({'margin-top': '0px'})
+        expect(modalBackdrop).toHaveCss({opacity: 0})
+
+        MockNow.tick(delay / 2)
+        MockRaf.next()
+
+        expect(modalDialog).toHaveCss({'margin-top': '37.5px'})
+        expect(modalBackdrop).toHaveCss({opacity: 0.6})
+
+        MockNow.tick(delay / 2)
+        MockRaf.next()
+
+        expect(modalDialog).toHaveCss({'margin-top': '50px'})
+        expect(modalBackdrop).toHaveCss({opacity: 0.8})
+      })
+
+      it('animates the modal out', () => {
+        MockNow.tick(delay)
+        MockRaf.next()
+
+        const modalDialog = findByClass(result, 'modal-dialog')
+        const modalBackdrop = findByClass(result, 'modal-backdrop')
+
+        result.close()
+
+        expect(modalDialog).toHaveCss({'margin-top': '50px'})
+        expect(modalBackdrop).toHaveCss({opacity: 0.8})
+
+        MockNow.tick(delay / 2)
+        MockRaf.next()
+
+        expect(modalDialog).toHaveCss({'margin-top': '12.5px'})
+        expect(modalBackdrop).toHaveCss({opacity: 0.2})
+
+        MockNow.tick(delay / 2)
+        MockRaf.next()
+
+        expect(findAllByClass(result, 'modal-dialog')).toHaveLength(0)
+        expect(findAllByClass(result, 'modal-backdrop')).toHaveLength(0)
+      })
+    })
+
+    describe('with animations false', () => {
+      it('does not animate if animation is false', () => {
+        result = renderComponent({animation: false})
+        result.open()
+
+        const modalDialog = findByClass(result, 'modal-dialog')
+        const modalBackdrop = findByClass(result, 'modal-backdrop')
+
+        expect(modalDialog).toHaveCss({'margin-top': '50px'})
+        expect(modalBackdrop).toHaveCss({opacity: 0.8})
+
+        result.close()
+
+        expect(findAllByClass(result, 'modal-dialog')).toHaveLength(0)
+        expect(findAllByClass(result, 'modal-backdrop')).toHaveLength(0)
+      })
+    })
+  })
+
+  describe('onEntered/onExited', () => {
+    describe('with animation', () => {
+      let delay, onEnterSpy, onExitSpy
+      beforeEach(() => {
+        delay = BaseModal.ANIMATION_TIME
+        onEnterSpy = jasmine.createSpy('onEnter')
+        onExitSpy = jasmine.createSpy('onExit')
+        result = renderComponent({animation: true,  onExited: onExitSpy, onEntered: onEnterSpy})
+        result.open()
+        MockNow.tick(delay)
+        MockRaf.next()
+      })
+
+      it('calls on entered when the shown animations are complete', () => {
+        expect(onEnterSpy).toHaveBeenCalled()
+        expect(onExitSpy).not.toHaveBeenCalled()
+      })
+
+      it('calls on exited when the closing animations are complete', () => {
+        onEnterSpy.calls.reset()
+
+        result.close()
+        MockNow.tick(delay)
+        MockRaf.next()
+
+        expect(onEnterSpy).not.toHaveBeenCalled()
+        expect(onExitSpy).toHaveBeenCalled()
+      })
     })
   })
 })
 
 describe('BaseModal', () => {
-  let BaseModal, subject
+  const renderComponent = props => ReactTestUtils.renderIntoDocument(<BaseModal {...props}/>)
+  const renderIntoDom = props => ReactDOM.render(<BaseModal {...props}/>, root)
 
-  beforeEach(() => {
-    BaseModal = require('../../../src/pivotal-ui-react/modals/modals').BaseModal
+  it('supports dialog className', () => {
+    result = renderComponent({className: 'myModal', show: true})
+    expect(findByClass(result, 'modal')).toHaveClass('myModal')
   })
 
-  afterEach(() => {
-    ReactDOM.unmountComponentAtNode(root)
-  })
-
-  describe('show', () => {
+  describe('when show is true', () => {
     beforeEach(() => {
-      subject = ReactDOM.render(
-        <BaseModal show id="mr-modal" title="hey mr modal"/>,
-        root
-      )
-    })
-    describe('when show is true', () => {
-      it('shows the modal', () => {
-        expect('#mr-modal').toExist()
-        expect('#mr-modal').toContainText('hey mr modal')
-      })
-
-      it('renders the title', () => {
-        expect('.modal-title').toHaveText('hey mr modal')
-      })
-
-      it('renders the modal in a dialog with a scrim', () => {
-        expect('.modal-backdrop').toHaveClass(['fade', 'in'])
-        expect('.modal').toHaveClass(['fade', 'in'])
-        expect('.modal').toHaveAttr('style', /display: block/)
-      })
+      result = renderComponent({show: true, className: 'myModal', title: 'hey mr modal'})
     })
 
-    describe('when show is false', () => {
-      beforeEach(() => {
-        subject::setProps({show: false})
-      })
-      it('hides the modal', () => {
-        expect('#mr-modal').not.toExist()
-      })
+    it('shows the modal', () => {
+      expect(findByClass(result, 'myModal')).toBeDefined()
+    })
+
+    it('shows the title', () => {
+      expect(findByClass(result, 'modal-title')).toHaveText('hey mr modal')
+    })
+
+    it('renders the modal in a dialog with a scrim', () => {
+      expect(findByClass(result, 'modal-backdrop')).toHaveClass(['fade', 'in'])
+      const modal = findByClass(result, 'modal')
+      expect(modal).toHaveClass(['fade', 'in'])
+      expect(modal).toHaveCss({display: 'block'})
     })
   })
 
-  describe('animations', () => {
-    let delay
+  describe('when show is false', () => {
     beforeEach(() => {
-      delay = BaseModal.ANIMATION_TIME
-
-      subject = ReactDOM.render(
-        <BaseModal show id="mr-modal" title="hey mr modal" animation={true}/>,
-        root
-      )
+      result = renderComponent({show: false, className: 'myModal'})
     })
 
-    it('animates the modal in (with easeOutQuad)', () => {
-      expect('.modal-dialog').toHaveAttr('style', /margin-top: 0px/)
-      expect('.modal-backdrop').toHaveAttr('style', /opacity: 0/)
-      MockNow.tick(delay / 2)
-      MockRaf.next()
-      expect('.modal-dialog').toHaveAttr('style', /margin-top: 37\.5px/)
-      expect('.modal-backdrop').toHaveAttr('style', /opacity: 0.6/)
-      MockNow.tick(delay / 2)
-      MockRaf.next()
-      expect('.modal-dialog').toHaveAttr('style', /margin-top: 50px/)
-      expect('.modal-backdrop').toHaveAttr('style', /opacity: 0.8/)
-    })
-
-    it('animates the modal out', () => {
-      MockNow.tick(delay)
-      MockRaf.next()
-      subject::setProps({show: false})
-      expect('.modal-dialog').toHaveAttr('style', /margin-top: 50px/)
-      expect('.modal-backdrop').toHaveAttr('style', /opacity: 0.8/)
-      MockNow.tick(delay / 2)
-      MockRaf.next()
-      expect('.modal-dialog').toHaveAttr('style', /margin-top: 12\.5px/)
-      expect('.modal-backdrop').toHaveAttr('style', /opacity: 0.2/)
-      MockNow.tick(delay / 2)
-      MockRaf.next()
-      expect('.modal-dialog').not.toExist()
-      expect('.modal-backdrop').not.toExist()
-    })
-
-    it('does not animate if animation is false', () => {
-      subject::setProps({animation: false})
-      expect('.modal-dialog').toHaveAttr('style', /margin-top: 50px/)
-      expect('.modal-backdrop').toHaveAttr('style', /opacity: 0.8/)
-      subject::setProps({show: false})
-      expect('.modal-dialog').not.toExist()
-      expect('.modal-backdrop').not.toExist()
+    it('hides the modal', () => {
+      expect(findAllByClass(result, 'myModal')).toHaveLength(0)
     })
   })
+
 
   describe('onHide', () => {
     let onHide
 
     beforeEach(() => {
       onHide = jasmine.createSpy('onHide')
-      subject = ReactDOM.render(
-        <BaseModal show id="ms-modal" onHide={onHide} animation={false}/>,
-        root
-      )
     })
 
     it('is triggered when close button is clicked', () => {
-      $('#ms-modal button.btn-icon').simulate('click')
+      result = renderComponent({show: true, onHide, animation: false})
+      clickOn(findByClass(result, 'btn-icon'))
+
       expect(onHide).toHaveBeenCalled()
     })
 
     it('is not triggered when the modal content itself is clicked', () => {
-      $('.modal-dialog').simulate('click')
+      result = renderComponent({show: true, onHide, animation: false})
+      clickOn(findByClass(result, 'modal-dialog'))
       expect(onHide).not.toHaveBeenCalled()
-      $('.modal-content').simulate('click')
+
+      clickOn(findByClass(result, 'modal-content'))
       expect(onHide).not.toHaveBeenCalled()
     })
 
-    it('is triggered when the backdrop', () => {
-      $('.modal').simulate('click')
+    it('is triggered when the backdrop is clicked', () => {
+      result = renderComponent({show: true, onHide, animation: false})
+      clickOn(findByClass(result, 'modal'))
       expect(onHide).toHaveBeenCalled()
     })
 
     it('is triggered on esc key down', () => {
+      result = renderIntoDom({show: true, onHide, animation: false})
+
       const evt = document.createEvent('HTMLEvents')
       evt.initEvent('keydown', true, true)
       evt.keyCode = BaseModal.ESC_KEY
       document.documentElement.dispatchEvent(evt)
+
       expect(onHide).toHaveBeenCalled()
     })
 
     it('is not triggered on esc key down if keyboard is false', () => {
-      subject::setProps({keyboard: false})
+      result = renderIntoDom({show: true, onHide, animation: false, keyboard: false})
+
       const evt = document.createEvent('HTMLEvents')
       evt.initEvent('keydown', true, true)
       evt.keyCode = BaseModal.ESC_KEY
       document.documentElement.dispatchEvent(evt)
+
       expect(onHide).not.toHaveBeenCalled()
     })
 
     it('cleans up keydown listeners', () => {
+      result = renderIntoDom({show: true, onHide, animation: false})
       ReactDOM.unmountComponentAtNode(root)
+
       const evt = document.createEvent('HTMLEvents')
       evt.initEvent('keydown', true, true)
       evt.keyCode = BaseModal.ESC_KEY
       document.documentElement.dispatchEvent(evt)
+
       expect(onHide).not.toHaveBeenCalled()
     })
   })
@@ -201,103 +258,34 @@ describe('BaseModal', () => {
     })
 
     it('steals focus on open when acquireFocus is true', () => {
-      subject = ReactDOM.render(
-        <BaseModal show id="ms-modal" animation={false}/>,
-        root
-      )
+      renderIntoDom({show: true, animation: false, acquireFocus: true})
       jasmine.clock().tick(1)
       expect('.modal').toBeFocused()
       expect('.i-was-here-first').not.toBeFocused()
     })
 
     it('does not steal focus on open when acquireFocus is false', () => {
-      subject = ReactDOM.render(
-        <BaseModal acquireFocus={false} show id="ms-modal" animation={false}/>,
-        root
-      )
+      renderIntoDom({show: true, animation: false, acquireFocus: false})
       jasmine.clock().tick(1)
       expect('.modal').not.toBeFocused()
       expect('.i-was-here-first').toBeFocused()
     })
   })
 
-  describe('onEntered/onExited', () => {
-    describe('with animation', () => {
-      let delay, onEnterSpy, onExitSpy
-      beforeEach(() => {
-        delay = BaseModal.ANIMATION_TIME
-        onEnterSpy = jasmine.createSpy('onEnter')
-        onExitSpy = jasmine.createSpy('onExit')
-        subject = ReactDOM.render(
-          <BaseModal show id="mr-modal" title="hey mr modal" animation={true} onExited={onExitSpy}
-                     onEntered={onEnterSpy}/>,
-          root
-        )
-      })
-
-      it('calls on entered when the shown animations are complete', () => {
-        expect(onEnterSpy).not.toHaveBeenCalled()
-        expect(onExitSpy).not.toHaveBeenCalled()
-        MockNow.tick(delay)
-        MockRaf.next()
-        expect(onEnterSpy).toHaveBeenCalled()
-        expect(onExitSpy).not.toHaveBeenCalled()
-      })
-
-      it('calls on exited when the closing animations are complete', () => {
-        MockNow.tick(delay)
-        MockRaf.next()
-        onEnterSpy.calls.reset()
-        subject::setProps({show: false})
-        MockNow.tick(delay)
-        MockRaf.next()
-        expect(onEnterSpy).not.toHaveBeenCalled()
-        expect(onExitSpy).toHaveBeenCalled()
-      })
-    })
-  })
 
   describe('sizing', () => {
-    beforeEach(() => {
-      subject = ReactDOM.render(
-        <BaseModal show id='ms-modal' animation={false}/>,
-        root
-      )
+    it('can set the size', () => {
+      expect(findByClass(renderComponent({show: true, size: 'sm'}), 'modal-dialog')).toHaveClass('modal-sm')
+      expect(findByClass(renderComponent({show: true, size: 'small'}), 'modal-dialog')).toHaveClass('modal-sm')
+      expect(findByClass(renderComponent({show: true, size: 'lg'}), 'modal-dialog')).toHaveClass('modal-lg')
+      expect(findByClass(renderComponent({show: true, size: 'large'}), 'modal-dialog')).toHaveClass('modal-lg')
     })
-
-    it('can set the small (sm) size', () => {
-      subject::setProps({size: 'sm'})
-      expect('.modal-dialog').toHaveClass('modal-sm')
-    })
-
-    it('can set the small size', () => {
-      subject::setProps({size: 'small'})
-      expect('.modal-dialog').toHaveClass('modal-sm')
-    })
-
-    it('can set the large (lg) size', () => {
-      subject::setProps({size: 'lg'})
-      expect('.modal-dialog').toHaveClass('modal-lg')
-    })
-
-    it('can set the large size', () => {
-      subject::setProps({size: 'large'})
-      expect('.modal-dialog').toHaveClass('modal-lg')
-    })
-  })
-
-  it('supports dialog className', () => {
-    ReactDOM.render(
-      <BaseModal show id="ms-modal" animation={false} dialogClassName="some-class-name"/>,
-      root
-    )
-    expect('.modal-dialog').toHaveClass('some-class-name')
   })
 
   describe('when there is no document', () => {
     it('does not throw an exception when rendered', () => {
       expect(() => {
-        ReactDOM.render(<BaseModal show id="mr-modal" title="hey mr modal" getDocument={() => {}}/>, root)
+        renderIntoDom(<BaseModal show id="mr-modal" title="hey mr modal" getDocument={() => {}}/>, root)
       }).not.toThrow()
     })
   })
