a5c4ff421130223cbcb4feba878f252036e65131
Ming Xiao <mxiao@pivotal.io>
2017-05-23 12:02:24 -0700

    chore(pipeline): Add jobs to automate creating a new release
    - introduce semver resource
    - use git-resource to merge to master
    - use serial_groups to so that test and deploy do not run concurrently
    - add new tasks
    
    [#145891047]
    
    Signed-off-by: Stephane Jolicoeur, sjolicoeur <s.jolicoeur, sjolicoeur@pivotal.io>

---
+++ concourse/tasks/update-styleguide/run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -ex
+
+function update_versions() {
+  pushd pivotal-ui/styleguide > /dev/null
+    pushd docs > /dev/null
+
+      # remove ` (latest)` string
+      line_number=`grep -nr "latest" other-versions.scss  | cut -d':' -f 2`
+      modified_file=/tmp/modified-versions.scss
+      /usr/local/bin/sed 's/ (latest)//g' other-versions.scss > $modified_file
+      latest_version_number=`/usr/local/bin/sed ${line_number}'!d' $modified_file | grep -Eo '([0-9]+)'`
+
+      # replace `"/"` with `"/$latest_version_number"`
+      /usr/local/bin/sed "s/\"\/\"/\"\/"$latest_version_number"\"/g" $modified_file > /tmp/tmp-top
+
+      head -n $line_number /tmp/tmp-top > /tmp/top
+      cat <<EOF > /tmp/bottom
+  </ul>
+  */
+  EOF
+
+     next_version_number=$((latest_version_number+1))
+     echo "  <li><a href=\"/\">Version ${next_version_number} (latest)</a></li>" > /tmp/text_to_insert
+     cat /tmp/top /tmp/text_to_insert /tmp/bottom > $modified_file
+
+     mv $modified_file other-versions.scss
+    popd > /dev/null
+  popd > /dev/null
+}
+
+function add_and_commit() {
+  version=`cat semver/version`
+  pushd pivotal-ui > /dev/null
+    git add styleguide/docs/other-versions.scss
+    git config --global user.email "cf-apps-manager@pivotal.io"
+    git config --global user.name "apps-manager"
+
+    git commit -m "Update other-versions.scss with major bump to v${version}"
+  popd > /dev/null
+}
+
+function main() {
+  update_versions
+  add_and_commit
+}
+
+main
