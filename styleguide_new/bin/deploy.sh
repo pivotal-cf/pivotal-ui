1360019998074db175f8ad66ca4281cae251a61c
Jonathan Berney <jberney@pivotal.io>
2017-06-01 11:02:08 -0700

    Do not prune out pui-* node modules
    Signed-off-by: Ming Xiao <mxiao@pivotal.io>

--- styleguide_new/bin/deploy.sh
+++ styleguide_new/bin/deploy.sh
@@ -6,22 +6,12 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-cp package.json package.json.bak
-
-function finish {
-  cp package.json.bak package.json
-  rm package.json.bak
-}
-
-trap finish EXIT
-
 cf api api.run.pivotal.io
 cf auth pivotal-ui@pivotal.io $1
 cf target -o pivotal -s pivotal-ui-staging
 
 yarn cache clean
 yarn install --no-progress
-npm prune
 ./node_modules/.bin/webpack --config ./backend-webpack.config.babel.js --progress -p
 ./node_modules/.bin/webpack --config ./frontend-webpack.config.babel.js --progress -p
 cf push
