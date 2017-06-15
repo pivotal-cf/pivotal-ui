8674cac0641d8a5c8c1148226c1484aa51f2f03b
Anushka Makhija <amakhija@pivotal.io>
2017-06-15 14:36:05 -0700

    TOC indented links should be based on h4 and not h3 - parse title in jsx and html code blocks
    [#146813697]
    
    Signed-off-by: Ming Xiao <mxiao@pivotal.io>

--- styleguide_new/spec/components/toc_sidebar_spec.js
+++ styleguide_new/spec/components/toc_sidebar_spec.js
@@ -1,77 +1,121 @@
 import '../spec_helper';
+const TocSidebar = require('../../src/components/toc_sidebar');
 
 describe('TocSidebar', () => {
-  let TocSidebar;
+  let json;
+  describe('headers', () => {
+    beforeEach(() => {
+      json = [
+        {
+          type: 'heading',
+          depth: 2,
+          children: [
+            {
+              value: 'I am an h2'
+            }
+          ]
+        },
+        {
+          type: 'heading',
+          depth: 4,
+          children: [
+            {
+              value: 'I am an H4'
+            }
+          ]
+        },
+        {
+          type: 'heading',
+          depth: 2,
+          children: [
+            {
+              value: 'I am another h2'
+            }
+          ]
+        },
+        {
+          type: 'heading',
+          depth: 4,
+          children: [
+            {
+              value: 'I am another H4'
+            }
+          ]
+        },
+        {
+          type: 'code'
+        }
+      ];
 
-  beforeEach(() => {
-    TocSidebar = require('../../src/components/toc_sidebar');
-
-    const json = [
-      {
-        type: 'heading',
-        depth: 2,
-        children: [
-          {
-            value: 'I am an h2'
-          }
-        ]
-      },
-      {
-        type: 'heading',
-        depth: 3,
-        children: [
-          {
-            value: 'I am an H3'
-          }
-        ]
-      },
-      {
-        type: 'heading',
-        depth: 2,
-        children: [
-          {
-            value: 'I am another h2'
-          }
-        ]
-      },
-      {
-        type: 'heading',
-        depth: 3,
-        children: [
-          {
-            value: 'I am another H3'
-          }
-        ]
-      },
-      {
-        type: 'code'
-      }
-    ];
-
-    ReactDOM.render(<TocSidebar {...{json}}/>, root);
-  });
-
-  it('renders the toc sidebar', () => {
-    expect('.toc-sidebar').toExist();
-  });
-
-  describe('renders the headings', () => {
-    it('with the correct classes', () => {
-      expect('.toc-sidebar a:eq(0)').toHaveText('I am an h2');
-      expect('.toc-sidebar a:eq(0)').toHaveClass('heading-2');
-      expect('.toc-sidebar a:eq(1)').toHaveText('I am an H3');
-      expect('.toc-sidebar a:eq(1)').toHaveClass('heading-3');
-      expect('.toc-sidebar a:eq(2)').toHaveText('I am another h2');
-      expect('.toc-sidebar a:eq(2)').toHaveClass('heading-2');
-      expect('.toc-sidebar a:eq(3)').toHaveText('I am another H3');
-      expect('.toc-sidebar a:eq(3)').toHaveClass('heading-3');
+      ReactDOM.render(<TocSidebar {...{json}}/>, root);
     });
 
-    it('as links to their subsections', () => {
-      expect('.toc-sidebar a:eq(0)').toHaveAttr('href', '#i-am-an-h-2');
-      expect('.toc-sidebar a:eq(1)').toHaveAttr('href', '#i-am-an-h-3');
-      expect('.toc-sidebar a:eq(2)').toHaveAttr('href', '#i-am-another-h-2');
-      expect('.toc-sidebar a:eq(3)').toHaveAttr('href', '#i-am-another-h-3');
-    })
+    it('renders the toc sidebar', () => {
+      expect('.toc-sidebar').toExist();
+    });
+
+    describe('renders the headings', () => {
+      it('with the correct classes', () => {
+        expect('.toc-sidebar a:eq(0)').toHaveText('I am an h2');
+        expect('.toc-sidebar a:eq(0)').toHaveClass('heading-2');
+        expect('.toc-sidebar a:eq(1)').toHaveText('I am an H4');
+        expect('.toc-sidebar a:eq(1)').toHaveClass('heading-4');
+        expect('.toc-sidebar a:eq(2)').toHaveText('I am another h2');
+        expect('.toc-sidebar a:eq(2)').toHaveClass('heading-2');
+        expect('.toc-sidebar a:eq(3)').toHaveText('I am another H4');
+        expect('.toc-sidebar a:eq(3)').toHaveClass('heading-4');
+      });
+
+      it('as links to their subsections', () => {
+        expect('.toc-sidebar a:eq(0)').toHaveAttr('href', '#i-am-an-h-2');
+        expect('.toc-sidebar a:eq(1)').toHaveAttr('href', '#i-am-an-h-4');
+        expect('.toc-sidebar a:eq(2)').toHaveAttr('href', '#i-am-another-h-2');
+        expect('.toc-sidebar a:eq(3)').toHaveAttr('href', '#i-am-another-h-4');
+      })
+    });
+  });
+
+  describe('jsx code-block', () => {
+    beforeEach(() => {
+      json = [
+        {
+          "type": "code",
+          "lang": "jsx",
+          "value": "::title=Example Title\n<div>foo</div>"
+        }
+      ];
+
+      ReactDOM.render(<TocSidebar {...{json}}/>, root);
+    });
+
+    it('renders the titles', () => {
+      expect('.toc-sidebar a:eq(0)').toHaveText('Example Title');
+    });
+
+    it('as links to their examples', () => {
+      expect('.toc-sidebar a:eq(0)').toHaveAttr('href', '#example-title');
+    });
+  });
+
+  describe('html code-block', () => {
+    beforeEach(() => {
+      json = [
+        {
+          "type": "code",
+          "lang": "html",
+          "value": "::title=Basic Example\n<div>content</div>"
+        }
+      ];
+
+      ReactDOM.render(<TocSidebar {...{json}}/>, root);
+    });
+
+    it('renders the titles', () => {
+      expect('.toc-sidebar a:eq(0)').toHaveText('Basic Example');
+    });
+
+    it('as links to their examples', () => {
+      expect('.toc-sidebar a:eq(0)').toHaveAttr('href', '#basic-example');
+    });
   });
 });
