47121ed2d47b633421ae9f159b082b0178525658
Anushka Makhija <amakhija@pivotal.io>
2017-07-06 10:20:42 -0700

    changed the default branch for the edit link in the JSArea toolbar
    [Finishes #148114207]
    
    Signed-off-by: Stephane Jolicoeur <sjolicoeur@pivotal.io>

--- styleguide_new/src/helpers/constants.js
+++ styleguide_new/src/helpers/constants.js
@@ -1,5 +1,5 @@
 export const githubRepo = 'https://github.com/pivotal-cf/pivotal-ui';
-export const githubBranch = 'development';
+export const githubBranch = 'master';
 
 export const issueUrl = name => {
   return `${githubRepo}/issues/new?title=fix(${name})%3A%20description&body=**Steps%20to%20Reproduce**%0A%0A**Expected**%0A${name}%20should%20do%20this%0A%0A**Result**%0A${name}%20do%20not%20do%20this`;
