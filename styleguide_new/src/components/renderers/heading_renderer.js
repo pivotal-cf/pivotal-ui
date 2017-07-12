e300b180393b67f0adf348c9fa0b05845421bd21
Jonathan Berney <jberney@pivotal.io>
2017-07-12 11:08:18 -0700

    custom styles for headers [#148631715]

--- styleguide_new/src/components/renderers/heading_renderer.js
+++ styleguide_new/src/components/renderers/heading_renderer.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import kebabCase from 'lodash.kebabcase';
+import classnames from 'classnames';
 
 export default level => class extends Component {
   render() {
     const Type = `h${level}`;
-    const {children: [text]} = this.props;
+    const {children: [text], className} = this.props;
     const id = kebabCase(text.toLowerCase());
 
-    return <Type {...this.props} {...{id}}/>;
+    return <Type {...this.props} {...{
+      id,
+      className: classnames(className, Type)
+    }}/>;
   }
 }
