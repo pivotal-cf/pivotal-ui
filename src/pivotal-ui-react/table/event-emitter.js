32ca3d230f10b5e09161f8efb63ff9e165e7597b
Jonathan <jberney1@icloud.com>
2017-08-13 19:20:49 -0700

    emitter refactor

--- src/pivotal-ui-react/table/event-emitter.js
+++ src/pivotal-ui-react/table/event-emitter.js
@@ -1,6 +1,3 @@
-export function emit(subject, {event, opts = {}, initial}) {
-  return subject.props.plugins.reduce((memo, plugin) => plugin[event]
-    ? plugin[event]({...opts, memo, subject})
-    : memo,
-    initial)
-}
+export const emit = (subject, {event, opts = {}, initial}) =>
+  subject.props.plugins.reduce((memo, plugin) =>
+    plugin[event] ? plugin[event]({...opts, memo, subject}) : memo, initial);
