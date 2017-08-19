ebc93e4cbcd2400c47f7476c8ccf61e578405b4d
Jonathan Berney <jberney1@icloud.com>
2017-08-19 15:21:16 -0700

    Revert "pluggable: only render first child"
    This reverts commit 40982b26b2d1ed1d5e53ea8fdf1b75de82d1cadd.

--- src/pivotal-ui-react/table/pluggable.js
+++ src/pivotal-ui-react/table/pluggable.js
@@ -1,8 +1,7 @@
 import flow from 'lodash.flow';
-import React from 'react';
 
 export default function Pluggable({plugins, children, type}) {
-  return flow(...plugins.map(p => p[type]).filter(Boolean))(React.Children.only(children));
+  return flow(...plugins.map(p => p[type]).filter(Boolean))(children);
 }
 
 export function useLast({reversedPlugins, type}) {
