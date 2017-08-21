0073f96322caad6c53ac16629c58addcfb4304d8
Jonathan <jberney1@icloud.com>
2017-08-20 18:40:49 -0700

    children refactor

--- src/pivotal-ui-react/table/table.js
+++ src/pivotal-ui-react/table/table.js
@@ -22,20 +22,20 @@ export class Table extends TablePlugin {
   render() {
     const {columns, data, Table, Thead, Tbody, Tr, Th, Td} = this.props;
 
-    const headers = columns.map((column, key) => {
-      return <Th {...{
-        key,
+    const headers = columns.map((column, key) =>
+      <Th {...{key}} {...{
         ...this.th({children: column.displayName || column.attribute}, {column})
-      }}/>;
-    });
+      }}/>);
 
     const headerRow = <Tr {...this.tr()}>{headers}</Tr>;
 
     const bodyCols = rowDatum => columns.map((column, key) =>
-      <Td {...{key, ...this.td({}, {column})}}>{rowDatum[column.attribute]}</Td>);
+      <Td {...{key}} {...{
+        ...this.td({children: rowDatum[column.attribute]}, {column})
+      }}/>);
 
     const bodyRows = data.map((rowDatum, key) =>
-      <Tr {...{key, ...this.tr()}}>{bodyCols(rowDatum)}</Tr>);
+      <Tr {...{key}} {...{...this.tr({children: bodyCols(rowDatum)})}}/>);
 
     return (
       <Table {...this.table({className: 'table'})}>
