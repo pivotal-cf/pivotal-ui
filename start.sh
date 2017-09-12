2317dd7fc3a9356108f4de008212ca51adbfc3de
Ming Xiao <mxiao@pivotal.io>
2017-09-12 10:32:13 -0700

    import pivotal-ui prismjs [#150374399]
    Signed-off-by: Jonathan Berney <jberney@pivotal.io>

--- start.sh
+++ start.sh
@@ -12,7 +12,8 @@ yarn cache clean
 yarn --no-progress
 
 rm -rf dist
-./node_modules/.bin/webpack --config ./backend_webpack.config.babel.js --progress -p
-./node_modules/.bin/webpack --config ./frontend_webpack.config.babel.js --progress -p
+./node_modules/.bin/webpack --config ./backend_webpack.config.babel.js -p &
+./node_modules/.bin/webpack --config ./frontend_webpack.config.babel.js -p &
+wait
 
 npm run watch
