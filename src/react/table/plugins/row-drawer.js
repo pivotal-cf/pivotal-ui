bbe85867421364e8623c4e2e6983f3917e155bbe
CF Apps Manager CI user <cf-apps-manager@pivotal.io>
2017-09-16 10:10:06 -0700

    css for table drawers [#151028511]

--- src/react/table/plugins/row-drawer.js
+++ src/react/table/plugins/row-drawer.js
@@ -146,7 +146,7 @@ export function withRowDrawer(Table) {
         }}/> );
       }
       return (
-        <div>
+        <div className="tr-drawer">
           <div {...props} {...{onClick, className}}>
             {leftColumn}
             {children}
