20464c8fb2189749514383e572a061097802c523
Reid Mitchell <rmitchell@pivotal.io>
2018-01-16 14:52:19 -0800

    Add ignored stories that come from Apps Manager tracker
    Signed-off-by: Steven Locke <slocke@pivotal.io>

--- tasks/helpers/tracker-helper.js
+++ tasks/helpers/tracker-helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ignoredStories = ['151262175', '148077489', '151620080'];
+const ignoredStories = ['151262175', '148077489', '151620080', '150190185', '153578787'];
 
 function url(storyNumber) {
   return `https://www.pivotaltracker.com/services/v5/projects/1126018/stories/${storyNumber}`;
