8632abcc8f8e2cf11be0b7609acacc012ec951a7
Steven Locke <slocke@pivotal.io>
2018-05-14 15:58:05 -0700

    Updated to gulp 4 and pui-react-tools 4.0.1 and use chrome in headless mode
    Signed-off-by: Ming Xiao <mxiao@pivotal.io>

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,25 @@
 import 'babel-polyfill';
-import requireDir from 'require-dir';
+import {Jasmine} from 'pui-react-tools';
+import devWebpack from './frontend_webpack.config.babel';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-requireDir('./tasks');
+
+Jasmine.install({
+  headlessServerOptions: {
+    includeStackTrace: true,
+    random: false,
+    isVerbose: false,
+    port: 8888,
+    driver: 'chrome'
+  },
+  appGlobs: ['./spec/index.js'],
+  headlessSpecRunnerOptions: {profile: true},
+  webpack: {
+    test: () => {
+      return {
+        ...devWebpack,
+        entry: './spec/index.js'
+      }
+    }
+  }
+});
