66322ccce2219656114bbc2697cc21956366c1f0
Ming Xiao <mxiao@pivotal.io>
2018-05-15 14:21:26 -0700

    Simplify webpack image loading
    Signed-off-by: Reid Mitchell <rmitchell@pivotal.io>

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,7 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
+        loaders: 'file-loader!image-webpack-loader'
       }
     ]
   },
