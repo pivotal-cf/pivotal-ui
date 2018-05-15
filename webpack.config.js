292e193a95030d3936a0cc94024312d320353496
CF Apps Manager CI user <cf-apps-manager@pivotal.io>
2018-05-15 14:47:12 -0700

    Revert "Simplify webpack image loading"
    This reverts commit 66322ccce2219656114bbc2697cc21956366c1f0.

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,10 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: 'file-loader!image-webpack-loader'
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
