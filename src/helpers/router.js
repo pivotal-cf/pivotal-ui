d8da646514e9d01be522e2c5be17a464e54dd49b
Reid Mitchell <reidmitchell1@gmail.com>
2018-06-02 22:38:01 -0700

    Move app.js into components/

---
+++ src/helpers/router.js
@@ -0,0 +1,33 @@
+import routes, {getRouteContent} from '../routes';
+
+let handlers;
+
+const Router = {
+  init() {
+    handlers = [];
+    window.addEventListener('popstate', Router.onPopState, false);
+  },
+
+  destroy() {
+    handlers = undefined;
+    window.removeEventListener('popstate', Router.onPopState);
+  },
+
+  onRouteChange(callback) {
+    if (!handlers) Router.init();
+    handlers.push(callback);
+  },
+
+  onPopState(evt) {
+    Router.navigate(evt.currentTarget.location.pathname, true);
+  },
+
+  navigate(newRoute, skipPushState) {
+    const newRouteContent = getRouteContent(newRoute);
+    document.getElementById('content').scrollTop = 0;
+    skipPushState || window.history.pushState({}, '', newRoute);
+    handlers.forEach(handler => handler(newRoute, newRouteContent));
+  }
+};
+
+export default Router;
