a4a7e0b2cc9d99d3bd77882b524791daacb767ed
Reid Mitchell <rmitchell@pivotal.io>
2018-06-07 16:45:02 -0700

    Add push script

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash -e
+
+echo "git pulling with rebase"
+git pull -r
+yarn lint
+yarn test
+git status
+printf 'push commit(s)? [y/N]: '
+read input
+if [[ "$input" == "y" || "$input" == "Y" ]]; then
+  git push
+else
+  echo "commit(s) not pushed"
+fi
