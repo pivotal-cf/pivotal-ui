fa78f5b085d571cc02bc5db05ebf95d59047f43e
Reid Mitchell <reidmitchell1@gmail.com>
2018-06-19 18:32:42 -0700

    Remove unused Fragment from PreRenderer

--- src/components/renderers/pre_renderer.js
+++ src/components/renderers/pre_renderer.js
@@ -1,4 +1,4 @@
-import React, {Fragment, PureComponent} from 'react';
+import React, {PureComponent} from 'react';
 import classnames from 'classnames';
 import CodeExample from '../code_example';
 
