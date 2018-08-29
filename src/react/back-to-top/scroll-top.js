2c61158c6e1de0ab2f7564bdc26598364c63da31
Reid Mitchell <rmitchell@pivotal.io>
2018-08-29 11:20:26 -0700

    fix backToTop in Chrome when scrollableId not set [#160149375]
    Signed-off-by: Sarah Weinstein <sweinstein@pivotal.io>

--- src/react/back-to-top/scroll-top.js
+++ src/react/back-to-top/scroll-top.js
@@ -1,3 +1,9 @@
-const getScrollTop = ({scrollTop}) => scrollTop;
-const setScrollTop = (value, element) => element.scrollTop = value;
+const getScrollTop = (element, document) => {
+  if (element) return element.scrollTop;
+  return document.body.scrollTop || document.documentElement.scrollTop;
+};
+const setScrollTop = (value, element, document) => {
+  if (element) element.scrollTop = value;
+  else document.body.scrollTop = document.documentElement.scrollTop = value;
+};
 export default {getScrollTop, setScrollTop};
