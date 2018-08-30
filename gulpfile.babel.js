09ce2831cf61dcf159570c126da0b95e855cc8e1
Reid Mitchell <rmitchell@pivotal.io>
2018-08-29 17:48:42 -0700

    Remove pui-react-tools

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,6 @@
-import {Jasmine} from 'pui-react-tools';
-import testWebpack from './config/webpack/test';
 import gulp from 'gulp';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-Jasmine.install({
-  headlessServerOptions: {
-    includeStackTrace: true,
-    random: undefined,
-    isVerbose: false,
-    port: 8888,
-    driver: 'chrome'
-  },
-  appGlobs: ['./spec/pivotal-ui-react/**/*_spec.js'],
-  headlessSpecRunnerOptions: {profile: true},
-  webpack: {test: () => testWebpack}
-});
 
 require('./tasks/css-components');
 require('./tasks/react-components');
