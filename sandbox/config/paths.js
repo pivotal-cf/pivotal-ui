9b05404450d72584c581533eac60ed252eeaf201
Reid Mitchell <reidmitchell1@gmail.com>
2019-01-09 18:43:36 -0800

    Upgrade sandbox dependencies, simplify webpack config

--- sandbox/config/paths.js
+++ sandbox/config/paths.js
@@ -4,8 +4,6 @@ const path = require('path');
 const fs = require('fs');
 const url = require('url');
 
-// Make sure any symlinks in the project folder are resolved:
-// https://github.com/facebookincubator/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
@@ -25,12 +23,6 @@ function ensureSlash(path, needsSlash) {
 const getPublicUrl = appPackageJson =>
   envPublicUrl || require(appPackageJson).homepage;
 
-// We use `PUBLIC_URL` environment variable or "homepage" field to infer
-// "public path" at which the app is served.
-// Webpack needs to know it to put the right <script> hrefs into HTML even in
-// single-page apps that may serve index.html for nested URLs like /todos/42.
-// We can't use a relative path in HTML because we don't want to load something
-// like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
   const servedUrl =
@@ -38,7 +30,6 @@ function getServedPath(appPackageJson) {
   return ensureSlash(servedUrl, true);
 }
 
-// config after eject: we're in ./config/
 module.exports = {
   dotenv: resolveApp('.env'),
   appBuild: resolveApp('build'),
@@ -50,7 +41,6 @@ module.exports = {
   puiJsSrc: path.resolve(__dirname, '../../src/js'),
   puiReactSrc: path.resolve(__dirname, '../../src/react'),
   yarnLockFile: resolveApp('yarn.lock'),
-  testsSetup: resolveApp('src/setupTests.js'),
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json')),
--- sandbox/sandbox.default.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import {DefaultButton} from 'pivotal-ui/react/buttons';
-
-export default () => <DefaultButton>Hello!</DefaultButton>
