b1016fc04dff9fe767cf2b8d7a09c2227914a6e4
Reid Mitchell <reidmitchell1@gmail.com>
2019-02-16 15:52:38 -0800

    Fix webpacking + babelifying of styleguide

--- styleguide/babel.config.js
+++ styleguide/babel.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    ['@babel/preset-env', {'loose': true}],
     '@babel/preset-react'
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-function-bind'
   ]
 };
