9ce5fb140e89853581404bcbe6dd62964576a5ec
Reid Mitchell <reidmitchell1@gmail.com>
2019-02-16 15:00:32 -0800

    Remove no-longer-used styleguide spec helper files

--- styleguide/spec/support/matchers/common.js
+++
@@ -1,8 +0,0 @@
-export function matcherResult({matcher, pass, notMessage, message}) {
-  return {
-    pass,
-    message: pass
-      ? () => `${this.utils.matcherHint('.not.' + matcher)}\n\n${notMessage}`
-      : () => `${this.utils.matcherHint('.' + matcher)}\n\n${message}`
-  };
-}
