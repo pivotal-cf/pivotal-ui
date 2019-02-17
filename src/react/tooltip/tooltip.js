371c3d1f6d301f0b62ed76cf4d556801dcf47f8b
Reid Mitchell <reidmitchell1@gmail.com>
2019-02-16 16:01:14 -0800

    Fix warning about calling setState on unmounted component in TooltipTrigger

--- src/react/tooltip/tooltip.js
+++ src/react/tooltip/tooltip.js
@@ -75,6 +75,11 @@ export class TooltipTrigger extends React.Component {
 
   componentDidMount() {
     require('../../css/tooltips');
+    this.mounted = true;
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
   }
 
   hoverHandler(e) {
@@ -85,7 +90,7 @@ export class TooltipTrigger extends React.Component {
     this.setState({visible: true});
     onClick(e);
     setTimeout(() => {
-      this.setState({visible: false});
+      this.mounted && this.setState({visible: false});
     }, this.props.clickHideDelay);
   }
 
