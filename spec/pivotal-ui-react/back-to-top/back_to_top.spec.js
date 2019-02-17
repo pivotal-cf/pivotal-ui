78d1f8e53dd5d5fb25f373647e4c9ee24ec7b075
Reid Mitchell <reidmitchell1@gmail.com>
2019-02-16 17:38:17 -0800

    Convert back-to-top spec to Jest

--- spec/pivotal-ui-react/back-to-top/back_to_top.spec.js
+++ spec/pivotal-ui-react/back-to-top/back_to_top.spec.js
@@ -1,6 +1,20 @@
-import '../spec_helper';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import {BackToTop} from '../../../src/react/back-to-top';
 import ScrollTop from '../../../src/react/back-to-top/scroll_top';
+
+const mockGetScrollTop = jest.fn(() => 0);
+const mockGetOpacity = jest.fn(() => 0);
+
+jest.mock('../../../src/react/mixins/mixins/animation_mixin',
+  () => ParentClass => class MockAnimation extends ParentClass {
+    animate(property) {
+      if (property === 'opacity') return mockGetOpacity();
+      return mockGetScrollTop();
+    }
+  }
+);
 
 describe('BackToTop', () => {
   let scrollTop;
@@ -20,18 +34,13 @@ describe('BackToTop', () => {
   describe('without scrollableId', () => {
     let subject;
 
-    beforeEach(done => {
+    beforeEach(() => {
       spyOn(window, 'addEventListener').and.callThrough();
       spyOn(window, 'removeEventListener').and.callThrough();
       subject = ReactDOM.render(<BackToTop className="foo" id="bar" style={{fontSize: '200px'}}/>, root);
 
-      jasmine.clock().uninstall();
-      setTimeout(() => {
-        jasmine.clock().install();
-        ScrollTop.setScrollTop(500);
-        triggerScroll();
-        done();
-      }, 0);
+      ScrollTop.setScrollTop(500);
+      triggerScroll();
     });
 
     it('adds an event listener to the window', () => {
@@ -54,11 +63,15 @@ describe('BackToTop', () => {
 
     it('fades in the button', () => {
       expect('.pui-back-to-top').toHaveStyle({display: 'inline', opacity: 0, 'font-size': '200px'});
-      MockNow.tick(BackToTop.FADE_DURATION / 2);
-      MockRaf.next();
+
+      mockGetOpacity.mockReturnValue(0.5);
+      subject.forceUpdate();
+
       expect('.pui-back-to-top').toHaveStyle({display: 'inline', opacity: 0.5, 'font-size': '200px'});
-      MockNow.tick(BackToTop.FADE_DURATION / 2);
-      MockRaf.next();
+
+      mockGetOpacity.mockReturnValue(1);
+      subject.forceUpdate();
+
       expect('.pui-back-to-top').toHaveStyle({display: 'inline', opacity: 1, 'font-size': '200px'});
     });
 
@@ -69,8 +82,6 @@ describe('BackToTop', () => {
 
     describe('when the scroll top is less than 400', () => {
       beforeEach(() => {
-        MockNow.tick(BackToTop.FADE_DURATION);
-        MockRaf.next();
         expect('.pui-back-to-top').toHaveStyle({display: 'inline', opacity: 1, 'font-size': '200px'});
 
         ScrollTop.setScrollTop(0);
@@ -79,27 +90,36 @@ describe('BackToTop', () => {
 
       it('fades out the button', () => {
         expect('.pui-back-to-top').toHaveStyle({display: 'inline', opacity: 1, 'font-size': '200px'});
-        MockNow.tick(BackToTop.FADE_DURATION / 2);
-        MockRaf.next();
+
+        mockGetOpacity.mockReturnValue(0.5);
+        subject.forceUpdate();
+
         expect('.pui-back-to-top').toHaveStyle({display: 'inline', opacity: 0.5, 'font-size': '200px'});
-        MockNow.tick(BackToTop.FADE_DURATION / 2);
-        MockRaf.next();
+
+        mockGetOpacity.mockReturnValue(0);
+        subject.forceUpdate();
+
         expect('.pui-back-to-top').toHaveStyle({display: 'inline', opacity: 0, 'font-size': '200px'});
       });
     });
 
     describe('when the back to top link is clicked', () => {
       beforeEach(() => {
+        mockGetScrollTop.mockReturnValue(500);
         $('.pui-back-to-top').simulate('click');
       });
 
       it('animates the body scroll to the top', () => {
         expect(ScrollTop.getScrollTop()).toEqual(500);
-        MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-        MockRaf.next();
+
+        mockGetScrollTop.mockReturnValue(62.5);
+        subject.forceUpdate();
+
         expect(ScrollTop.getScrollTop()).toEqual(62.5);
-        MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-        MockRaf.next();
+
+        mockGetScrollTop.mockReturnValue(0);
+        subject.forceUpdate();
+
         expect(ScrollTop.getScrollTop()).toEqual(0);
       });
 
@@ -109,27 +129,32 @@ describe('BackToTop', () => {
 
       it('calls setScrollTop', () => {
         expect(ScrollTop.setScrollTop).toHaveBeenCalledWith(500, undefined, document);
-        MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-        MockRaf.next();
+
+        mockGetScrollTop.mockReturnValue(62.5);
+        subject.forceUpdate();
+
         expect(ScrollTop.setScrollTop).toHaveBeenCalledWith(62.5, undefined, document);
-        MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-        MockRaf.next();
+
+        mockGetScrollTop.mockReturnValue(0);
+        subject.forceUpdate();
+
         expect(ScrollTop.setScrollTop).toHaveBeenCalledWith(0, undefined, document);
       });
     });
   });
 
   describe('with a scrollableId', () => {
-    let scrollableId, element, updateScroll;
+    let scrollableId, element, updateScroll, subject;
 
-    beforeEach(done => {
+    beforeEach(() => {
       scrollableId = 'scrollable';
-      element = jasmine.createSpyObj('element', ['addEventListener', 'removeEventListener']);
+      element = {addEventListener: jest.fn(), removeEventListener: jest.fn()};
       spyOn(document, 'getElementById').and.returnValue(element);
       ReactDOM.render(<div id={scrollableId} style={{height: '100px', maxHeight: '100px', overflowY: 'scroll'}}>
         <div {...{height: '500px'}}/>
         <BackToTop {...{
           ref: el => {
+            subject = el;
             if (el) updateScroll = el.updateScroll;
           },
           className: 'foo',
@@ -138,13 +163,8 @@ describe('BackToTop', () => {
         }}/>
       </div>, root);
 
-      jasmine.clock().uninstall();
-      setTimeout(() => {
-        jasmine.clock().install();
-        ScrollTop.setScrollTop(100, scrollableId);
-        triggerScroll();
-        done();
-      }, 0);
+      ScrollTop.setScrollTop(100, scrollableId);
+      triggerScroll();
     });
 
     it('adds an event listener to the element', () => {
@@ -158,6 +178,7 @@ describe('BackToTop', () => {
 
     describe('when the back to top link is clicked', () => {
       beforeEach(() => {
+        mockGetScrollTop.mockReturnValue(100);
         $('.pui-back-to-top').simulate('click');
       });
 
@@ -167,21 +188,29 @@ describe('BackToTop', () => {
 
       it('calls setScrollTop', () => {
         expect(ScrollTop.setScrollTop).toHaveBeenCalledWith(100, element, document);
-        MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-        MockRaf.next();
+
+        mockGetScrollTop.mockReturnValue(12.5);
+        subject.forceUpdate();
+
         expect(ScrollTop.setScrollTop).toHaveBeenCalledWith(12.5, element, document);
-        MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-        MockRaf.next();
+
+        mockGetScrollTop.mockReturnValue(0);
+        subject.forceUpdate();
+
         expect(ScrollTop.setScrollTop).toHaveBeenCalledWith(0, element, document);
       });
 
       it('animates the body scroll to the top', () => {
         expect(scrollTop).toEqual(100);
-        MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-        MockRaf.next();
+
+        mockGetScrollTop.mockReturnValue(12.5);
+        subject.forceUpdate();
+
         expect(scrollTop).toEqual(12.5);
-        MockNow.tick(BackToTop.SCROLL_DURATION / 2);
-        MockRaf.next();
+
+        mockGetScrollTop.mockReturnValue(0);
+        subject.forceUpdate();
+
         expect(scrollTop).toEqual(0);
       });
     });
