ac1d6307ef327fda3b366fb3659e1993d949b6bf
Reid Mitchell <reidmitchell1@gmail.com>
2019-02-16 21:16:52 -0800

    Reorganize & simplify gulp tasks slightly

--- gulpfile.babel.js
+++
@@ -1,6 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-require('./tasks/css_components');
-require('./tasks/react_components');
-require('./tasks/dev');
-require('./tasks/release');
