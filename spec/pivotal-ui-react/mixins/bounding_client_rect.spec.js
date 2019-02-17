e70f9a288e72515cd91ae4277327e305de77f36c
Reid Mitchell <reidmitchell1@gmail.com>
2019-02-16 18:04:56 -0800

    Convert bounding_client_rect_spec to use Jest

--- spec/pivotal-ui-react/mixins/bounding_client_rect.spec.js
+++ spec/pivotal-ui-react/mixins/bounding_client_rect.spec.js
@@ -1,20 +1,33 @@
-import '../spec_helper';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {setProps} from '../../support/jest-helpers';
 import {useBoundingClientRect} from '../../../src/react/mixins/components/bounding_client_rect';
+
+jest.mock('raf', () => callback => callback());
 
 describe('BoundingClientRect', () => {
   const width = 100;
   const height = 200;
 
-  let Klass, subject, resizeSpy;
+  let Klass, subject, resizeSpy, domNode;
 
   beforeEach(() => {
-    class Base extends React.Component {
+    domNode = {
+      getBoundingClientRect: jest.fn(() => ({width: 100, height: 200}))
+    };
+
+    jest.spyOn(ReactDOM, 'findDOMNode').mockReturnValue(domNode);
+
+    Klass = class extends React.Component {
       render() {
         return (<div className="klass" style={{width, height}}/>);
       }
-    }
-    spyOnRender(Base).and.callThrough();
-    Klass = Base;
+    };
+
+    jest.spyOn(Klass.prototype, 'render').mockImplementation(function() {
+      return this.props.children;
+    });
+
     const Component = useBoundingClientRect(Klass);
     subject = ReactDOM.render(<Component/>, root);
     resizeSpy = spyOn(subject, 'resize').and.callThrough();
@@ -22,22 +35,17 @@ describe('BoundingClientRect', () => {
 
   it('renders the component with a bounding rect and container', () => {
     expect(Klass).toHaveBeenRenderedWithProps({
-      container: $('.klass')[0],
+      container: domNode,
       boundingClientRect: jasmine.any(Object),
-      containerReady: jasmine.any(Promise)
+      containerReady: jasmine.any(Object)
     });
   });
 
   describe('containerReady', () => {
     describe('when some time has passed', () => {
-      beforeEach(() => {
-        MockNextTick.next();
-      });
-
-      it('returns a promise with the container', () => {
+      it('returns a promise with the container', async () => {
         const containerReadySpy = jasmine.createSpy('containerReady');
-        subject.state.containerReady.then(containerReadySpy);
-        MockPromises.tick();
+        await subject.state.containerReady.then(containerReadySpy);
         expect(containerReadySpy).toHaveBeenCalledWith(subject.state.container);
       });
     });
@@ -54,7 +62,7 @@ describe('BoundingClientRect', () => {
 
     it('does not do call forceUpdate if unmounted', () => {
       ReactDOM.render(<div/>, root);
-      expect(() => MockRaf.next()).not.toThrow();
+      expect(() => ReactDOM.unmountComponentAtNode(root)).not.toThrow();
     });
   });
 
@@ -62,34 +70,33 @@ describe('BoundingClientRect', () => {
     describe('when the bounding rect changes', () => {
       const width = 200;
       const height = 400;
+
       beforeEach(() => {
-        Klass.prototype.render.calls.reset();
-        const el = ReactDOM.findDOMNode(subject);
-        $(el).css({width, height});
+        Klass.prototype.render.mockClear();
+        domNode.getBoundingClientRect = jest.fn(() => ({width, height}));
         const event = document.createEvent('HTMLEvents');
         event.initEvent('resize', true, false);
         window.dispatchEvent(event);
-        MockRaf.next();
       });
 
       it('re-renders the component', () => {
-        expect(Klass.prototype.render).toHaveBeenCalled();
+        expect(Klass).toHaveBeenRendered();
       });
     });
 
     describe('when the bounding rect does not change', () => {
       let boundingClientRect;
+
       beforeEach(() => {
         boundingClientRect = subject.props.boundingClientRect;
-        Klass.prototype.render.calls.reset();
+        Klass.prototype.render.mockClear();
         const event = document.createEvent('HTMLEvents');
         event.initEvent('resize', true, false);
         window.dispatchEvent(event);
-        MockRaf.next();
       });
 
       it('does not re-renders the component', () => {
-        expect(Klass.prototype.render).not.toHaveBeenCalled();
+        expect(Klass).not.toHaveBeenRendered();
       });
 
       it('does not create a different bounding client rect', () => {
