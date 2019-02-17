645959c1f1b77ed8cc33df5bbf5c57dcff36426c
Reid Mitchell <reidmitchell1@gmail.com>
2019-02-17 13:36:18 -0800

    Share wrapPageElement between gatsby-browser and gatsby-ssr

--- styleguide/gatsby-ssr.js
+++ styleguide/gatsby-ssr.js
@@ -1,6 +1,1 @@
-const React = require('react');
-const Layout = require('./src/components/layout').default;
-
-exports.wrapPageElement = ({element, props}) => {
-  return <Layout {...props}>{element}</Layout>;
-};
+exports.wrapPageElement = require('./gatsby-browser').wrapPageElement;
