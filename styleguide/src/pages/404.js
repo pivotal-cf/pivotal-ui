d36af2d9b6822f319bb728fcf01222440d6d2ca8
Reid Mitchell <reidmitchell1@gmail.com>
2019-02-18 00:40:25 -0800

    Improve messaging on 404 page

--- styleguide/src/pages/404.js
+++ styleguide/src/pages/404.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Head from '../components/head';
+import {Link} from 'gatsby';
 
 const NotFoundPage = () => (
   <>
     <Head title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <header>
+      <h1 className="mvxxl pbl em-high border-bottom styleguide-h1">
+        404: Page not found
+      </h1>
+    </header>
+
+    <p>It looks like this page doesn't exist. Sorry about that!</p>
+
+    <p>Do you want to <Link to="/">go back home</Link>?</p>
   </>
 );
 
