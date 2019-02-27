deddb4d0ff947321996e62ce80668df2414b9635
Reid Mitchell <rmitchell@pivotal.io>
2019-02-27 15:29:30 -0800

    Add webpack config for building components.css file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const glob = require('glob');
+
+module.exports = {
+  entry: glob.sync('./dist/css/**/*.css'),
+  output: {
+    path: __dirname + '/dist',
+    filename: 'pivotal-ui.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(eot|ttf|woff)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({filename: 'components.css'})
+  ],
+  node: {
+    fs: 'empty',
+    module: 'empty'
+  }
+};
