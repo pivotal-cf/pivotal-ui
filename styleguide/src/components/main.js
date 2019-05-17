447e6b2f85d604db795e5b0463200c58fa8526a8
Raquel Breternitz <rbreternitz@pivotal.io>
2019-05-17 12:13:01 -0400

    Styleguide Edits: Restructure footer, update sidebar styling, and update Getting Started wording

--- styleguide/src/components/main.js
+++ styleguide/src/components/main.js
@@ -2,18 +2,35 @@ import React from 'react';
 import SidebarWrapper from './sidebar-wrapper';
 import '../../stylesheets/main.scss';
 
-const Main = ({children}) => (
+const year = new Date().getFullYear();
+
+const Main = ({ children }) => (
   <div className="sg-content">
     <div className="sg-full-sidebar">
-      <SidebarWrapper/>
+      <SidebarWrapper />
     </div>
-    <main className="sg-main" id="main" tabIndex="-1">
-      <div id="main-content-target"/>
-      <div className="sg-main__content">
-        {children}
-      </div>
-    </main>
+    <div className="sg-main-wrapper">
+      <main className="sg-main" id="main" tabIndex="-1">
+        <div id="main-content-target" />
+        <div className="sg-main__content">
+          {children}
+        </div>
+      </main>
+      <footer className="sg-footer mtxxxl pvxl border-top">
+        © {year} <a href="https://pivotal.io" target="_blank" rel="noreferrer">Pivotal Software</a>, Inc. All Rights Reserved.
+        <span className="pln">
+          <span className="mll">
+            <a href="https://pivotal.io/privacy-policy" target="_blank" rel="noreferrer">Privacy Policy</a>
+          </span>
+          <span className="mll">
+            <a href="https://pivotal.io/terms-of-use" target="_blank" rel="noreferrer">Terms of Use</a>
+          </span>
+          <span className="mll" id="teconsent" />
+        </span>
+      </footer>
+    </div>
   </div>
+
 );
 
 export default Main;
