00c51eeaf6410755ebe5659a23fd5aea3ce12c67
Sarah Weinstein <sweinstein@pivotal.io>
2019-06-01 13:57:53 -0700

    Bring 'Show HTML' button back to React code examples[#166396349]

--- styleguide/src/components/code-editor.js
+++ styleguide/src/components/code-editor.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
+import ReactDOMServer from 'react-dom/server';
 import {DefaultButton} from '../../../src/react/buttons';
 import {transform as babelify} from '@babel/standalone';
-import CodeMirror from 'react-codemirror';
+import pretty from 'pretty';
+import {Controlled as CodeMirror} from 'react-codemirror2';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/jsx/jsx';
 import 'codemirror/mode/htmlmixed/htmlmixed';
@@ -20,39 +22,65 @@ class ErrorBoundary extends React.Component {
   }
 }
 
-const getOutput = (code, language) => {
-  if (language === 'html') return (
-    <div dangerouslySetInnerHTML={{__html: code}}/>
-  );
-
+const getRenderedReact = jsCode => {
   try {
-    const transpiled = babelify(code, {presets: ['es2015', 'react']}).code;
     // eslint-disable-next-line no-eval
-    return eval(transpiled) || null;
+    const tempElem = React.createElement('div', {}, eval(jsCode));
+    const renderedCode = ReactDOMServer.renderToStaticMarkup(tempElem);
+    const strippedCode = renderedCode.replace(/^<div>/, '').replace(/<\/div>$/, '');
+    return pretty(strippedCode);
   } catch (err) {
-    return <pre><code>{err.toString()}</code></pre>;
+    console.error(err);
+    return '<!--Failed to render React into HTML. See console for details.-->';
   }
 };
 
 const CodeEditor = props => {
   const {title, description, code, language} = props;
+
+  const transpile = code => {
+    try {
+      return babelify(code, {presets: ['es2015', 'react']}).code;
+    } catch (err) {
+      return <pre><code>{err.toString()}</code></pre>;
+    }
+  };
+
+  const getOutput = (code, language) => {
+    if (!hasReact) return (
+      <div dangerouslySetInnerHTML={{__html: code}}/>
+    );
+    // eslint-disable-next-line no-eval
+    return eval(transpiled) || null;
+  };
+
   const [value, setValue] = useState(code);
-  const [showCode, setShowCode] = useState(false);
+  const [transpiled, setTranspiled] = useState(transpile(code));
+  const [showHtmlCode, setShowHtmlCode] = useState(false);
+  const [showReactCode, setShowReactCode] = useState(false);
+
+  const hasReact = language !== 'html';
   const output = getOutput(value, language);
-  const buttonText = language === 'html' ? 'HTML' : 'React';
-  const mode = language === 'html' ? 'htmlmixed' : 'jsx';
 
   return (
     <figure className="sg-code-editor">
       <figcaption className="sg-code-editor__header pal">
         <div className="display-flex" style={{justifyContent: 'space-between'}}>
           <span className="em-high">{title}</span>
-          <DefaultButton
-            small flat
-            className="sg-code-editor__button phn"
-            onClick={() => setShowCode(!showCode)}>
-            {showCode ? 'hide' : 'show'} {buttonText}
-          </DefaultButton>
+          <span>
+            {hasReact && <DefaultButton
+              small flat
+              className="sg-code-editor__button phn"
+              onClick={() => setShowReactCode(!showReactCode)}>
+              {showReactCode ? 'hide' : 'show'} React
+            </DefaultButton>}
+            <DefaultButton
+              small flat
+              className="sg-code-editor__button phn"
+              onClick={() => setShowHtmlCode(!showHtmlCode)}>
+              {showHtmlCode ? 'hide' : 'show'} HTML
+            </DefaultButton>
+          </span>
         </div>
         {description && (
           <div
@@ -64,13 +92,24 @@ const CodeEditor = props => {
       <div className="pal">
         <ErrorBoundary>{output}</ErrorBoundary>
       </div>
-      {showCode && (
+      {showReactCode && (
         <div className="border-top pal">
           <CodeMirror
             className="sg-code-editor__codemirror"
-            options={{mode}}
+            options={{mode: 'jsx'}}
             value={value}
-            onChange={newValue => setValue(newValue)}
+            onBeforeChange={(editor, data, value) => setValue(value) || setTranspiled(transpile(value))}
+            onChange={(editor, data, value) => {}}
+          />
+        </div>
+      )}{showHtmlCode && (
+        <div className="border-top pal">
+          <CodeMirror
+            className="sg-code-editor__codemirror"
+            options={{mode: 'htmlmixed', readOnly: hasReact}}
+            value={hasReact ? getRenderedReact(transpiled) : value}
+            onBeforeChange={(editor, data, value) => hasReact ? setTranspiled(value) : setValue(value)}
+            onChange={(editor, data, value) => {}}
           />
         </div>
       )}
