817b8fd43d91c941f31ae6fe69e8578b422c9bbb
Nick Guerette <nguerette@pivotal.io>
2019-07-30 14:11:04 -0700

    Add logHtml helper function
    Signed-off-by: Sarah Weinstein <sweinstein@pivotal.io>
    Signed-off-by: Nick Guerette <nguerette@pivotal.io>

--- spec/support/jest-helpers.js
+++ spec/support/jest-helpers.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import jQuery from 'jquery';
+import prettyFormat from 'pretty-format';
+
+const {DOMElement, DOMCollection} = prettyFormat.plugins;
 
 export function setProps(props, node = root) {
   const Component = this.constructor;
   ReactDOM.render(<Component {...this.props} {...props}/>, node);
 };
+
+export function logHtml(selector) {
+  const domNode = jQuery(selector)[0];
+  // eslint-disable-next-line no-console
+  console.log(prettyFormat(domNode, {plugins: [DOMElement, DOMCollection]}));
+}
