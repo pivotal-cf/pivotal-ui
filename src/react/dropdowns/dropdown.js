c54f496bfb38af74dfa6142e426b5d538616d770
Kenny Wang <kwang@pivotal.io>
2019-10-10 14:10:52 -0700

    Make dropdown more accessible as a navigation menu

--- src/react/dropdowns/dropdown.js
+++ src/react/dropdowns/dropdown.js
@@ -2,34 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import {default as mixin} from '../mixins';
-import Scrim from '../mixins/mixins/scrim_mixin';
 import Transition from '../mixins/mixins/transition_mixin';
 import {Icon} from '../iconography';
 import {Grid, FlexCol} from '../flex-grids';
+import {Scrim} from './scrim';
 
-const defaultToggleNode = (showIcon, icon, onClick, size, title, className, ariaLabel) => {
-  return (
-    <button {...{
-      type: 'button',
-      className,
-      onClick,
-      'aria-haspopup': true,
-      'aria-label': ariaLabel
-    }}>
-      {title}
-      {showIcon && <Icon src={icon} className="icon-toggle"/>}
-    </button>
-  );
-};
+const defaultToggleNode = ({showIcon, icon, title, ...props}) => (
+  <button {...{
+    type: 'button',
+    'aria-haspopup': 'true',
+    ...props
+  }}>
+    {title}
+    {showIcon && <Icon src={icon} className="icon-toggle"/>}
+  </button>
+);
 
-export class Dropdown extends mixin(React.Component).with(Scrim, Transition) {
+export class Dropdown extends mixin(React.Component).with(Transition) {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      open: false
+      open: false,
+      buttonId: props.buttonId || `dropdown-button-${Math.round(Math.random() * Number.MAX_SAFE_INTEGER)}`
     };
 
     this.click = this.click.bind(this);
+    this.containerRef = React.createRef();
   }
 
   static propTypes = {
@@ -37,8 +35,10 @@ export class Dropdown extends mixin(React.Component).with(Scrim, Transition) {
     border: PropTypes.bool,
     buttonAriaLabel: PropTypes.string,
     buttonClassName: PropTypes.string,
+    buttonId: PropTypes.string,
     closeOnMenuClick: PropTypes.bool,
     disableScrim: PropTypes.bool,
+    dropdownMenuClassName: PropTypes.string,
     flat: PropTypes.bool,
     floatMenu: PropTypes.bool,
     icon: PropTypes.string,
@@ -48,12 +48,12 @@ export class Dropdown extends mixin(React.Component).with(Scrim, Transition) {
     onClick: PropTypes.func,
     onEntered: PropTypes.func,
     onExited: PropTypes.func,
-    title: PropTypes.oneOfType([PropTypes.node, PropTypes.object]),
-    toggle: PropTypes.node,
     scroll: PropTypes.bool,
     showIcon: PropTypes.bool,
     size: PropTypes.oneOf(['normal', 'large', 'small']),
-    split: PropTypes.bool
+    split: PropTypes.bool,
+    title: PropTypes.oneOfType([PropTypes.node, PropTypes.object]),
+    toggle: PropTypes.node
   };
 
   static defaultProps = {
@@ -68,7 +68,6 @@ export class Dropdown extends mixin(React.Component).with(Scrim, Transition) {
   };
 
   componentDidMount() {
-    super.componentDidMount();
     require('../../css/dropdowns');
   }
 
@@ -87,18 +86,29 @@ export class Dropdown extends mixin(React.Component).with(Scrim, Transition) {
   render() {
     const {
       // eslint-disable-next-line no-unused-vars
-      closeOnMenuClick, onClick, onEntered, onExited,
+      closeOnMenuClick, onClick, onEntered, onExited, buttonId = this.state.buttonId,
       blockingScrim, border, buttonAriaLabel, buttonClassName, children, className, disableScrim, showIcon,
-      flat, link, menuAlign, size, icon, split, title, toggle, floatMenu, scroll, itemClassName, ...props
+      flat, link, menuAlign, size, icon, split, title, toggle, floatMenu, scroll, itemClassName, dropdownMenuClassName, ...props
     } = this.props;
 
-    const {open} = this.state;
+    const open = this.props.hasOwnProperty('open') ? this.props.open : this.state.open;
     const buttonStyleClasses = classnames('dropdown-toggle', buttonClassName);
     const noTitle = typeof title === 'undefined' || title === null || title.length === 0;
+    const menuId = `${buttonId}-menu`;
 
-    const forceIcon = noTitle || split;
-    const iconVisible = forceIcon || showIcon;
-    const toggleNode = toggle ? toggle : defaultToggleNode(iconVisible, icon, this.click, size, !split && title, buttonStyleClasses, buttonAriaLabel);
+    const toggleProps = {
+      showIcon: noTitle || split || showIcon,
+      icon,
+      onClick: this.click,
+      title: !split && title,
+      className: buttonStyleClasses,
+      'aria-label': buttonAriaLabel,
+      'aria-expanded': open,
+      'aria-controls': menuId,
+      id: buttonId
+    };
+
+    const toggleNode = toggle ? React.cloneElement(toggle, toggleProps) : defaultToggleNode(toggleProps);
     const menuVisibility = open ? 'dropdown-open' : 'dropdown-closed';
 
     const dropdownClasses = classnames('dropdown', {
@@ -117,26 +127,35 @@ export class Dropdown extends mixin(React.Component).with(Scrim, Transition) {
         'dropdown-menu-left': menuAlign === 'left',
         'dropdown-menu-float': split || flat || link || floatMenu || noTitle || menuAlign !== 'none',
         'dropdown-menu-scroll': scroll
-      }
+      },
+      dropdownMenuClassName
     );
     const dropdownOptions = (
       <div className={dropdownMenuClasses}>
-        <ul aria-label="submenu" onClick={this.menuClick}>
-          {React.Children.map(children, child => child ? <li className={itemClassName}>{child}</li> : null)}
+        <ul aria-labelledby={buttonId} role="menu" onClick={this.menuClick} id={menuId}>
+          {React.Children.map(children, child => child ? (
+            <li className={itemClassName} role="none">
+              {React.cloneElement(child, {role: 'menuitem', ...child.props})}
+            </li>
+          ) : null)}
         </ul>
       </div>
     );
 
-    return (<div className={dropdownClasses} {...props}>
-      {split ? <Grid gutter={false}>
-          <FlexCol className="dropdown-label">{title}</FlexCol>
-          <FlexCol fixed className="dropdown-icon-col col-middle">
-            {toggleNode}
-          </FlexCol>
-        </Grid>
-        : toggleNode}
-      {(blockingScrim && open && !disableScrim) && <div className="scrim" onClick={this.scrimClick}/>}
-      {dropdownOptions}
-    </div>);
+    return (
+      <Scrim containerRef={this.containerRef} onScrimClick={this.scrimClick}>
+        <div className={dropdownClasses} {...props} ref={this.containerRef}>
+          {split ? <Grid gutter={false}>
+              <FlexCol className="dropdown-label">{title}</FlexCol>
+              <FlexCol fixed className="dropdown-icon-col col-middle">
+                {toggleNode}
+              </FlexCol>
+            </Grid>
+            : toggleNode}
+          {(blockingScrim && open && !disableScrim) && <div className="scrim" onClick={this.scrimClick}/>}
+          {dropdownOptions}
+        </div>
+      </Scrim>
+    );
   }
 }
