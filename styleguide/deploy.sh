2b3f3158cb3c0c6b235f3c0833494651ce2e15ba
Reid Mitchell <rmitchell@pivotal.io>
2020-10-14 14:34:42 -0700

    Add styleguide deploy script
    This script builds the styleguide and pushes it to the gh-pages branch,
    where GitHub Pages will deploy from.
    
    [#174389991](https://www.pivotaltracker.com/story/show/174389991)

---
+++ styleguide/deploy.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+STYLEGUIDE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PUI_DIR="${STYLEGUIDE_DIR}/.."
+
+pushd $PUI_DIR
+  if [[ -n $(git status -s) ]]; then
+    echo "uncommitted changes detected; bailing out"
+    popd
+    exit
+  fi
+
+  yarn
+
+  pushd $STYLEGUIDE_DIR
+    yarn
+    yarn build
+  popd
+
+  CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+  CURRENT_SHA="$(git rev-parse HEAD)"
+  COMMIT_MSG="$(echo -e "build & deploy\n\nbranch: $CURRENT_BRANCH\nsha: $CURRENT_SHA")"
+
+  git checkout gh-pages
+
+  rm -rf docs
+  mkdir -p docs
+
+  mv styleguide/public/* docs
+  git add -N docs
+  git add docs
+
+  git commit \
+    --no-verify \
+    --author "pivotal-ui-bot <pivotal-ui@pivotal.io>" \
+    --message "${COMMIT_MSG}"
+
+  git push
+
+  git clean -df
+  git checkout -
+popd
